b02ab15de95464ad3e3b5099335b34d25077f823
Joe Zhou <bitcoin2095@gmail.com>
2016-07-31 19:06:01 -0400

    fixed type under post.jsx

--- app/components/pages/Post.jsx
+++ app/components/pages/Post.jsx
@@ -80,7 +80,7 @@ class Post extends React.Component {
                 </div>
 
                 //@Jasonmcz
-                //Comment: Perhaps it would be good to havd a render/timeout
+                //Comment: Perhaps it would be good to HAVE a render/timeout
                 //Because it takes about 5-15 seconds right now to load an account
                 //which pretty much shows up the promotion of $10.00 STEEM every single time
                 //perhaps add a timeout here prior to render this?

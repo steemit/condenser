6cf08b5f09fba59447a3838527e2c2292764c64d
Tim <roadscape@users.noreply.github.com>
2016-08-16 10:42:52 -0400

    clean up PostSummary, make preview text ctrl-clickable

--- app/components/cards/PostSummary.jsx
+++ app/components/cards/PostSummary.jsx
@@ -90,11 +90,10 @@ export default class PostSummary extends React.Component {
 
         // if(p.net_rshares < 0) desc = "";
 
-        let content_body = <div className="PostSummary__body entry-content" onClick={() => browserHistory.push(title_link_url)}>{desc}</div>;
+        let content_body = <div className="PostSummary__body entry-content"><Link to={title_link_url}>{desc}</Link></div>;
         let content_title = <h1 className="entry-title">{title_link}</h1>;
 
-        if( !currentCategory || (currentCategory && !currentCategory.match( /nsfw/ )) )
-        {
+        if( !(currentCategory && currentCategory.match( /nsfw/ )) ) {
            if (currentCategory !== '-' && currentCategory !== p.category && p.category.match(/nsfw/) ) {
                return null;
            }

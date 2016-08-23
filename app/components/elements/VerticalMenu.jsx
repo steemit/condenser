34b46a580f1b8dc50fa7673d67299d0fed22ecac
Tim <roadscape@users.noreply.github.com>
2016-08-23 15:58:34 -0500

    use Link instead of custom nav helper. Resolves #154

--- app/components/elements/VerticalMenu.jsx
+++ app/components/elements/VerticalMenu.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { browserHistory } from 'react-router';
+import { Link } from 'react-router'
 import Icon from 'app/components/elements/Icon.jsx';
 
 export default class VerticalMenu extends React.Component {
@@ -10,15 +10,6 @@ export default class VerticalMenu extends React.Component {
         hideValue: React.PropTypes.string,
     };
 
-    navigate = (e) => {
-        const a = e.target.nodeName.toLowerCase() === 'a' ? e.target : e.target.parentNode;
-        this.setState({show: false});
-        if (a.host !== window.location.host) return;
-        e.preventDefault();
-        document.body.click();
-        browserHistory.push(a.pathname + a.search + a.hash);
-    };
-
     render() {
         const {items, title, className, hideValue} = this.props;
         return <ul className={'VerticalMenu menu vertical' + (className ? ' ' + className : '')}>
@@ -26,9 +17,9 @@ export default class VerticalMenu extends React.Component {
             {items.map(i => {
                 if(i.value === hideValue) return null
                 return <li key={i.value}>
-                    {i.link ? <a href={i.link} onClick={i.onClick ? i.onClick : this.navigate}>
+                    {i.link ? <Link to={i.link} onClick={i.onClick}>
                         {i.icon && <Icon name={i.icon} />}{i.label ? i.label : i.value}
-                    </a> :
+                    </Link> :
                     <span>
                         {i.icon && <Icon name={i.icon} />}{i.label ? i.label : i.value}
                     </span>

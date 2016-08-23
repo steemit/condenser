43e32326e3405cbc7bc26898fab848ef4cf4b264
James Calfee <james@jcalfee.info>
2016-08-23 13:45:21 -0500

    Add youtube preview component. Speeds up rendering

--- app/components/elements/Template.jsx
+++ app/components/elements/Template.jsx
@@ -5,30 +5,40 @@ import transaction from 'app/redux/Transaction'
 import shouldComponentUpdate from 'app/utils/shouldComponentUpdate'
 import {Map} from 'immutable'
 
-const {func, string, object} = React.PropTypes
+const {string, object} = React.PropTypes
 
 class Template extends React.Component {
     static propTypes = {
     }
+
     static defaultProps = {
     }
+
     constructor() {
         super()
         this.state = {}
     }
+
     componentWillMount() {
     }
+
     componentDidMount() {
     }
+
     componentWillReceiveProps(nextProps) {
     }
-    shouldComponentUpdate = shouldComponentUpdate(this, 'Template') // This is based on react PureRenderMixin, it makes the component very efficient by not re-rendering unless something in the props or state changed.. PureRenderMixin comes highly recommended.  shouldComponentUpdate adds a debug boolean to show you why your component rendered (what changed, in the browser console type: steemDebug_shouldComponentUpdate=true).
+
+    // This is based on react PureRenderMixin, it makes the component very efficient by not re-rendering unless something in the props or state changed.. PureRenderMixin comes highly recommended.  shouldComponentUpdate adds a debug boolean to show you why your component rendered (what changed, in the browser console type: steemDebug_shouldComponentUpdate=true).
+    shouldComponentUpdate = shouldComponentUpdate(this, 'Template')
     componentWillUpdate(nextProps, nextState) {
     }
+
     componentDidUpdate(prevProps, prevState) {
     }
+
     componentWillUnmount() {
     }
+
     render() {
         const {} = this.props
         return (
@@ -37,7 +47,9 @@ class Template extends React.Component {
         )
     }
 }
+
 import {connect} from 'react-redux'
+
 export default connect(
     (state, ownProps) => {
         return {

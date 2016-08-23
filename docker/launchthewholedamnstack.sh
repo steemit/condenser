cc0009d8ecad16b6b61479c81751803e74cd6174
root <root@USASALE>
2016-08-22 22:19:55 -0500

    People say git's the easiest thing for what git does.  I'm not so sure.  This adds config.ini.  It's just a boilerplate.

---
+++ docker/launchthewholedamnstack.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm witness_node_data_dir.tar.gz
+rm -rf witness_node_data_dir
+wget http://www.steemitup.eu/witness_node_data_dir.tar.gz
+tar xvf witness_node_data_dir.tar.gz
+rm witness_node_data_dir/config.ini
+docker-compose build
+docker-compose up

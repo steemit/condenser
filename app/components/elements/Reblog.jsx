e14907a8dd6179549e262b81bf74c5f3e7df2333
Tim <roadscape@users.noreply.github.com>
2016-08-31 14:05:05 -0500

    reblog button component

---
+++ app/components/elements/Reblog.jsx
@@ -0,0 +1,57 @@
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import LoadingIndicator from 'app/components/elements/LoadingIndicator';
+import shouldComponentUpdate from 'app/utils/shouldComponentUpdate'
+import transaction from 'app/redux/Transaction';
+import Icon from 'app/components/elements/Icon';
+
+const {string, object, bool, func, any} = PropTypes
+
+export default class Reblog extends React.Component {
+    static propTypes = {
+        account: string,
+        author: string,
+        permlink: string,
+        reblog: func,
+    }
+    constructor(props) {
+        super()
+        this.shouldComponentUpdate = shouldComponentUpdate(this, 'Reblog')
+        this.state = {active: false} // may need a "loading" flag
+        this.reblog = () => {
+            const {account, author, permlink, reblog} = this.props
+            reblog(account, author, permlink)
+            this.setState({active: true})
+        }
+    }
+
+    render() {
+        let state = this.state.active ? 'active' : 'inactive'
+        return <span className={'Reblog__button Reblog__button-'+state}>
+            <a href="#" onClick={this.reblog}><Icon name="reblog" /></a>
+        </span>
+    }
+}
+module.exports = connect(
+    (state, ownProps) => {
+        let {account} = ownProps
+        if(!account) {
+            const current_user = state.user.get('current')
+            account = current_user ? current_user.get('username') : null
+        }
+        return {...ownProps, account}
+    },
+    dispatch => ({
+        reblog: (account, author, permlink) => {
+            const json = ['reblog', {account, author, permlink}]
+            dispatch(transaction.actions.broadcastOperation({
+                type: 'custom_json',
+                operation: {
+                    id: 'reblog',
+                    required_posting_auths: [account],
+                    json: JSON.stringify(json),
+                },
+            }))
+        },
+    })
+)(Reblog)

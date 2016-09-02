3e2a3a0bba36f90b0438693c1fa4405149270189
valzav <i@valzav.com>
2016-09-02 14:43:33 -0400

    simplify author drop down

--- app/components/elements/Author.jsx
+++ app/components/elements/Author.jsx
@@ -33,18 +33,11 @@ class Author extends React.Component {
         if(!username)
             return author_link
 
-        const dropdown = <span>
-            <div className="row">
-                <div className="column small-12">
-                    {author_link}
-                </div>
-                <hr />
-                <div className="column small-12">
-                    <Follow className="float-right" follower={username} following={author} what="blog"
-                        showFollow={follow} showMute={mute} />
-                </div>
-            </div>
-        </span>
+        const dropdown = <div className="Author__dropdown">
+            <Link to={'/@' + author}>Profile</Link> &nbsp;
+            <Follow className="float-right" follower={username} following={author} what="blog"
+                    showFollow={follow} showMute={mute} />
+        </div>
 
         return (
             <span className="Author">

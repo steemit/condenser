e66def04982954aa58acba1daad4a664cee0bf7c
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2016-09-06 14:45:53 +0200

    test branch chain_id

--- shared/api_client/ChainConfig.js
+++ shared/api_client/ChainConfig.js
@@ -2,9 +2,9 @@ import { ecc_config, hash } from "../ecc"
 
 ecc_config.address_prefix = "TST";
 
-let chain_id = ""
-for(let i = 0; i < 32; i++) chain_id += "00"
-//let chain_id = "8f5ec76c58e23352a90785f8fa4674aed80bc1d318e19956c5d263e7fba53186"
+//let chain_id = ""
+//for(let i = 0; i < 32; i++) chain_id += "00"
+let chain_id = "8f5ec76c58e23352a90785f8fa4674aed80bc1d318e19956c5d263e7fba53186"
 
 module.exports = {
     address_prefix: "TST",

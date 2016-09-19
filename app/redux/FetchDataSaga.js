31056dedefc7a588ec729eb1bd3e9b5a658aa619
Valentine Zavgorodnev <i@valzav.com>
2016-09-19 18:14:26 -0400

    #282 post overlay (#286)
    * add overlay to PostsList; add onClick event to PostSummary; adjust overlay's style
    
    * close post overlay on back button
    
    * add PostPage component that wraps Post
    
    * fetch post's data
    
    * style on smaller screen differently
    
    * some more styling and ability to close via outside click
    
    * use onClick to open posts in overlay in PostSummary
    
    * fix changing sort order issue
    
    * put button inside post's container

--- app/redux/FetchDataSaga.js
+++ app/redux/FetchDataSaga.js
@@ -6,14 +6,14 @@ import GlobalReducer from './GlobalReducer';
 import constants from './constants';
 import {fromJS, Map} from 'immutable'
 
-export const fetchDataWatches = [watchLocationChange, watchDataRequests, watchApiRequests, watchFetchJsonRequests];
+export const fetchDataWatches = [watchLocationChange, watchDataRequests, watchApiRequests, watchFetchJsonRequests, watchFetchState];
 
 export function* watchDataRequests() {
     yield* takeLatest('REQUEST_DATA', fetchData);
 }
 
 export function* fetchState(location_change_action) {
-    const {pathname, /*search*/} = location_change_action.payload;
+    const {pathname} = location_change_action.payload;
     const m = pathname.match(/@([a-z0-9\.-]+)/)
     if(m && m.length === 2) {
         const username = m[1]
@@ -52,6 +52,10 @@ export function* fetchState(location_change_action) {
 
 export function* watchLocationChange() {
     yield* takeLatest('@@router/LOCATION_CHANGE', fetchState);
+}
+
+export function* watchFetchState() {
+    yield* takeLatest('FETCH_STATE', fetchState);
 }
 
 export function* fetchData(action) {

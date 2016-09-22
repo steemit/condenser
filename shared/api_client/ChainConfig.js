57d2e43fabeea2f5a85dbc131a4a42e67e609358
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2016-09-22 22:15:22 +0200

    chain id

--- shared/api_client/ChainConfig.js
+++ shared/api_client/ChainConfig.js
@@ -2,8 +2,9 @@ import { ecc_config, hash } from "../ecc"
 
 ecc_config.address_prefix = "STM";
 
-let chain_id = ""
-for(let i = 0; i < 32; i++) chain_id += "00"
+//let chain_id = ""
+//for(let i = 0; i < 32; i++) chain_id += "00"
+let chain_id = "782a3039b478c839e4cb0c941ff4eaeb7df40bdd68bd441afd444b9da763de12"
 
 module.exports = {
     address_prefix: "STM",

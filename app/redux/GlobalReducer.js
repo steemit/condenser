b1d1063a4d8e0c5ce844a2aa18594150a9b2601d
Tim <roadscape@users.noreply.github.com>
2016-09-22 14:44:15 -0400

    always key by current account #313

--- app/redux/GlobalReducer.js
+++ app/redux/GlobalReducer.js
@@ -152,8 +152,8 @@ export default createModule({
                 // console.log('-- RECEIVE_DATA state -->', state.toJS());
                 let new_state;
                 if (order === 'by_author' || order === 'by_feed' || order === 'by_comments' || order === 'by_replies') {
-                    // category is either "blog", "feed", "posts", or "recent_replies"
-                    const key = ['accounts', order === 'by_feed' || order === 'by_replies' ? accountname : author, category]
+                    // category is either "blog", "feed", "posts", or "recent_replies" (respectively) -- and all posts are keyed under current profile
+                    const key = ['accounts', accountname, category]
                     new_state = state.updateIn(key, List(), list => {
                         return list.withMutations(posts => {
                             data.forEach(value => {

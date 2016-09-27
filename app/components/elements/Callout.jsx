12d6c4e054060cdd8119b90ac426b085493a3e72
Mihail Paley <undeadlol1@mail.ru>
2016-09-27 16:52:52 +0200

    (Callout) fix empty object margin

--- app/components/elements/Callout.jsx
+++ app/components/elements/Callout.jsx
@@ -4,7 +4,8 @@ export default ({title, body, type = 'alert'}) => {
     return <div className="row">
         <div className="column">
             <div className={'callout ' + type}>
-                <h4>{title}</h4>
+                {/* do not crate header element if not needed to avoid padding/margin styles of empty element */}
+                {title ? <h4>{title}</h4> : null}
                 <p>{body}</p>
             </div>
         </div>

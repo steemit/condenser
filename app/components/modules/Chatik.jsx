0a611c279d0b9cfb958eb530c7fa42d1f4d0f321
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2016-09-27 17:29:34 +0200

    edit rrrocket widget

--- app/components/modules/Chatik.jsx
+++ app/components/modules/Chatik.jsx
@@ -4,29 +4,36 @@ import CloseButton from 'react-foundation-components/lib/global/close-button';
 export default class Chatik extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {toggle: false};
+        this.state = {open: false};
     }
 
     toggle() {
-        this.setState({toggle: !this.state.toggle});
+        if (!this.state.toggled) {
+            setTimeout(this.setState({toggled: true}), 500)
+        }
+        this.setState({open: !this.state.open});
     }
 
-    sizeW(){
-        return this.state.toggle?'500vw':'0'
+    link() {
+        return this.state.toggled?"https://chat.golos.io":""
     }
 
-    sizeH(){
-        return this.state.toggle?"500vh":'0'
+    iconUrl(){ return "https://avatars1.githubusercontent.com/RocketChat?&s=48"}
+
+    chatikClass(){
+        return 'chatik '
     }
 
     buttonClass(){
-        return this.state.toggle?"":"open"
+        return this.state.open?"open":""
     }
 
     render() {
-        return <div className="chatik" style={{backgound:'white', position:'absolute', bottom:'0', right:'0'}}>
-            <button className={this.buttonClass()} onClick={this.toggle.bind(this)}><img src="https://avatars1.githubusercontent.com/RocketChat?&s=48" /></button>
-                <iframe src="https://chat.golos.io" width={this.sizeW()} height={this.sizeH()}/>
+        return <div className={this.chatikClass()+this.buttonClass()}>
+            <button className={this.buttonClass()} style={{bottom:'0', position:'absolute', width:'48px', height:'48px', left:'-48px'}} onClick={this.toggle.bind(this)}><img src={this.iconUrl()} /></button>
+            <div className="sidebar" style={{position:'relative', width:'100%', height:'100%'}}>
+                <iframe src={this.link()} width="100%" height="100%" />
+            </div>
         </div>;
     }
 }

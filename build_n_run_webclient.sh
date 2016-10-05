caed7c954bd12e550932520758f83a2a7a94146d
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2016-10-05 15:17:08 +0200

    .

--- build_n_run_webclient.sh
+++ build_n_run_webclient.sh
@@ -2,7 +2,7 @@
 git status
 echo "commit hash#"$(git rev-parse --verify HEAD)"\n\n"
 echo "{\"githash\":\""$(git rev-parse --verify HEAD)"\"}" >> dist/test.json
-#npm install && npm run build 
+npm install && npm run build 
 if [ $(screen -ls | grep 'web' | awk '{print $1}') ]
 then 
   screen -X -S $(screen -ls | grep 'web' | awk '{print $1}') quit

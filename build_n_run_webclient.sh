a329ac51750e0dcd03afd2165a3d800a2efcdeaa
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2016-10-05 22:57:02 +0200

    fix build script

--- build_n_run_webclient.sh
+++ build_n_run_webclient.sh
@@ -3,10 +3,10 @@ git status
 echo "commit hash#"$(git rev-parse --verify HEAD)"\n\n"
 echo "{\"githash\":\""$(git rev-parse --verify HEAD)"\"}" > config/last-build.json
 npm install && npm run build 
-if [ $(screen -ls | grep 'web' | awk '{print $1}') ]
+if [ $(screen -ls | grep 'webclient' | awk '{print $1}') ]
 then 
-  screen -X -S $(screen -ls | grep 'webclientgolos' | awk '{print $1}') quit
+  screen -X -S $(screen -ls | grep 'webclient' | awk '{print $1}') quit
 fi
 sleep 5
-screen -dmS webclientgolos npm run prod
+screen -dmS webclient npm run prod
 

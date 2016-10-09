d4bf0c29833f6a63e7a34ffb8c21f9ec73d71ad9
Mihail Paley <undeadlol1@mail.ru>
2016-10-09 21:15:23 +0200

    (LandingTeam) add animations, fix facebook logo

--- app/components/elements/LandingTeam.jsx
+++ app/components/elements/LandingTeam.jsx
@@ -13,7 +13,7 @@ export default class LandingTeam extends React.Component {
 				</div>
 
 				<div className="row Team__members">
-					<div className="small-6 medium-4 large-3 columns Team__member">
+					<div className="small-6 medium-4 large-3 columns Team__member wow fadeIn" data-wow-delay="1s">
 						<div className="Team__members__image-wrapper">
 							<img className="Team__member__img" src="https://avatars2.githubusercontent.com/u/410789?v=3&s=400" alt="фотография Димы Стародубцева" />
 						</div>
@@ -25,7 +25,7 @@ export default class LandingTeam extends React.Component {
 						</ul>
 					</div>
 
-					<div className="small-6 medium-4 large-3 columns Team__member">
+					<div className="small-6 medium-4 large-3 columns Team__member wow fadeIn" data-wow-delay="1s">
 						<div className="Team__members__image-wrapper">
 							<img className="Team__member__img" src="https://avatars1.githubusercontent.com/u/1690657?v=3&s=400" alt="фотография Валерия Литвина" />
 						</div>
@@ -33,11 +33,11 @@ export default class LandingTeam extends React.Component {
 						<p>Блокчейн-разработка</p>
 						<ul>
 							<li><a href="https://www.linkedin.com/in/valery-litvin-97864267"><img src="images/landing/linkedin_icon.jpg" /></a></li>
-							<li><a href="https://www.facebook.com/bessonby"><Icon name="facebook" size="2x" /></a></li>
+							<li><a href="https://www.facebook.com/bessonby"><img className="Team__facebook-logo" src="https://cdn1.iconfinder.com/data/icons/logotypes/32/square-facebook-512.png" /></a></li>
 							<li><a href="https://github.com/ValeryLitvin"><img src="images/landing/github_icon.jpg" /></a></li>
 						</ul>
 					</div>
-					<div className="small-6 medium-4 large-3 columns Team__member Team__member__marina">
+					<div className="small-6 medium-4 large-3 columns Team__member Team__member__marina wow fadeIn" data-wow-delay="1s">
 						<div className="Team__members__image-wrapper">
 							<img className="Team__member__img" src="images/landing/Marina.jpg" alt="фотография Марины Гуревой" />
 						</div>
@@ -45,12 +45,12 @@ export default class LandingTeam extends React.Component {
 						<p>Партнерство</p>
 						<ul>
 							<li><a href="https://fr.linkedin.com/in/marina-guryeva-1390a03b"><img src="images/landing/linkedin_icon.jpg" /></a></li>
-							<li><a href="https://www.facebook.com/marina.guryeva.79"><Icon name="facebook" size="2x" /></a></li>
+							<li><a href="https://www.facebook.com/marina.guryeva.79"><img className="Team__facebook-logo" src="https://cdn1.iconfinder.com/data/icons/logotypes/32/square-facebook-512.png" /></a></li>
 							<li><a href="https://www.github.com/mguryeva"><img src="images/landing/github_icon.jpg" /></a></li>
 						</ul>
 					</div>
 
-					<div className="small-6 medium-4 large-3 columns Team__member Team__member__alexey">
+					<div data-wow-delay="2s" className="wow fadeIn small-6 medium-4 large-3 columns Team__member Team__member__alexey">
 						<div className="Team__members__image-wrapper">
 							<img className="Team__member__img" src="images/landing/alexey.jpg" alt="фотография Алексея Фрумина" />
 						</div>
@@ -62,7 +62,7 @@ export default class LandingTeam extends React.Component {
 							<li><a href="https://github.com/tomarcafe"><img src="images/landing/github_icon.jpg" /></a></li>
 						</ul>
 					</div>
-					<div className="small-6 medium-4 large-3 columns Team__member Team__member__vitaly">
+					<div data-wow-delay="2s" className="wow fadeIn small-6 medium-4 large-3 columns Team__member Team__member__vitaly">
 						<div className="Team__members__image-wrapper">
 							<img className="Team__member__img" src="https://s3.amazonaws.com/keybase_processed_uploads/bc86969a26560b7a081d1f720f6f3c05_360_360_square_360.jpeg" alt="фотография Виталия Львова" />
 						</div>
@@ -75,7 +75,7 @@ export default class LandingTeam extends React.Component {
 						</ul>
 					</div>
 
-					<div className="small-6 medium-4 large-3 columns Team__member Team__member__kostya">
+					<div data-wow-delay="2s" className="wow fadeIn small-6 medium-4 large-3 columns Team__member Team__member__kostya">
 						<div className="Team__members__image-wrapper">
 							<img className="Team__member__img" src="images/landing/kostya.jpg" alt="фотография Контстантина Ломашука" />
 						</div>
@@ -83,26 +83,27 @@ export default class LandingTeam extends React.Component {
 						<p>Развитие бизнеса</p>
 						<ul>
 							<li><a href="https://ru.linkedin.com/in/lomashuk"><img src="images/landing/linkedin_icon.jpg" /></a></li>
-							<li><a href="https://www.facebook.com/k.lomashuk"><Icon name="facebook" size="2x" /></a></li>
+							<li><a href="https://www.facebook.com/k.lomashuk"><img className="Team__facebook-logo" src="https://cdn1.iconfinder.com/data/icons/logotypes/32/square-facebook-512.png" /></a></li>
 							<li><a href="https://github.com/lomashuk"><img src="images/landing/github_icon.jpg" /></a></li>
 						</ul>
 					</div>
-					<div className="small-6 medium-4 large-3 columns Team__member Team__member__misha">
+					<div data-wow-delay="3s" className="wow fadeIn small-6 medium-4 large-3 columns Team__member Team__member__misha">
 						<div className="Team__members__image-wrapper">
 							{/* <img className="Team__member__img" src="https://pp.vk.me/c619721/v619721379/1de32/S_1velJqxSA.jpg" alt="фотография Михаила Палей" /> */}
-							<img className="Team__member__img" src="https://pp.vk.me/c637427/v637427379/6319/yNXcC70HrLA.jpg" alt="фотография Михаила Палей" />
+							<img className="Team__member__img" src="https://pp.vk.me/c636924/v636924379/26083/2NFy5W_6V8o.jpg" alt="фотография Михаила Палей" />
+							{/* <img className="Team__member__img" src="https://pp.vk.me/c637427/v637427379/6319/yNXcC70HrLA.jpg" alt="фотография Михаила Палей" /> */}
 						</div>
 						<strong>Михаил Палей</strong>
 						<p>Front-end разработка</p>
 						<ul>
-							<li><a href="https://www.vk.com/paleyblog">vk</a></li>
+							{/* <li><a href="https://www.vk.com/paleyblog">vk</a></li> */}
 							<li><a href="https://www.github.com/Undeadlol1"><img src="images/landing/github_icon.jpg" /></a></li>
 						</ul>
 					</div>
 				</div>
 
 				<div className="row center Team__partners">
-					<div className="small-6 medium-4 large-3 small-centered medium-centered large-centered columns Team__member">
+					<div data-wow-delay="4s" className="wow fadeIn small-6 medium-4 large-3 small-centered medium-centered large-centered columns Team__member">
 						<div className="Team__members__image-wrapper">
 							<a href="https://www.facebook.com/mike.chobanyan.7"><img className="Team__member__img" src="http://kuna.com.ua/wp-content/uploads/2015/12/DSC_3422-240x300.jpg.pagespeed.ce.vQtihTFuzz.jpg" /></a>
 						</div>
@@ -112,7 +113,7 @@ export default class LandingTeam extends React.Component {
 							<li></li>
 						</ul>
 					</div>
-					<div className="small-6 medium-4 large-3 small-centered medium-centered large-centered columns Team__member">
+					<div data-wow-delay="4s" className="wow fadeIn small-6 medium-4 large-3 small-centered medium-centered large-centered columns Team__member">
 						<div className="Team__members__image-wrapper">
 							<a href="http://kuna.com.ua/about/"><img className="Team__member__img" src="http://kuna.com.ua/wp-content/uploads/2014/08/kuna_logo_258x258.png.pagespeed.ce.qhr50_CAH4.png" /></a>
 						</div>

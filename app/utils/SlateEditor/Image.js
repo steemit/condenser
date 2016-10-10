d64c7117dde419e53561e7580dbf09e51fbd7f13
Tim <roadscape@users.noreply.github.com>
2016-10-10 14:10:49 -0400

    image drag-drop #185

---
+++ app/utils/SlateEditor/Image.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+export default class Image extends React.Component {
+    state = {};
+
+    componentDidMount() {
+        console.log("** image mounted..", this.state)
+        const { node } = this.props
+        const { data } = node
+        const file = data.get('file')
+        if(file) this.load(file)
+    }
+
+    load(file) {
+        console.log("** image being loaded.. ----->", file)
+        const reader = new FileReader()
+        reader.addEventListener('load', () => this.setState({ src: reader.result }))
+        reader.readAsDataURL(file)
+    }
+
+    render() {
+        const { node, state, attributes } = this.props
+        let { src } = this.state
+
+        const isFocused = state.selection.hasEdgeIn(node)
+        const className = isFocused ? 'active' : null
+
+        if(src) {
+            console.log("** uploaded image being rendered..", src, state)
+        } else {
+            const src2 = node.data.get('src')
+            console.log("** image source was not in state. data.src = ", src2)
+            src = src2;
+        }
+
+        if(!src) {
+            // src = 'https://img1.steemit.com/0x0/http://ariasprado.name/wp-content/uploads/2012/09/missing-tile-256x256.png'
+            // src = $STM_Config.img_proxy_prefix + '0x0/' + src
+        }
+
+        return src
+          ? <img {...attributes} src={src} className={className} />
+          : <span>Loading... ({src})</span>
+    }
+}

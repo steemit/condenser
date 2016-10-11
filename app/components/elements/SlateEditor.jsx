824311cc92c78466789cbc5a3f2b6845db317ca2
Tim <roadscape@users.noreply.github.com>
2016-10-11 17:24:02 -0400

    link button, block formatting menu, soft returns #185

--- app/components/elements/SlateEditor.jsx
+++ app/components/elements/SlateEditor.jsx
@@ -3,6 +3,8 @@ import { Editor, Mark, Raw, Html } from 'slate'
 import Portal from 'react-portal'
 import position from 'selection-position'
 
+import Icon from 'app/components/elements/Icon';
+
 import demoState from 'app/utils/SlateEditor/DemoState'
 import {HtmlRules, schema, getMarkdownType} from 'app/utils/SlateEditor/Schema'
 
@@ -10,6 +12,8 @@ const serializer = new Html({rules: HtmlRules})
 export const serializeHtml   = (state) => serializer.serialize(state)
 export const deserializeHtml = (html)  => serializer.deserialize(html)
 export const getDemoState    = ()      => Raw.deserialize(demoState, { terse: true })
+
+const DEFAULT_NODE = 'paragraph'
 
 let plugins = []
 
@@ -54,15 +58,34 @@ export default class SlateEditor extends React.Component {
         this.updateMenu()
     }
 
+    onChange = (state) => {
+        this.setState({ state })
+        this.props.onChange(state)
+    }
+
+    // When the portal opens, cache the menu element.
+    onOpen = (portal) => {
+        this.setState({ menu: portal.firstChild })
+    }
+
+
     // Check if the current selection has a mark with `type` in it.
     hasMark = (type) => {
         const { state } = this.state
         return state.marks.some(mark => mark.type == type)
     }
 
-    onChange = (state) => {
-        this.setState({ state })
-        this.props.onChange(state)
+    // Check if the current selection has a block with `type` in it.
+    hasBlock = (type) => {
+        const { state } = this.state
+        const { document } = state
+        return state.blocks.some(node => (node.type == type) || !!document.getClosest(node, parent => parent.type == type) )
+    }
+
+    // Check if the current selection has an inline of `type`.
+    hasInline = (type) => {
+        const { state } = this.state
+        return state.inlines.some(inline => inline.type == type)
     }
 
     // When a mark button is clicked, toggle the current mark.
@@ -78,17 +101,110 @@ export default class SlateEditor extends React.Component {
         this.setState({ state })
     }
 
-    // When the portal opens, cache the menu element.
-    onOpen = (portal) => {
-        this.setState({ menu: portal.firstChild })
+    // Toggle block type
+    onClickBlock = (e, type) => {
+        e.preventDefault()
+        let { state } = this.state
+        let transform = state.transform()
+        const { document } = state
+
+        // Handle everything but list buttons.
+        if (type != 'bulleted-list' && type != 'numbered-list') {
+            const isActive = this.hasBlock(type)
+            const isList = this.hasBlock('list-item')
+
+            if (isList) {
+                transform = transform
+                    .setBlock(isActive ? DEFAULT_NODE : type)
+                    .unwrapBlock('bulleted-list')
+                    .unwrapBlock('numbered-list')
+            }
+
+            else {
+                transform = transform
+                    .setBlock(isActive ? DEFAULT_NODE : type)
+            }
+        }
+
+        // Handle the extra wrapping required for list buttons.
+        else {
+            const isList = this.hasBlock('list-item')
+            const isType = state.blocks.some((block) => {
+                return !!document.getClosest(block, parent => parent.type == type)
+            })
+
+            if (isList && isType) {
+                transform = transform
+                    .setBlock(DEFAULT_NODE)
+                    .unwrapBlock('bulleted-list')
+                    .unwrapBlock('numbered-list')
+            } else if (isList) {
+              transform = transform
+                  .unwrapBlock(type == 'bulleted-list' ? 'numbered-list' : 'bulleted-list')
+                  .wrapBlock(type)
+            } else {
+              transform = transform
+                  .setBlock('list-item')
+                  .wrapBlock(type)
+            }
+        }
+
+        state = transform.apply()
+        this.setState({ state })
     }
+
+    onClickLink = (e) => {
+        e.preventDefault()
+        let { state } = this.state
+        const hasLinks = this.hasInline('link')
+
+        if (hasLinks) {
+console.log(JSON.stringify(Raw.serialize(state, {terse: false})))
+            state = state
+                .transform()
+                .unwrapInline('link')
+                .apply()
+        }
+
+        else if (state.isExpanded) {
+            const href = window.prompt('Enter the URL of the link:')
+            if(href) {
+                state = state
+                    .transform()
+                    .wrapInline({
+                        type: 'link',
+                        data: { href }
+                    })
+                    .collapseToEnd()
+                    .apply()
+            }
+        }
+
+        else {
+          const href = window.prompt('Enter the URL of the link:')
+          const text = window.prompt('Enter the text for the link:')
+          state = state
+              .transform()
+              .insertText(text)
+              .extendBackward(text.length)
+              .wrapInline({
+                  type: 'link',
+                  data: { href }
+              })
+              .collapseToEnd()
+              .apply()
+        }
+console.log(JSON.stringify(Raw.serialize(state, {terse: false})))
+        this.setState({ state })
+    }
+
 
     // Markdown-style quick formatting
     onKeyDown = (e, data, state) => {
         switch (data.key) {
             case 'space': return this.onSpace(e, state)
             case 'backspace': return this.onBackspace(e, state)
-            case 'enter': return this.onEnter(e, state)
+            case 'enter': return data.isShift ? this.onShiftEnter(e, state) : this.onEnter(e, state)
         }
     }
 
@@ -97,20 +213,19 @@ export default class SlateEditor extends React.Component {
         if (state.isExpanded) return
         let { selection } = state
         const { startText, startBlock, startOffset } = state
-        const chars = startBlock.text.slice(0, startOffset).replace(/\s*/g, '')
+        const chars = startBlock.text.slice(0, startOffset)//.replace(/\s*/g, '')
         const type = getMarkdownType(chars)
 
         if (!type) return
-        if (type == 'bulleted-list-item' && startBlock.type == 'bulleted-list-item') return
-        if (type == 'numbered-list-item' && startBlock.type == 'numbered-list-item') return
+        if (type == 'list-item' && startBlock.type == 'list-item') return
         e.preventDefault()
 
         let transform = state
             .transform()
             .setBlock(type)
 
-        if (type == 'bulleted-list-item') transform = transform.wrapBlock('bulleted-list')
-        if (type == 'numbered-list-item') transform = transform.wrapBlock('numbered-list')
+        if (type == 'list-item' && chars != '1.') transform = transform.wrapBlock('bulleted-list')
+        if (type == 'list-item' && chars == '1.') transform = transform.wrapBlock('numbered-list')
 
         state = transform
             .extendToStartOf(startBlock)
@@ -133,17 +248,18 @@ export default class SlateEditor extends React.Component {
             .transform()
             .setBlock('paragraph')
 
-        if (startBlock.type == 'bulleted-list-item') transform = transform.unwrapBlock('bulleted-list')
-        if (startBlock.type == 'numbered-list-item') transform = transform.unwrapBlock('numbered-list')
+        if (startBlock.type == 'list-item')
+            transform = transform
+                .unwrapBlock('bulleted-list')
+                .unwrapBlock('numbered-list')
 
         state = transform.apply()
         return state
     }
 
-    onEnter = (e, state) => {
+    onShiftEnter = (e, state) => {
         if (state.isExpanded) return
         const { startBlock, startOffset, endOffset } = state
-
 
         // Allow soft returns for certain block types
         if (startBlock.type == 'code-block' || startBlock.type == 'block-quote') {
@@ -152,6 +268,11 @@ export default class SlateEditor extends React.Component {
             transform = transform.insertText('\n')
             return transform.apply()
         }
+    }
+
+    onEnter = (e, state) => {
+        if (state.isExpanded) return
+        const { startBlock, startOffset, endOffset } = state
 
         // On return, if at the end of a node type that should not be extended, create a new paragraph below it.
         if (startOffset == 0 && startBlock.length == 0) return this.onBackspace(e, state) //empty block
@@ -162,9 +283,10 @@ export default class SlateEditor extends React.Component {
             startBlock.type != 'heading-two' &&
             startBlock.type != 'heading-three' &&
             startBlock.type != 'heading-four' &&
-            startBlock.type != 'block-quote'
+            startBlock.type != 'block-quote' &&
+            startBlock.type != 'code-block'
         ) return
-        
+
         e.preventDefault()
         return state
             .transform()
@@ -186,10 +308,18 @@ export default class SlateEditor extends React.Component {
     renderMenu = () => {
         const { state } = this.state
         const isOpen = state.isExpanded && state.isFocused
+
         return (
             <Portal isOpened onOpen={this.onOpen}>
-                <div className="SlateEditor__menu SlateEditor__hover-menu">
+                <div className="SlateEditor__menu SlateEditor__menu">
                     {schema.toolbarMarks.map(this.renderMarkButton)}
+                    {this.renderInlineButton({type: 'link', label: <Icon name="link" />})}
+                    {this.renderBlockButton({type: 'block-quote', label: <span>&ldquo;</span>})}
+                    {this.renderBlockButton({type: 'heading-one', label: 'H1'})}
+                    {this.renderBlockButton({type: 'heading-two', label: 'H2'})}
+                    {this.renderBlockButton({type: 'bulleted-list', label: 'ul'})}
+                    {this.renderBlockButton({type: 'numbered-list', label: 'ol'})}
+                    {this.renderBlockButton({type: 'code-block', label: '<>'})}
                 </div>
             </Portal>
         )
@@ -201,7 +331,31 @@ export default class SlateEditor extends React.Component {
         const onMouseDown = e => this.onClickMark(e, type)
 
         return (
-            <span key={type} className="SlateEditor__menu-button" onMouseDown={onMouseDown} data-active={isActive}>
+            <span key={type} className={'SlateEditor__menu-button SlateEditor__menu-button-'+type} onMouseDown={onMouseDown} data-active={isActive}>
+                <span>{label}</span>
+            </span>
+        )
+    }
+
+    renderBlockButton = (props) => {
+        const {type, label} = props
+        const isActive = this.hasBlock(type)
+        const onMouseDown = e => this.onClickBlock(e, type)
+
+        return (
+            <span key={type} className={'SlateEditor__menu-button SlateEditor__menu-button-'+type} onMouseDown={onMouseDown} data-active={isActive}>
+                <span>{label}</span>
+            </span>
+        )
+    }
+
+    renderInlineButton = (props) => {
+        const {type, label} = props
+        const isActive = this.hasInline(type)
+        const onMouseDown = e => this.onClickLink(e, type)
+
+        return (
+            <span key={type} className={'SlateEditor__menu-button SlateEditor__menu-button-'+type} onMouseDown={onMouseDown} data-active={isActive}>
                 <span>{label}</span>
             </span>
         )

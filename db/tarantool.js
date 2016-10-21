168f20c9c76e70b0244ab8ad351aaed549282d7e
valzav <i@valzav.com>
2016-10-21 13:22:13 -0400

    refactor tarantool connector class

--- db/tarantool.js
+++ db/tarantool.js
@@ -1,10 +1,59 @@
-import Tarantool from 'tarantool-driver';
+import TarantoolDriver from 'tarantool-driver';
 
-const tarantool = new Tarantool({port: 3313});
-tarantool.connect()
-.then(() => tarantool.auth('guest', ''))
-.catch(error => {
-    console.error('failed to connect to tarantool server:', error);
-});
+let instance = null;
 
-export default tarantool;
+class Tarantool {
+    constructor() {
+        console.log('-- Tarantool.constructor -->');
+        const connection = this.connection = new TarantoolDriver({port: 3313});
+        this.ready_promise = new Promise((resolve, reject) => {
+            connection.connect()
+            .then(() => connection.auth('guest', ''))
+            .then(() => resolve())
+            .catch(error => reject(error));
+        });
+    }
+
+    makeCall(call_name, args) {
+        return this.ready_promise
+            .then(() => this.connection[call_name].apply(this.connection, args))
+            .catch(error => {
+                console.error('Tarantool error', error.message);
+                if (error.message.indexOf('connection') >= 0)
+                    instance = null;
+                return Promise.reject(error);
+            });
+    }
+
+    select() {
+       return this.makeCall('select', arguments);
+    }
+    delete() {
+        return this.makeCall('delete', arguments);
+    }
+    insert() {
+        return this.makeCall('insert', arguments);
+    }
+    replace() {
+        return this.makeCall('replace', arguments);
+    }
+    update() {
+        return this.makeCall('update', arguments);
+    }
+    eval() {
+        return this.makeCall('eval', arguments);
+    }
+    call() {
+        return this.makeCall('call', arguments);
+    }
+    upsert() {
+        return this.makeCall('upsert', arguments);
+    }
+}
+
+Tarantool.instance = function () {
+    if (!instance) instance = new Tarantool();
+    return instance;
+};
+
+export default Tarantool;
--- server/api/notifications.js
+++ server/api/notifications.js
@@ -1,6 +1,6 @@
 import koa_router from 'koa-router';
 import koa_body from 'koa-body';
-import tarantool from 'db/tarantool';
+import Tarantool from 'db/tarantool';
 
 function toResArray(result) {
     if (!result || result.length < 1) return null;
@@ -11,25 +11,36 @@ export default function useNotificationsApi(app) {
     const router = koa_router({prefix: '/api/v1'});
     app.use(router.routes());
 
-    // get all account's notifications
+    // get all notifications for account
     router.get('/notifications/:account', function *() {
         const account = this.params.account;
         // TODO: make sure account name matches session
-        console.log('-- account -->', account);
-        const res = yield tarantool.select('notifications', 0, 1, 0, 'eq', account);
-        this.body = toResArray(res);
+        console.log('-- GET /notifications/:account -->', account);
+        try {
+            const res = yield Tarantool.instance().select('notifications', 0, 1, 0, 'eq', account);
+            console.log('-- res -->', res);
+            this.body = toResArray(res);
+        } catch (error) {
+            console.error('-- /notifications/:account error -->', error.message);
+            this.body = null;
+        }
         return;
     });
 
-    // mark notification as read
+    // mark account's notification as read
     router.put('/notifications/:account/:id', function *() {
         const {account, id} = this.params;
         if (!id || id < 0) {
-            body = ''; return;
+            body = null; return;
         }
-        console.log('-- account put -->', account, id);
-        const res = yield tarantool.call('notification_read', account, id);
-        this.body = toResArray(res);
+        console.log('-- PUT /notifications/:account/:id -->', account, id);
+        try {
+            const res = yield Tarantool.instance().call('notification_read', account, id);
+            this.body = toResArray(res);
+        } catch (error) {
+            console.error('-- /notifications/:account/:id error -->', error.message);
+            this.body = null;
+        }
         return;
     });
 }

c6ff6ab9db6cb1ac8343e12cd89c933fb6ba67bc
James Calfee <james@jcalfee.info>
2016-11-01 13:04:44 -0500

    Null safe code when private_keys is null (can happen after a single transaction signature) (close #238)

--- app/redux/AuthSaga.js
+++ app/redux/AuthSaga.js
@@ -107,9 +107,13 @@ export function* findSigningKey({opType, username, password}) {
 
     const currentUser = yield select(state => state.user.get('current'))
     const currentUsername = currentUser && currentUser.get('username')
+
     username = username || currentUsername
     if (!username) return null
+
     const private_keys = currentUsername === username ? currentUser.get('private_keys') : Map()
+    if(!private_keys) return null
+
     let account = yield select(state => state.global.getIn(['accounts', username]))
     if (!account) {
         [account] = yield call(Apis.db_api, 'get_accounts', [username])

96ae5a09d14074d5ad85bbd3a1c9a12330f015f8
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2016-11-01 13:08:00 +0200

    test mode artfacts removal

--- shared/api_client/ChainConfig.js
+++ shared/api_client/ChainConfig.js
@@ -4,7 +4,7 @@ ecc_config.address_prefix = "GLS";
 
 // let chain_id = ""
 // for(let i = 0; i < 32; i++) chain_id += "00"
-let chain_id = "8ec6e00cefb9b8b2d483215240d3eb40e477e3c1e38a1fe032673758c98db3fe"
+let chain_id = "782a3039b478c839e4cb0c941ff4eaeb7df40bdd68bd441afd444b9da763de12"
 
 
 module.exports = {

eeda30792aecd3bd04d69b2e8a3638c3b4406cf2
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2016-11-01 13:06:01 +0200

    test mode artfacts removal

--- build_n_run_webclient.sh
+++ build_n_run_webclient.sh
@@ -2,11 +2,10 @@
 git status
 echo "commit hash#"$(git rev-parse --verify HEAD)"\n\n"
 echo "{\"githash\":\""$(git rev-parse --verify HEAD)"\"}" > config/last-build.json
-npm install && npm run build 
-if [ $(screen -ls | grep 'test_webclient' | awk '{print $1}') ]
-then 
-  screen -X -S $(screen -ls | grep 'test_webclient' | awk '{print $1}') quit
+npm install && npm run build
+if [ $(screen -ls | grep 'webclient' | awk '{print $1}') ]
+then
+  screen -X -S $(screen -ls | grep 'webclient' | awk '{print $1}') quit
 fi
 sleep 5
-screen -dmSL test_webclient npm run prod
-
+screen -dmSL webclient npm run prod

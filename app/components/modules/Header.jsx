2fd0a6e25287be5b1e37cb7f38568d1286e2a8b6
Mike Cifani <mcifani@gmail.com>
2016-11-01 10:52:11 -0400

    return to original logo theme/emblem from halloween

--- app/components/modules/Header.jsx
+++ app/components/modules/Header.jsx
@@ -187,10 +187,10 @@ class Header extends React.Component {
                             <ul className="menu">
                                 <li className="Header__top-logo">
                                     <Link to={logo_link}>
-                                        <Icon name="halloween" size="2x" />
+                                        <Icon name="steem" size="2x" />
                                     </Link>
                                 </li>
-                                <li className="Header__top-steemit show-for-medium noPrint"><Link to={logo_link} className="logo"><img src={require('app/assets/images/steemit-halloween.png')} style={{width: 80, height: 35}} /><span className="beta">beta</span></Link></li>
+                                <li className="Header__top-steemit show-for-medium noPrint"><Link to={logo_link}>steemit<span className="beta">beta</span></Link></li>
                                 {(topic_link || user_name || page_name) && <li className="delim show-for-medium">|</li>}
                                 {topic_link && <li className="Header__top-topic">{topic_link}</li>}
                                 {user_name && <li><Link to={`/@${user_name}`}>{user_name}</Link></li>}

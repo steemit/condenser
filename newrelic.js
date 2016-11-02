ae0cdadbb5fa3fb0130104a7cf0fcaf70e3c636e
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2016-11-02 18:07:36 +0200

    better newrelic usage, safely loading from config

---
+++ newrelic.js
@@ -0,0 +1,28 @@
+'use strict'
+import config from './config'
+const newrelic = config.newrelic;
+
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: newrelic.app_name || ['Golos.io'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: newrelic.license_key,
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: newrelic.log_level || 'info'
+  }
+}

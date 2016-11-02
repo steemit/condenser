ce88bcb31c1cb6cf8ae634f905f49061880035c8
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2016-11-02 17:13:12 +0200

    extended config so if it exposes PORT (must be number) and CHAIN_ID (must be string) values, these values are used. if these are not present (or boolean false, i.e. 0 and empty string) - defaults of port 3002 and chain_id of GOLOS production network are used

--- shared/api_client/ChainConfig.js
+++ shared/api_client/ChainConfig.js
@@ -1,11 +1,11 @@
 import { ecc_config, hash } from "../ecc"
-
+import config from "../../config"
 ecc_config.address_prefix = "GLS";
 
 // let chain_id = ""
 // for(let i = 0; i < 32; i++) chain_id += "00"
-let chain_id = "782a3039b478c839e4cb0c941ff4eaeb7df40bdd68bd441afd444b9da763de12"
-
+let chain_id = config.CHAIN_ID || "782a3039b478c839e4cb0c941ff4eaeb7df40bdd68bd441afd444b9da763de12"
+console.log (chain_id, "CHAIN_ID")
 
 module.exports = {
     address_prefix: "GLS",

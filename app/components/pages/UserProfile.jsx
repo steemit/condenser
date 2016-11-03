a6322a1fe487da4c01db241293c267966957f783
originate <mcifani@gmail.com>
2016-11-03 14:10:07 -0400

    display new date join wrapper element join date on profile page

--- app/components/pages/UserProfile.jsx
+++ app/components/pages/UserProfile.jsx
@@ -20,6 +20,7 @@ import {repLog10} from 'app/utils/ParsersAndFormatters.js';
 import Tooltip from 'app/components/elements/Tooltip';
 import { LinkWithDropdown } from 'react-foundation-components/lib/global/dropdown';
 import VerticalMenu from 'app/components/elements/VerticalMenu';
+import DateJoinWrapper from 'app/components/elements/DateJoinWrapper'
 
 export default class UserProfile extends React.Component {
     constructor() {
@@ -233,6 +234,9 @@ export default class UserProfile extends React.Component {
             {link: `/@${accountname}/author-rewards`, label: "Author rewards", value: "Author rewards"}
         ];
 
+        // set account join date
+        let accountjoin = account.created;
+
         const top_menu = <div className="row UserProfile__top-menu">
             <div className="columns small-10 medium-12 medium-expand">
                 <ul className="menu" style={{flexWrap: "wrap"}}>
@@ -279,7 +283,6 @@ export default class UserProfile extends React.Component {
                             </div>
                         </div>
                         <h2>{account.name} <Tooltip t={`This is ${accountname}'s reputation score.\n\nThe reputation score is based on the history of votes received by the account, and is used to hide low quality content.`}><span style={{fontSize: "80%"}}>({rep})</span></Tooltip></h2>
-
                         <div>
                             <div className="UserProfile__stats">
                                 <span><Link to={`/@${accountname}/followers`}>{followerCount} followers</Link></span>
@@ -287,6 +290,7 @@ export default class UserProfile extends React.Component {
                                 <span><Link to={`/@${accountname}/followed`}>{followingCount} followed</Link></span>
                             </div>
                         </div>
+                        <DateJoinWrapper date={accountjoin}></DateJoinWrapper>
                     </div>
                 </div>
                 <div className="UserProfile__top-nav row expanded noPrint">

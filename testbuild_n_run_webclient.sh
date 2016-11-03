04e81ea725f8a6515ef4f76dc6f6b4f4a051a0f9
ValeryLitvin <litvintech@gmail.com>
2016-11-03 09:40:22 +0000

    cloned buil'n'run script

---
+++ testbuild_n_run_webclient.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+git status
+echo "commit hash#"$(git rev-parse --verify HEAD)"\n\n"
+echo "{\"githash\":\""$(git rev-parse --verify HEAD)"\"}" > config/last-build.json
+npm install && npm run build
+if [ $(screen -ls | grep 'test_webclient' | awk '{print $1}') ]
+then
+  screen -X -S $(screen -ls | grep 'test_webclient' | awk '{print $1}') quit
+fi
+sleep 5
+screen -dmS test_webclient npm run prod

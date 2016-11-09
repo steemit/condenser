7ea5b1e849277cc63b09117e2b91be04daa9b775
originate <mcifani@gmail.com>
2016-11-08 19:35:58 -0500

    remove harded coded secondary message, replaced by dynamic value

--- app/utils/Tips.js
+++ app/utils/Tips.js
@@ -1,5 +1,4 @@
 export const steemTip = 'Tradeable tokens that may be transferred anywhere at anytime. Steem can be converted to Steem Power in a process called powering up.'
-export const dollarTip = 'Tokens worth about $1.00 of STEEM, currently collecting 10% APR.'
 export const powerTip = 'Influence tokens which give you more control over post payouts and allow you to earn on curation rewards.'
 export const valueTip = 'The estimated value is based on a 7 day average value of Steem in US Dollars.'
 export const powerTip2 = 'Steem Power is non-transferrable and will require 2 years and 104 payments to convert back to Steem.'

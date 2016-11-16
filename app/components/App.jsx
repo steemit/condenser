cdebc788e9498c4ac7db7de9d0a6c5081953b232
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2016-11-16 23:49:11 +0200

    жужль анал итикс пиксиль

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -38,6 +38,14 @@ class App extends React.Component {
             analytics.load(SEGMENT_ANALYTICS_KEY);
             analytics.page()
             }}();
+
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-49238979-12', 'auto');
+            ga('send', 'pageview');
         }
     }
 

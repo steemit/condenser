d8f3824a7920be0bd89131b6ca0c1e443ffebbc9
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2016-11-17 00:08:53 +0200

    фейсбук аналитикс пиксель

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -46,6 +46,15 @@ class App extends React.Component {
 
             ga('create', 'UA-49238979-12', 'auto');
             ga('send', 'pageview');
+
+            !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+            n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+            document,'script','https://connect.facebook.net/en_US/fbevents.js');
+
+            fbq('init', '1594659427507927');
+            fbq('track', "PageView");
         }
     }
 

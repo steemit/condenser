498135023bf6079c1f86d5435d657c0503e74d69
Tim <roadscape@users.noreply.github.com>
2016-11-28 17:18:39 -0500

    restore auto-closing of menu, with some exceptions. #722

--- app/components/elements/VerticalMenu.jsx
+++ app/components/elements/VerticalMenu.jsx
@@ -13,13 +13,21 @@ export default class VerticalMenu extends React.Component {
         ]),
     };
 
+    closeMenu = (e) => {
+        // If this was not a left click, or if CTRL or CMD were held, do not close the menu.
+        if(e.button !== 0 || e.ctrlKey || e.metaKey) return;
+
+        // Simulate clicking of document body which will close any open menus
+        document.body.click();
+    }
+
     render() {
         const {items, title, className, hideValue} = this.props;
         return <ul className={'VerticalMenu menu vertical' + (className ? ' ' + className : '')}>
             {title && <li className="title">{title}</li>}
             {items.map(i => {
                 if(i.value === hideValue) return null
-                return <li key={i.value}>
+                return <li key={i.value} onClick={this.closeMenu}>
                     {i.link ? <Link to={i.link} onClick={i.onClick}>
                         {i.icon && <Icon name={i.icon} />}{i.label ? i.label : i.value}
                         &nbsp; {i.addon}

e31452de013dc4f3d6808bb3f85b2a28293489d1
Mihail Paley <undeadlol1@mail.ru>
2016-12-15 12:50:01 +0500

    (Header) fix topic link

--- app/components/modules/Header.jsx
+++ app/components/modules/Header.jsx
@@ -165,7 +165,7 @@ class Header extends React.Component {
         if (process.env.BROWSER && route.page !== 'Post') document.title = page_title + ' — ' + APP_NAME;
 
         const logo_link = route.params && route.params.length > 1 && this.last_sort_order ? '/' + this.last_sort_order : '/hot';
-        let topic_link = topic ? <Link to={`/${this.last_sort_order || 'hot'}/${topic}`}>{detransliterate(topic)}</Link> : null;
+        let topic_link = topic ? <Link to={`/${this.last_sort_order || 'hot'}/${topic_original_link}`}>{detransliterate(topic)}</Link> : null;
 
         const sort_orders = [
             ['created', translate('new')],

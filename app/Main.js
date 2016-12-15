ec339578d3e6601655e88a6d9081ab2460b64bbe
valzav <i@valzav.com>
2016-12-15 16:25:52 -0500

    use local intl polyfill

--- app/Main.js
+++ app/Main.js
@@ -2,17 +2,25 @@ import 'babel-core/register';
 import 'babel-polyfill';
 import 'whatwg-fetch';
 import './assets/stylesheets/app.scss';
-
 import Iso from 'iso';
 import universalRender from 'shared/UniversalRender';
 import ConsoleExports from './utils/ConsoleExports';
 import {serverApiRecordEvent} from 'app/utils/ServerApiClient';
 
 window.onerror = error => {
-    serverApiRecordEvent('client_error', error);
+    if (window.$STM_csrf) serverApiRecordEvent('client_error', error);
 };
 
-Iso.bootstrap(initial_state => {
+try {
+    if(process.env.NODE_ENV === 'development') {
+        // Adds some object refs to the global window object
+        ConsoleExports.init(window)
+    }
+} catch (e) {
+    console.error(e)
+}
+
+function runApp(initial_state) {
     console.log('Initial state', initial_state);
     window.$STM_Config = initial_state.offchain.config;
     if (initial_state.offchain.serverBusy) {
@@ -28,13 +36,17 @@ Iso.bootstrap(initial_state => {
         console.error(error);
         serverApiRecordEvent('client_error', error);
     });
-});
-
-try {
-    if(process.env.NODE_ENV === 'development') {
-        // Adds some object refs to the global window object
-        ConsoleExports.init(window)
-    }
-} catch (e) {
-    console.error(e)
 }
+
+if (!window.Intl) {
+    require.ensure(['intl/dist/Intl'], (require) => {
+        window.IntlPolyfill = window.Intl = require('intl/dist/Intl')
+        require('intl/locale-data/jsonp/en-US.js')
+        Iso.bootstrap(runApp);
+    }, "IntlBundle");
+}
+else {
+    Iso.bootstrap(runApp);
+}
+
+

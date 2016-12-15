75b9198e10515b599023d242b610a5c165164353
James Calfee <james@jcalfee.info>
2016-12-15 13:18:54 -0600

    Use case insensitive version. Revert "Shorter tag search syntax. #827"
    This reverts commit e03cc1e0522cbb7c56f13437bf6eaa4cee139b33.

--- app/utils/StateFunctions.js
+++ app/utils/StateFunctions.js
@@ -85,7 +85,7 @@ export function contentStats(content) {
         tags = []
     }
     tags.push(content.get('category'))
-    const isNsfw = tags.includes('nsfw');
+    const isNsfw = tags.filter(tag => tag.match(/^nsfw$/i)).length > 0;
 
     return {hide, gray, pictures, netVoteSign, hasPendingPayout, authorRepLog10, hasReplies, hasFlag, isNsfw}
 }

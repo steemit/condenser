fb8bb7aad4b357b11dbf0cf6ee4c4e6723210527
Valentine Zavgorodnev <i@valzav.com>
2017-01-18 16:47:04 -0500

    add profile pics support to notifications (#1017)
    * add profile pics support to notifications
    
    * make default value valid

--- scripts/webpush_notify.js
+++ scripts/webpush_notify.js
@@ -4,12 +4,12 @@ import Tarantool from '../db/tarantool';
 
 webPush.setGCMAPIKey(config.gcm_key);
 
-function notify(account, nparams, title, body, url) {
+function notify(account, nparams, title, body, url, pic) {
      var payload = JSON.stringify({
         title,
         body,
         url,
-        icon: 'https://steemit.com/favicon.ico'
+        icon: pic || 'https://steemit.com/favicon.ico'
     });
     return new Promise((resolve, reject) => {
         webPush.sendNotification(nparams, payload).then(function() {
@@ -26,11 +26,11 @@ async function process_queue() {
         const queue = await Tarantool.instance().call('webpush_get_delivery_queue');
         for (const n of queue) {
             if (n.length === 0) return;
-            const [account, nparams_array, title, body, url] = n;
-            console.log('-- notification -->', account, body, url);
+            const [account, nparams_array, title, body, url, pic] = n;
+            console.log('-- notification -->', account, body, url, pic);
             for (const nparams of nparams_array) {
                 try {
-                    await notify(account, nparams, title, body, url);
+                    await notify(account, nparams, title, body, url, pic);
                 } catch (err) {
                     console.error('-- error in notify -->', account, err);
                 }

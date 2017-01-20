532f15141412d3ddc4c74313cefaaa983fab2e0e
Tim <roadscape@users.noreply.github.com>
2017-01-20 15:10:28 -0500

    clean up post route: first case was unused, and 2nd-3rd can be combined. #1040

--- app/ResolveRoute.js
+++ app/ResolveRoute.js
@@ -6,6 +6,7 @@ export const routeRegex = {
     UserEndPoints: /^(blog|posts|comments|recommended|transfers|curation-rewards|author-rewards|permissions|created|recent-replies|feed|password|followed|followers|settings)$/,
     CategoryFilters: /^\/(hot|created|trending|active|promoted)\/?$/ig,
     PostNoCategory: /^\/(@[\w\.\d-]+)\/([\w\d-]+)/,
+    Post: /^\/([\w\d\-\/]+)\/(\@[\w\d\.-]+)\/([\w\d-]+)\/?($|\?)/,
     UserJson: /^\/(@[\w\.\d-]+)(\/json)$/
 }
 
@@ -79,9 +80,7 @@ export default function resolveRoute(path)
     if (match) {
         return {page: 'PostNoCategory', params: match.slice(1)};
     }
-    match = path.match(/^\/(\@[\w\d-]+)\/([\w\d-]+)\/?$/) ||
-        path.match(/^\/([\w\d\-\/]+)\/(\@[\w\d\.-]+)\/([\w\d-]+)\/?$/) ||
-        path.match(/^\/([\w\d\-\/]+)\/(\@[\w\d\.-]+)\/([\w\d-]+)\/?\?sort=(\w+)$/);
+    match = path.match(routeRegex.Post);
     if (match) {
         return {page: 'Post', params: match.slice(1)};
     }

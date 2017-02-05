c189787319d7d5ec1373580b5e623fd55f5d56c3
Tim Fesenko <roadscape@users.noreply.github.com>
2017-02-04 19:16:02 -0500

    Adjust author heading to use horizontal space instead of two lines (#1101)
    * make author heading single line, adjust font, fix spacings
    
    * eslint

--- app/components/elements/TimeAgoWrapper.js
+++ app/components/elements/TimeAgoWrapper.js
@@ -5,11 +5,11 @@ import Tooltip from 'app/components/elements/Tooltip';
 
 export default class TimeAgoWrapper extends React.Component {
     render() {
-        let {date} = this.props
+        let {date, className} = this.props
         if(date && /^\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d$/.test(date)) {
             date = date + 'Z' // Firefox really wants this Z (Zulu)
         }
-        return <Tooltip t={new Date(date).toLocaleString()}>
+        return <Tooltip t={new Date(date).toLocaleString()} className={className}>
                   <FormattedRelative {...this.props} value={date} />
                </Tooltip>
     }

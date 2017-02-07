bb7ff53e3970b7f3727552339ba99712f6b71a7e
pavelit <info@simdo.ru>
2017-02-07 23:52:07 +0300

    loc Footer

--- app/components/modules/Footer.jsx
+++ app/components/modules/Footer.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import { Link } from 'react-router';
 import {connect} from 'react-redux';
 import { translate } from 'app/Translator';
+import { TERMS_OF_SERVICE_URL, PRIVACY_POLICY_URL } from 'config/client_config';
 
 const Footer = props => (
     <footer className="Footer row expanded">
         <div className="large-6 columns">
             <ul className="menu">
                <li><Link to="/about.html">{translate('about')}</Link></li>
-               <li><Link to="/privacy.html" rel="nofollow">{translate('privacy_policy')}</Link></li>
-               <li><Link to="/tos.html" rel="nofollow">{translate('terms_of_service')}</Link></li>
+               <li><Link to={PRIVACY_POLICY_URL} rel="nofollow">{translate('privacy_policy')}</Link></li>
+               <li><Link to={TERMS_OF_SERVICE_URL} rel="nofollow">{translate('terms_of_service')}</Link></li>
                <li><Link to="/~witnesses">{translate('witnesses')}</Link></li>
             </ul>
         </div>

9cce44717c5d2edac69788dcf0ab732577d87f89
Jeffrey Paul <jp@eeqj.com>
2017-02-13 11:52:47 -0500

    updated `prettier` output to use single quotes

--- server/server.js
+++ server/server.js
@@ -1,67 +1,67 @@
 // newrelic is not working with latest npm
 //if(config.has('newrelic')) require('newrelic');
 
-import path from "path";
-import fs from "fs";
-import Koa from "koa";
-import mount from "koa-mount";
-import helmet from "koa-helmet";
-import koa_logger from "koa-logger";
-import prod_logger from "./prod_logger";
-import favicon from "koa-favicon";
-import staticCache from "koa-static-cache";
-import useRedirects from "./redirects";
-import useOauthLogin from "./api/oauth";
-import useGeneralApi from "./api/general";
-import useAccountRecoveryApi from "./api/account_recovery";
-import useNotificationsApi from "./api/notifications";
-import useEnterAndConfirmEmailPages from "./server_pages/enter_confirm_email";
-import useEnterAndConfirmMobilePages from "./server_pages/enter_confirm_mobile";
-import useUserJson from "./json/user_json";
-import isBot from "koa-isbot";
-import session from "@steem/crypto-session";
-import csrf from "koa-csrf";
-import flash from "koa-flash";
-import minimist from "minimist";
-import Grant from "grant-koa";
-import config from "config";
-import { routeRegex } from "app/ResolveRoute";
-import secureRandom from "secure-random";
+import path from 'path';
+import fs from 'fs';
+import Koa from 'koa';
+import mount from 'koa-mount';
+import helmet from 'koa-helmet';
+import koa_logger from 'koa-logger';
+import prod_logger from './prod_logger';
+import favicon from 'koa-favicon';
+import staticCache from 'koa-static-cache';
+import useRedirects from './redirects';
+import useOauthLogin from './api/oauth';
+import useGeneralApi from './api/general';
+import useAccountRecoveryApi from './api/account_recovery';
+import useNotificationsApi from './api/notifications';
+import useEnterAndConfirmEmailPages from './server_pages/enter_confirm_email';
+import useEnterAndConfirmMobilePages from './server_pages/enter_confirm_mobile';
+import useUserJson from './json/user_json';
+import isBot from 'koa-isbot';
+import session from '@steem/crypto-session';
+import csrf from 'koa-csrf';
+import flash from 'koa-flash';
+import minimist from 'minimist';
+import Grant from 'grant-koa';
+import config from 'config';
+import { routeRegex } from 'app/ResolveRoute';
+import secureRandom from 'secure-random';
 
-console.log("application server starting, please wait.");
+console.log('application server starting, please wait.');
 
 const grant = new Grant(config.grant);
 // import uploadImage from 'server/upload-image' //medium-editor
 
 const app = new Koa();
-app.name = "Steemit app";
-const env = process.env.NODE_ENV || "development";
+app.name = 'Steemit app';
+const env = process.env.NODE_ENV || 'development';
 // cache of a thousand days
 const cacheOpts = { maxAge: 86400000, gzip: true };
 
-app.keys = [config.get("session_key")];
+app.keys = [config.get('session_key')];
 
-const crypto_key = config.get("server_session_secret");
+const crypto_key = config.get('server_session_secret');
 session(app, {
     maxAge: 1000 * 3600 * 24 * 60,
     crypto_key,
-    key: config.get("session_cookie_key")
+    key: config.get('session_cookie_key')
 });
 
 app.use(mount(grant));
-app.use(flash({ key: "flash" }));
+app.use(flash({ key: 'flash' }));
 
 // some redirects
 app.use(function*(next) {
     // redirect to home page/feed if known account
-    if (this.method === "GET" && this.url === "/" && this.session.a) {
+    if (this.method === 'GET' && this.url === '/' && this.session.a) {
         this.status = 302;
         this.redirect(`/@${this.session.a}/feed`);
         return;
     }
     // normalize user name url from cased params
     if (
-        this.method === "GET" &&
+        this.method === 'GET' &&
             (routeRegex.UserProfile1.test(this.url) ||
                 routeRegex.PostNoCategory.test(this.url))
     ) {
@@ -73,7 +73,7 @@ app.use(function*(next) {
         }
     }
     // normalize top category filtering from cased params
-    if (this.method === "GET" && routeRegex.CategoryFilters.test(this.url)) {
+    if (this.method === 'GET' && routeRegex.CategoryFilters.test(this.url)) {
         const p = this.originalUrl.toLowerCase();
         if (p !== this.originalUrl) {
             this.status = 301;
@@ -82,20 +82,20 @@ app.use(function*(next) {
         }
     }
     // start registration process if user get to create_account page and has no id in session yet
-    if (this.url === "/create_account" && !this.session.user) {
+    if (this.url === '/create_account' && !this.session.user) {
         this.status = 302;
-        this.redirect("/enter_email");
+        this.redirect('/enter_email');
         return;
     }
     // remember ch, cn, r url params in the session and remove them from url
-    if (this.method === "GET" && /\?[^\w]*(ch=|cn=|r=)/.test(this.url)) {
+    if (this.method === 'GET' && /\?[^\w]*(ch=|cn=|r=)/.test(this.url)) {
         let redir = this.url.replace(/((ch|cn|r)=[^&]+)/gi, r => {
-            const p = r.split("=");
+            const p = r.split('=');
             if (p.length === 2) this.session[p[0]] = p[1];
-            return "";
+            return '';
         });
-        redir = redir.replace(/&&&?/, "");
-        redir = redir.replace(/\?&?$/, "");
+        redir = redir.replace(/&&&?/, '');
+        redir = redir.replace(/\?&?$/, '');
         console.log(`server redirect ${this.url} -> ${redir}`);
         this.status = 302;
         this.redirect(redir);
@@ -105,14 +105,14 @@ app.use(function*(next) {
 });
 
 // load production middleware
-if (env === "production") {
-    app.use(require("koa-conditional-get")());
-    app.use(require("koa-etag")());
-    app.use(require("koa-compressor")());
+if (env === 'production') {
+    app.use(require('koa-conditional-get')());
+    app.use(require('koa-etag')());
+    app.use(require('koa-compressor')());
 }
 
 // Logging
-if (env === "production") {
+if (env === 'production') {
     app.use(prod_logger());
 } else {
     app.use(koa_logger());
@@ -122,31 +122,31 @@ app.use(helmet());
 
 app.use(
     mount(
-        "/static",
-        staticCache(path.join(__dirname, "../app/assets/static"), cacheOpts)
+        '/static',
+        staticCache(path.join(__dirname, '../app/assets/static'), cacheOpts)
     )
 );
 
 app.use(
-    mount("/robots.txt", function*() {
-        this.set("Cache-Control", "public, max-age=86400000");
-        this.type = "text/plain";
-        this.body = "User-agent: *\nAllow: /";
+    mount('/robots.txt', function*() {
+        this.set('Cache-Control', 'public, max-age=86400000');
+        this.type = 'text/plain';
+        this.body = 'User-agent: *\nAllow: /';
     })
 );
 
 app.use(
-    mount("/service-worker.js", function*() {
-        this.set("Cache-Control", "public, max-age=7200000");
-        this.type = "application/javascript";
+    mount('/service-worker.js', function*() {
+        this.set('Cache-Control', 'public, max-age=7200000');
+        this.type = 'application/javascript';
         const file_content = fs
-            .readFileSync(path.join(__dirname, "./service-worker.js"))
+            .readFileSync(path.join(__dirname, './service-worker.js'))
             .toString();
         // TODO: use APP_URL from client_config.js
         // actually use a config value for it
         this.body = file_content.replace(
             /\{DEFAULT_URL\}/i,
-            "https://" + this.request.header.host
+            'https://' + this.request.header.host
         );
     })
 );
@@ -157,7 +157,7 @@ app.use(function*(next) {
     const last_visit = this.session.last_visit;
     this.session.last_visit = new Date().getTime() / 1000 | 0;
     if (!this.session.uid) {
-        this.session.uid = secureRandom.randomBuffer(13).toString("hex");
+        this.session.uid = secureRandom.randomBuffer(13).toString('hex');
         this.session.new_visit = true;
     } else {
         this.session.new_visit = this.session.last_visit - last_visit > 1800;
@@ -176,77 +176,77 @@ useNotificationsApi(app);
 
 // helmet wants some things as bools and some as lists, makes config difficult.
 // our config uses strings, this splits them to lists on whitespace.
-if (env === "production") {
+if (env === 'production') {
     const helmetConfig = {
         directives: {
             defaultSrc: config
-                .get("helmet.directives.defaultSrc")
-                .split("/\s+/"),
-            childSrc: config.get("helmet.directives.childSrc").split("/\s+/"),
-            scriptSrc: config.get("helmet.directives.scriptSrc").split("/\s+/"),
-            styleSrc: config.get("helmet.directives.styleSrc").split("/\s+/"),
-            imgSrc: config.get("helmet.directives.imgSrc").split("/\s+/"),
-            fontSrc: config.get("helmet.directives.fontSrc").split("/\s+/"),
+                .get('helmet.directives.defaultSrc')
+                .split('/\s+/'),
+            childSrc: config.get('helmet.directives.childSrc').split('/\s+/'),
+            scriptSrc: config.get('helmet.directives.scriptSrc').split('/\s+/'),
+            styleSrc: config.get('helmet.directives.styleSrc').split('/\s+/'),
+            imgSrc: config.get('helmet.directives.imgSrc').split('/\s+/'),
+            fontSrc: config.get('helmet.directives.fontSrc').split('/\s+/'),
             connectSrc: config
-                .get("helmet.directives.connectSrc")
-                .split("/\s+/"),
-            reportUri: config.get("helmet.directives.reportUri"),
+                .get('helmet.directives.connectSrc')
+                .split('/\s+/'),
+            reportUri: config.get('helmet.directives.reportUri'),
             pluginTypes: config
-                .get("helmet.directives.pluginTypes")
-                .split("/\s+/"),
-            objectSrc: config.get("helmet.directives.objectSrc").split("/\s+/"),
+                .get('helmet.directives.pluginTypes')
+                .split('/\s+/'),
+            objectSrc: config.get('helmet.directives.objectSrc').split('/\s+/'),
             frameAncestors: config
-                .get("helmet.directives.frameAncestors")
-                .split("/\s+/")
+                .get('helmet.directives.frameAncestors')
+                .split('/\s+/')
         },
-        reportOnly: config.get("helmet.reportOnly"),
-        setAllHeaders: config.get("helmet.setAllHeaders")
+        reportOnly: config.get('helmet.reportOnly'),
+        setAllHeaders: config.get('helmet.setAllHeaders')
     };
     app.use(helmet.contentSecurityPolicy(helmetConfig));
 }
 
 app.use(
-    favicon(path.join(__dirname, "../app/assets/images/favicons/favicon.ico"))
+    favicon(path.join(__dirname, '../app/assets/images/favicons/favicon.ico'))
 );
 app.use(isBot());
 app.use(
     mount(
-        "/favicons",
+        '/favicons',
         staticCache(
-            path.join(__dirname, "../app/assets/images/favicons"),
+            path.join(__dirname, '../app/assets/images/favicons'),
             cacheOpts
         )
     )
 );
 app.use(
     mount(
-        "/images",
-        staticCache(path.join(__dirname, "../app/assets/images"), cacheOpts)
+        '/images',
+        staticCache(path.join(__dirname, '../app/assets/images'), cacheOpts)
     )
 );
 // Proxy asset folder to webpack development server in development mode
-if (env === "development") {
+if (env === 'development') {
     const webpack_dev_port = process.env.PORT
         ? parseInt(process.env.PORT) + 1
         : 8081;
-    const proxyhost = "http://0.0.0.0:" + webpack_dev_port;
-    console.log("proxying to webpack dev server at " + proxyhost);
-    const proxy = require("koa-proxy")({
+    const proxyhost = 'http://0.0.0.0:' + webpack_dev_port;
+    console.log('proxying to webpack dev server at ' + proxyhost);
+    const proxy = require('koa-proxy')({
         host: proxyhost,
-        map: filePath => "assets/" + filePath
+        map: filePath => 'assets/' + filePath
     });
-    app.use(mount("/assets", proxy));
+    app.use(mount('/assets', proxy));
 } else {
     app.use(
         mount(
-            "/assets",
-            staticCache(path.join(__dirname, "../dist"), cacheOpts)
+            '/assets',
+            staticCache(path.join(__dirname, '../dist'), cacheOpts)
         )
     );
 }
 
-if (env !== "test") {
-    const appRender = require("./app_render");
+if (env !== 'test') {
+    const appRender = require('./app_render');
     app.use(function*() {
         yield appRender(this);
         // if (app_router.dbStatus.ok) recordWebEvent(this, 'page_load');
@@ -265,7 +265,7 @@ if (env !== "test") {
     app.listen(port);
 
     // Tell parent process koa-server is started
-    if (process.send) process.send("online");
+    if (process.send) process.send('online');
     console.log(`Application started on port ${port}`);
 }
 
--- server/teleSign.js
+++ server/teleSign.js
@@ -1,17 +1,17 @@
-import fetch from "node-fetch";
-import config from "config";
-import crypto from "crypto";
-import secureRandom from "secure-random";
+import fetch from 'node-fetch';
+import config from 'config';
+import crypto from 'crypto';
+import secureRandom from 'secure-random';
 
-const { customer_id } = config.get("telesign.customer_id");
+const { customer_id } = config.get('telesign.customer_id');
 
-let api_key = "";
+let api_key = '';
 
-if (config.get("telesign.rest_api_key")) {
-    api_key = new Buffer(config.get("telesign.rest_api_key"), "base64");
+if (config.get('telesign.rest_api_key')) {
+    api_key = new Buffer(config.get('telesign.rest_api_key'), 'base64');
 }
 
-const use_case_code = "BACS"; // Use Case: avoid bulk attack and spammers
+const use_case_code = 'BACS'; // Use Case: avoid bulk attack and spammers
 
 // Testing, always blocked: 1-310-555-0100
 
@@ -20,12 +20,12 @@ export function* verify({ mobile, confirmation_code, ip }) {
     try {
         const result = yield getScore(mobile);
         const { recommendation, score } = result.risk;
-        if (recommendation !== "allow") {
+        if (recommendation !== 'allow') {
             console.log(
                 `TeleSign did not allow phone ${mobile} ip ${ip}. TeleSign responded: ${recommendation}`
             );
             return {
-                error: "Unable to verify your phone number. Please try a different phone number.",
+                error: 'Unable to verify your phone number. Please try a different phone number.',
                 score
             };
         }
@@ -36,8 +36,8 @@ export function* verify({ mobile, confirmation_code, ip }) {
         });
         return { reference_id, score };
     } catch (error) {
-        console.log("-- verify score error -->", error);
-        return { error: "Unable to verify phone, please try again later." };
+        console.log('-- verify score error -->', error);
+        return { error: 'Unable to verify phone, please try again later.' };
     }
 }
 
@@ -45,8 +45,8 @@ function getScore(mobile) {
     const fields = urlencode({
         ucid: use_case_code
     });
-    const resource = "/v1/phoneid/score/" + mobile.match(/\d+/g).join("");
-    const method = "GET";
+    const resource = '/v1/phoneid/score/' + mobile.match(/\d+/g).join('');
+    const method = 'GET';
     return fetch(`https://rest.telesign.com${resource}?${fields}`, {
         method,
         headers: authHeaders({ resource, method })
@@ -81,18 +81,18 @@ function verifySms({ mobile, confirmation_code, ip }) {
     // https://developer.telesign.com/v2.0/docs/rest_api-verify-sms
     const f = {
         phone_number: mobile,
-        language: "en-US",
+        language: 'en-US',
         ucid: use_case_code,
         verify_code: confirmation_code,
-        template: "$$CODE$$ is your Steemit confirmation code"
+        template: '$$CODE$$ is your Steemit confirmation code'
     };
     if (ip) f.originating_ip = ip;
     const fields = urlencode(f);
     // console.log('fields', fields) // logspam
 
-    const resource = "/v1/verify/sms";
-    const method = "POST";
-    return fetch("https://rest.telesign.com" + resource, {
+    const resource = '/v1/verify/sms';
+    const method = 'POST';
+    return fetch('https://rest.telesign.com' + resource, {
         method,
         body: fields,
         headers: authHeaders({ resource, method, fields })
@@ -132,44 +132,44 @@ function authHeaders(
     {
         resource,
         fields,
-        method = "GET"
+        method = 'GET'
     }
 ) {
-    const auth_method = "HMAC-SHA256";
+    const auth_method = 'HMAC-SHA256';
     const currDate = new Date().toUTCString();
     const nonce = parseInt(
-        secureRandom.randomBuffer(8).toString("hex"),
+        secureRandom.randomBuffer(8).toString('hex'),
         16
     ).toString(36);
 
-    let content_type = "";
+    let content_type = '';
     if (/POST|PUT/.test(method))
-        content_type = "application/x-www-form-urlencoded";
+        content_type = 'application/x-www-form-urlencoded';
 
     let strToSign = `${method}\n${content_type}\n\nx-ts-auth-method:${auth_method}\nx-ts-date:${currDate}\nx-ts-nonce:${nonce}`;
 
     if (fields) {
-        strToSign += "\n" + fields;
+        strToSign += '\n' + fields;
     }
-    strToSign += "\n" + resource;
+    strToSign += '\n' + resource;
 
     // console.log('strToSign', strToSign) // logspam
     const sig = crypto
-        .createHmac("sha256", api_key)
-        .update(strToSign, "utf8")
-        .digest("base64");
+        .createHmac('sha256', api_key)
+        .update(strToSign, 'utf8')
+        .digest('base64');
 
     const headers = {
         Authorization: `TSA ${customer_id}:${sig}`,
-        "Content-Type": content_type,
-        "x-ts-date": currDate,
-        "x-ts-auth-method": auth_method,
-        "x-ts-nonce": nonce
+        'Content-Type': content_type,
+        'x-ts-date': currDate,
+        'x-ts-auth-method': auth_method,
+        'x-ts-nonce': nonce
     };
     return headers;
 }
 
 const urlencode = json =>
     Object.keys(json)
-        .map(key => encodeURI(key) + "=" + encodeURI(json[key]))
-        .join("&");
+        .map(key => encodeURI(key) + '=' + encodeURI(json[key]))
+        .join('&');

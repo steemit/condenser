7ce2d1d41c56ba1407d41365264e57059d23fa38
pavelit <info@simdo.ru>
2017-02-21 15:59:39 +0300

    handle next tags nsfw / ru--mat / 18+

--- app/utils/StateFunctions.js
+++ app/utils/StateFunctions.js
@@ -90,7 +90,7 @@ export function contentStats(content) {
         tags = []
     }
     tags.push(content.get('category'))
-    const isNsfw = tags.filter(tag => tag.match(/^nsfw$/i)).length > 0;
+    const isNsfw = tags.filter(tag => tag.match(/^nsfw$|^ru--mat$|^18\+$/i)).length > 0;
 
     return {hide, gray, pictures, netVoteSign, hasPendingPayout, authorRepLog10, hasReplies, hasFlag, isNsfw}
 }

6ff12f48cf52e8eceabcd9effe260d51933d49a8
pavelit <info@simdo.ru>
2017-02-24 00:48:30 +0300

    add golos meta_tags, opengraph  and twitter card

--- app/utils/ExtractMeta.js
+++ app/utils/ExtractMeta.js
@@ -1,21 +1,20 @@
 import extractContent from 'app/utils/ExtractContent';
 import {objAccessor} from 'app/utils/Accessors';
-
-const site_desc = 'Steemit is a social media platform where everyone gets paid for creating and curating content. It leverages a robust digital points system, called Steem, that supports real value for digital rewards through market price discovery and liquidity';
+import { APP_NAME, APP_URL, SITE_DESCRIPTION, TWITTER_HANDLE, SHARE_IMAGE, TWITTER_SHARE_IMAGE } from 'config/client_config';
 
 function addSiteMeta(metas) {
-    metas.push({title: 'Steemit'});
+    metas.push({title: APP_NAME});
     metas.push({property: 'og:type', content: 'website'});
-    metas.push({property: 'og:site_name', content: 'Steemit'});
-    metas.push({property: 'og:title', content: 'Steemit'});
-    metas.push({property: 'og:description', content: site_desc});
-    metas.push({property: 'og:image', content: 'https://steemit.com/images/steemit-share.png'});
+    metas.push({property: 'og:site_name', content: APP_NAME});
+    metas.push({property: 'og:title', content: APP_NAME});
+    metas.push({property: 'og:description', content: SITE_DESCRIPTION});
+    metas.push({property: 'og:image', content: SHARE_IMAGE});
     metas.push({property: 'fb:app_id', content: $STM_Config.fb_app});
     metas.push({name: 'twitter:card', content: 'summary'});
-    metas.push({name: 'twitter:site', content: '@steemit'});
-    metas.push({name: 'twitter:title', content: 'Steemit'});
-    metas.push({name: 'twitter:description', site_desc});
-    metas.push({name: 'twitter:image', content: 'https://steemit.com/images/steemit-share.png'});
+    metas.push({name: 'twitter:site', content: TWITTER_HANDLE});
+    metas.push({name: 'twitter:title', content: APP_NAME});
+    metas.push({name: 'twitter:description', site_desc: SITE_DESCRIPTION});
+    metas.push({name: 'twitter:image', content: SHARE_IMAGE});
 }
 
 export default function extractMeta(chain_data, rp) {
@@ -25,10 +24,11 @@ export default function extractMeta(chain_data, rp) {
         const content = chain_data.content[post];
         if (content && content.id !== '0.0.0') { // API currently returns 'false' data with id 0.0.0 for posts that do not exist
             const d = extractContent(objAccessor, content, false);
-            const url   = 'https://steemit.com' + d.link;
-            const title = d.title + ' — Steemit';
+            const url = 'https://' + APP_URL + d.link;
+            const title = d.title + ' — ' + APP_NAME;
             const desc  = d.desc + " by " + d.author;
-            const image = d.image_link
+            const image = d.image_link ? d.image_link : SHARE_IMAGE;
+            const twimage = d.image_link ? d.image_link : TWITTER_SHARE_IMAGE;
             const {category, created} = d
 
             // Standard meta
@@ -40,19 +40,19 @@ export default function extractMeta(chain_data, rp) {
             metas.push({property: 'og:title',        content: title});
             metas.push({property: 'og:type',         content: 'article'});
             metas.push({property: 'og:url',          content: url});
-            metas.push({property: 'og:image',        content: image || 'https://steemit.com/images/steemit-share.png'});
+            metas.push({property: 'og:image', content: image});
             metas.push({property: 'og:description',  content: desc});
-            metas.push({property: 'og:site_name',    content: 'Steemit'});
+            metas.push({property: 'og:site_name', content: APP_NAME});
             metas.push({property: 'fb:app_id',       content: $STM_Config.fb_app});
             metas.push({property: 'article:tag',     content: category});
             metas.push({property: 'article:published_time', content: created});
 
             // Twitter card data
-            metas.push({name: 'twitter:card',        content: 'summary'});
-            metas.push({name: 'twitter:site',        content: '@steemit'});
-            metas.push({name: 'twitter:title',       content: title});
-            metas.push({name: 'twitter:description', content: desc});
-            metas.push({name: 'twitter:image',       content: image || 'https://steemit.com/images/steemit-twshare.png'});
+            metas.push({name: 'twitter:card', content: 'summary'});
+            metas.push({name: 'twitter:site', content: TWITTER_HANDLE});
+            metas.push({name: 'twitter:title', content: title});
+            metas.push({name: 'twitter:description', content: d.desc});
+            metas.push({name: 'twitter:image', content: twimage});
         } else {
             addSiteMeta(metas);
         }

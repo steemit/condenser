cd6375a884bc462470c57d5a62cf28e6cfaf0b48
Angelo Tomarcafe <angelo.tomarcafe@gmail.com>
2017-03-01 20:14:17 +0000

    server default port change

--- server/server.js
+++ server/server.js
@@ -169,7 +169,7 @@ if (env !== 'test') {
     });
 
     const argv = minimist(process.argv.slice(2));
-    const port = parseInt(argv.port, 10) || parseInt(process.env.PORT, 10) || 3002;
+    const port = parseInt(argv.port, 10) || parseInt(process.env.PORT, 10) || 3004;
     app.listen(port);
 
     // Tell parent process koa-server is started

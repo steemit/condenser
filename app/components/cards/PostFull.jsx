417bf5e1eeb1770e2ea24ace1989312821006a21
Tim <roadscape@users.noreply.github.com>
2017-03-23 12:43:43 -0400

    replace usage of mode prop, removed in 0.17. close #1259

--- app/components/cards/PostFull.jsx
+++ app/components/cards/PostFull.jsx
@@ -277,8 +277,9 @@ class PostFull extends React.Component {
             </div>)
         }
 
-        const readonly = post_content.get('mode') === 'archived' || $STM_Config.read_only_mode
-        const showPromote = username && post_content.get('mode') === "first_payout" && post_content.get('depth') == 0
+        const archived = post_content.get('cashout_time') === '1969-12-31T23:59:59' // TODO: audit after HF17. #1259
+        const readonly = archived || $STM_Config.read_only_mode
+        const showPromote = username && post_content.get('last_payout') === '1970-01-01T00:00:00' && post_content.get('depth') == 0 // TODO: audit after HF17. #1259
         const showReplyOption = post_content.get('depth') < 6
         const showEditOption = username === author
         const showDeleteOption = username === author && post_content.get('children') === 0 && content.stats.netVoteSign <= 0

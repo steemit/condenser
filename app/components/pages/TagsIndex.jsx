01cae443846d836b89ef3b5e1ecd3062c61e2e69
pavelit <info@simdo.ru>
2017-03-27 13:34:39 +0300

    set colors: green select-button and red deselect-button

--- app/components/pages/TagsIndex.jsx
+++ app/components/pages/TagsIndex.jsx
@@ -82,9 +82,11 @@ export default class TagsIndex extends React.Component {
         return (
             <div className="TagsIndex row">
                 <div className="column">
+                  {/*
                     <div className="medium-2 medium-offset-10">
                         <input type="text" placeholder={translate('filter')} value={search} onChange={this.onChangeSearch} />
                     </div>
+                  */}
                     <table>
                         <thead>
                         <tr>

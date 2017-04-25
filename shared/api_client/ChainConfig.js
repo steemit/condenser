dad7edd981cfee3f51fb0f37b391494ed66a990d
pavelit <info@simdo.ru>
2017-04-25 20:41:32 +0300

    Add chain_id

--- shared/api_client/ChainConfig.js
+++ shared/api_client/ChainConfig.js
@@ -2,7 +2,7 @@ import { ecc_config, hash } from "../ecc"
 
 ecc_config.address_prefix = "GLS";
 
-let chain_id = ""
+let chain_id = "5876894a41e6361bde2e73278f07340f2eb8b41c2facd29099de9deef6cdb679"
 for(let i = 0; i < 32; i++) chain_id += "00"
 
 module.exports = {

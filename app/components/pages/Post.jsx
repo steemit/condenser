513adbae8dfff2af0d2644bea853566210b9a7a7
Igor Tkachenko <tkachenko.igor88@gmail.com>
2017-05-03 20:17:51 +0300

    fix #363

--- app/components/pages/Post.jsx
+++ app/components/pages/Post.jsx
@@ -9,10 +9,8 @@ import {sortComments} from 'app/components/cards/Comment';
 import FoundationDropdownMenu from 'app/components/elements/FoundationDropdownMenu';
 import {Set} from 'immutable'
 import tt from 'counterpart';
-import { localizedCurrency } from 'app/components/elements/LocalizedCurrency';
 import shouldComponentUpdate from 'app/utils/shouldComponentUpdate';
 import {serverApiRecordEvent} from 'app/utils/ServerApiClient';
-import { VESTING_TOKEN_UPPERCASE } from 'app/client_config';
 
 class Post extends React.Component {
 
@@ -176,7 +174,7 @@ class Post extends React.Component {
                         <div className="Post__promo">
                             {tt('g.next_7_strings_sinngle_block.authors_get_paid_when_people_like_you_upvote_their_post')}.
                             <br /> {// remove '$' from signup_bonus before parsing it into local currency
-                                    tt('g.next_7_strings_sinngle_block.if_you_enjoyed_what_you_read_earn_amount', {amount: localizedCurrency(signup_bonus.substring(1)), VESTING_TOKEN_UPPERCASE})}
+                                    tt('g.next_7_strings_sinngle_block.if_you_enjoyed_what_you_read_earn_amount')}
                             <br />
                             <button type="button" className="button sign-up" onClick={showSignUp}>{tt('g.next_7_strings_sinngle_block.sign_up_now_to_receive')}<span className="free-money">{tt('g.next_7_strings_sinngle_block.free_steem', {VESTING_TOKEN_UPPERCASE})}</span></button>
                         </div>

9ce173b0b6a319bb91b7d24271c6ec60477826c8
pavelit <info@simdo.ru>
2017-05-23 14:37:50 +0300

    Add to translator Serbian and Romainan lang files

--- app/Translator.js
+++ app/Translator.js
@@ -6,19 +6,23 @@ import tt from 'counterpart';
 import en from 'react-intl/locale-data/en';
 import ru from 'react-intl/locale-data/ru';
 import uk from 'react-intl/locale-data/uk';
+import sr from 'react-intl/locale-data/sr';
+import ro from 'react-intl/locale-data/ro';
 
-addLocaleData([...uk, ...ru, ...en]);
+addLocaleData([...en, ...ru, ...uk, ...sr, ...ro]);
 
 tt.registerTranslations('en', require('app/locales/en.json'));
 tt.registerTranslations('ru', require('app/locales/ru-RU.json'));
 tt.registerTranslations('uk', require('app/locales/ua.json'));
+tt.registerTranslations('sr', require('app/locales/sr.json'));
+tt.registerTranslations('ro', require('app/locales/ro-RO.json'));
 
 class Translator extends React.Component {
     render() {
         const locale = this.props.locale || DEFAULT_LANGUAGE;
         const localeWithoutRegionCode = locale.toLowerCase().split(/[_-]+/)[0];
         tt.setLocale(localeWithoutRegionCode)
-		tt.setFallbackLocale('en');
+		    tt.setFallbackLocale('en');
         return <IntlProvider
             // to ensure dynamic language change, "key" property with same "locale" info must be added
             // see: https://github.com/yahoo/react-intl/wiki/Components#multiple-intl-contexts

1e96a7095080499f1931e247a4d04a8539a95fe1
Mike <mcifani@users.noreply.github.com>
2017-06-13 18:16:56 -0400

    New improved wallet delegation verbiage, display only when delegation… (#1477)
    * New improved wallet delegation verbiage, display only when delegation is detected
    
    * Update veribage for delegation, refactor for locals
    
    * updates
    
    * updates

--- app/locales/jp.js
+++ app/locales/jp.js
@@ -335,7 +335,8 @@ const jp = 	{
 	curation_rewards_last_24_hours: '24時間以内のキュレーションリワード',
 	daily_average_curation_rewards: '1にち平均のキュレーションリワード',
 	estimated_curation_rewards_last_week: "先週の予定されているキュレーションリワード",
-	curation_rewards_last_week: "先週のキュレーションリワード",
+    part_of_your_steem_power_is_currently_delegated: 'あなたのSTEAM POWERの一部が現在あなたに委任されています。 委任は、影響を与えたり、新しいユーザーがsteemitで行動を起こすのを助けるために寄付されます。 あなたは代表団の額が変動する可能性があります。',
+    curation_rewards_last_week: "先週のキュレーションリワード",
 	curation_rewards_history: 'キュレーションリワード履歴',
 	// Post.jsx
 	now_showing_comments_with_low_ratings: '評価が低いコメントを表示しています',

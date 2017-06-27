82262d7d20086b02a7dc13d0d42410afde532ef2
pavelit <info@simdo.ru>
2017-06-27 10:28:05 +0300

    Activate email+mobile registration

--- app/components/pages/CreateAccount.jsx
+++ app/components/pages/CreateAccount.jsx
@@ -1,29 +1,24 @@
 /* eslint react/prop-types: 0 */
 import React from 'react';
-import { browserHistory } from 'react-router';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import LoadingIndicator from 'app/components/elements/LoadingIndicator';
 import Apis from 'shared/api_client/ApiInstances';
-import { PrivateKey } from 'shared/ecc';
+import {PrivateKey} from 'shared/ecc';
 import user from 'app/redux/User';
 import {validate_account_name} from 'app/utils/ChainValidation';
 import SignUp from 'app/components/modules/SignUp';
 import runTests from 'shared/ecc/test/BrowserTests';
-import g from 'app/redux/GlobalReducer';
 import GeneratedPasswordInput from 'app/components/elements/GeneratedPasswordInput';
+import SignupProgressBar from 'app/components/elements/SignupProgressBar';
 import { translate } from 'app/Translator';
 import { localizedCurrency } from 'app/components/elements/LocalizedCurrency';
 import { FormattedHTMLMessage } from 'react-intl';
 import { SUPPORT_EMAIL } from 'config/client_config';
-//import SignupProgressBar from 'app/components/elements/SignupProgressBar';
-
-const PASSWORD_MIN_LENGTH = 32;
 
 class CreateAccount extends React.Component {
 
     static propTypes = {
         loginUser: React.PropTypes.func.isRequired,
-        showSignUp: React.PropTypes.func.isRequired,
         serverBusy: React.PropTypes.bool
     };
 
@@ -45,14 +40,11 @@ class CreateAccount extends React.Component {
     }
 
     componentDidMount() {
-        const cryptoTestResult = undefined;
-        //const cryptoTestResult = runTests();
+        const cryptoTestResult = undefined; // = runTests(); // temporarily switch BrowserTests off
         if (cryptoTestResult !== undefined) {
             console.error('CreateAccount - cryptoTestResult: ', cryptoTestResult);
             this.setState({cryptographyFailure: true}); // TODO: do not use setState in componentDidMount
         }
-        // Facebook Pixel events #200
-        //if (process.env.BROWSER) fbq('track', 'Lead');
     }
 
     onSubmit(e) {
@@ -62,9 +54,6 @@ class CreateAccount extends React.Component {
         if (!name || !password || !password_valid) return;
 
         let public_keys;
-        // try generating btc address via blockcypher
-        // if no success - abort (redirect with try again)
-        let icoAddress = ''
         try {
             const pk = PrivateKey.fromWif(password);
             public_keys = [1, 2, 3, 4].map(() => pk.toPublicKey().toString());
@@ -90,14 +79,13 @@ class CreateAccount extends React.Component {
                 owner_key: public_keys[0],
                 active_key: public_keys[1],
                 posting_key: public_keys[2],
-                memo_key: public_keys[3]//,
-                //json_meta: JSON.stringify({"ico_address": icoAddress})
+                memo_key: public_keys[3]
             })
         }).then(r => r.json()).then(res => {
             if (res.error || res.status !== 'ok') {
                 console.error('CreateAccount server error', res.error);
                 if (res.error === 'Unauthorized') {
-                     this.props.showSignUp();
+                    window.location = '/enter_email';
                 }
                 this.setState({server_error: res.error || translate('unknown'), loading: false});
             } else {
@@ -134,13 +122,18 @@ class CreateAccount extends React.Component {
         if (name.length > 0) {
             name_error = validate_account_name(name);
             if (!name_error) {
+                this.setState({name_error: ''});
                 promise = Apis.db_api('get_accounts', [name]).then(res => {
-                    return res && res.length > 0 ? translate('account_name_is_not_available') : '';
+                    return res && res.length > 0 ? 'Account name is not available' : '';
                 });
             }
         }
         if (promise) {
-            promise.then(error => this.setState({name_error: error}));
+            promise
+                .then(name_error => this.setState({name_error}))
+                .catch(() => this.setState({
+                    name_error: "Account name can't be verified right now due to server failure. Please try again later."
+                }));
         } else {
             this.setState({name_error});
         }
@@ -150,21 +143,19 @@ class CreateAccount extends React.Component {
         if (!process.env.BROWSER) { // don't render this page on the server
             return <div className="row">
                 <div className="column">
-                    {translate('loading')}..
+                    {translate('loading')}...
                 </div>
             </div>;
         }
 
         const {
-            name, password_valid, showPasswordString,
-            name_error, server_error, loading, cryptographyFailure
+            name, password_valid, //showPasswordString,
+            name_error, server_error, loading, cryptographyFailure, showRules
         } = this.state;
 
         const {loggedIn, logout, offchainUser, serverBusy} = this.props;
         const submit_btn_disabled =
-            loading ||
-            !name ||
-            !password_valid ||
+            loading || !name || !password_valid ||
             name_error;
         const submit_btn_class = 'button action' + (submit_btn_disabled ? ' disabled' : '');
 
@@ -186,7 +177,7 @@ class CreateAccount extends React.Component {
                         <p>
                             {translate('the_latest_versions_of') + ' '}
                             <a href="https://www.google.com/chrome/">Chrome</a>
-                            {' ' + translate('and')}
+                            {' ' + translate('and') + ' '}
                             <a href="https://www.mozilla.org/en-US/firefox/new/">Firefox</a>
                             {' ' + translate('are_well_tested_and_known_to_work_with')}
                         </p>
@@ -228,40 +219,66 @@ class CreateAccount extends React.Component {
             </div>;
         }
 
+        let next_step = null;
+        if (server_error) {
+            if (server_error === 'Email address is not confirmed') {
+                next_step = <div className="callout alert">
+                    <a href="/enter_email">{translate('confirm_email')}</a>
+                </div>;
+            } else if (server_error === 'Phone number is not confirmed') {
+                next_step = <div className="callout alert">
+                    <a href="/enter_mobile">Please verify your phone number</a>
+                </div>;
+            } else {
+                next_step = <div className="callout alert">
+                    <h5>{translate('couldnt_create_account_server_returned_error')}:</h5>
+                    <p>{server_error}</p>
+                </div>;
+            }
+        }
+
+        // 'goddamn syntax highlighting
         return (
+            <div>
+                <SignupProgressBar steps={[translate('email'), translate('phone'), translate('golos_account')]} current={3} />
                 <div className="CreateAccount row">
-                <div className="column large-7 small-10">
-                    <h2>{translate('sign_up')}</h2>
-                    <div className="CreateAccount__rules">
-                            <hr />
+                    <div className="column" style={{maxWidth: '36rem', margin: '0 auto'}}>
+                        <br />
                         {/* currently translateHtml() does not work, using <FormattedHTMLMessage /> instead */}
-                        <p><FormattedHTMLMessage id="the_rules_of_APP_NAME" /></p>
-                        <hr />
-                    </div>
+                        {showRules ? <div className="CreateAccount__rules">
+                            <p>
+                                <FormattedHTMLMessage id="the_rules_of_APP_NAME" />
+                            </p>
+                            <div>
+                                <a className="CreateAccount__rules-button" href="#" onClick={() => this.setState({showRules: false})}>
+                                    <span style={{display: 'inline-block', transform: 'rotate(-90deg)'}}>&raquo;</span>
+                                </a>
+                            </div>
+                            <hr />
+                        </div> : <div className="text-center">
+                            <a className="CreateAccount__rules-button" href="#" onClick={() => this.setState({showRules: true})}>{translate('show_rules')}&nbsp; &raquo;</a>
+                        </div>}
                         <form onSubmit={this.onSubmit} autoComplete="off" noValidate method="post">
                             <div className={name_error ? 'error' : ''}>
-                            <label className="uppercase">{translate('username')}
+                                <label>{translate('username').toUpperCase()}
                                     <input type="text" name="name" autoComplete="off" onChange={this.onNameChange} value={name} />
                                 </label>
                                 <p>{name_error}</p>
                             </div>
                             <GeneratedPasswordInput onChange={this.onPasswordChange} disabled={loading} showPasswordString={name.length > 0 && !name_error} />
                             <br />
-                        {server_error && <div className="callout alert">
-                            <h5>{translate('couldnt_create_account_server_returned_error')}:</h5>
-                            <p>{server_error}</p>
-                            {server_error === 'Email address is not confirmed' && <a href="/enter_email">{translate('confirm_email')}</a>}
-                        </div>}
+                            {next_step && <div>{next_step}<br /></div>}
                             <noscript>
                                 <div className="callout alert">
-                                <p>{translate('form_requires_javascript_to_be_enabled')}</p>
+                                    <p>{translate('form_requires_javascript_to_be_enabled')}</p>
                                 </div>
                             </noscript>
                             {loading && <LoadingIndicator type="circle" />}
-                        <input disabled={submit_btn_disabled} type="submit" className={submit_btn_class + ' uppercase'} value={translate('sign_up')} />
+                            <input disabled={submit_btn_disabled} type="submit" className={submit_btn_class} value={translate('sign_up')} />
                         </form>
                     </div>
                 </div>
+            </div>
         );
     }
 }
@@ -279,7 +296,6 @@ module.exports = {
         },
         dispatch => ({
             loginUser: (username, password) => dispatch(user.actions.usernamePasswordLogin({username, password, saveLogin: true})),
-            showSignUp: () => dispatch(user.actions.showSignUp()),
             logout: e => {
                 if (e) e.preventDefault();
                 dispatch(user.actions.logout())

755e4cc04b27dd035b2f74cd74dcceb30480a35a
pavelit <info@simdo.ru>
2017-07-05 14:54:24 +0300

    #393 Convert dynamic global props polling

--- app/redux/PollDataSaga.js
+++ app/redux/PollDataSaga.js
@@ -1,8 +1,8 @@
 import { call, put, select } from 'redux-saga/effects';
-import Apis from 'shared/api_client/ApiInstances';
 import GlobalReducer from './GlobalReducer';
 import {getNotifications, webPushRegister} from 'app/utils/ServerApiClient';
 import registerServiceWorker from 'app/utils/RegisterServiceWorker';
+import {api} from 'golos-js';
 
 const wait = ms => (
     new Promise(resolve => {
@@ -31,19 +31,13 @@ function* pollData() {
             yield put({type: 'UPDATE_NOTIFICOUNTERS', payload: nc});
         }
 
-        const ws_connection = yield select(state => state.app.get('ws_connection'));
-        if (ws_connection && ws_connection.status !== 'open') {
-            console.log('pollData: not connected, skipping');
-        } else {
-            try {
-                const db_api = Apis.instance().db_api;
-                const data = yield call([db_api, db_api.exec], 'get_dynamic_global_properties', []);
-                // console.log('-- pollData.pollData -->', data);
-                // const data = yield call([db_api, db_api.exec], 'get_discussions_by_created', [{limit: 10}]);
-                // yield put(GlobalReducer.actions.receiveRecentPosts({data}));
-            } catch (error) {
-                console.error('~~ pollData saga error ~~>', error);
-            }
+        try {
+            const data = yield call([api, api.getDynamicGlobalPropertiesAsync]);
+            // console.log('-- pollData.pollData -->', data);
+            // const data = yield call([api, api.getDiscussionsByCreatedAsync], {limit: 10});
+            // yield put(GlobalReducer.actions.receiveRecentPosts({data}));
+        } catch (error) {
+            console.error('~~ pollData saga error ~~>', error);
         }
     }
 }

ea4582c626dcd6daf7a330174c158d3775954704
b1acksun <tkachenko.igor88@gmail.com>
2017-07-12 11:05:33 +0300

    disable favicon 128 and 196x196

--- server/server-html.jsx
+++ server/server-html.jsx
@@ -35,11 +35,11 @@ export default function ServerHTML({ body, assets, locale, title, meta }) {
             <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/images/favicons/apple-icon-120x120.png" type="image/png" />
             <link rel="apple-touch-icon-precomposed" sizes="76x76" href="/images/favicons/apple-icon-76x76.png" type="image/png" />
             <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/images/favicons/apple-icon-152x152.png" type="image/png" />
-            <link rel="icon" type="image/png" href="/images/favicons/favicon-196x196.png" sizes="196x196" />
+            {/*<link rel="icon" type="image/png" href="/images/favicons/favicon-196x196.png" sizes="196x196" />*/}
             <link rel="icon" type="image/png" href="/images/favicons/favicon-96x96.png" sizes="96x96" />
             <link rel="icon" type="image/png" href="/images/favicons/favicon-32x32.png" sizes="32x32" />
             <link rel="icon" type="image/png" href="/images/favicons/favicon-16x16.png" sizes="16x16" />
-            <link rel="icon" type="image/png" href="/images/favicons/favicon-128.png" sizes="128x128" />
+            {/*<link rel="icon" type="image/png" href="/images/favicons/favicon-128.png" sizes="128x128" />*/}
             <meta name="application-name" content={LIQUID_TOKEN} />
             <meta name="msapplication-TileColor" content="#FFFFFF" />
             <meta name="msapplication-TileImage" content="/images/favicons/ms-icon-144x144.png" />

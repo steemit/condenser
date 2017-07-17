16fb9dfaf18b282766576890a032472a7d6d7679
pavelit <info@simdo.ru>
2017-07-17 19:44:45 +0300

    Update golos fest url

--- app/components/modules/TopRightMenu.jsx
+++ app/components/modules/TopRightMenu.jsx
@@ -77,7 +77,7 @@ function TopRightMenu({account, savings_withdraws, price_per_golos, globalprops,
     const submitStoryPencil = $STM_Config.read_only_mode ? null : <li className="show-for-small-only">
       <Link to="/submit.html"><Icon name="pencil" /></Link>
     </li>;
-    const golosFest = <li className={lcn + ' buttons'}><Link to="/golos/@golosevents/yaidunagolosfest-or-ceny-spikery-volontyory-besplatnyi-bilet" className="button alert fest">{tt('g.golos_fest')}</Link></li>;
+    const golosFest = <li className={lcn + ' buttons'}><Link to="/ru--yaidunagolosfest/@golosevents/yaidunagolosfest-or-itogi-konkursa-na-besplatnyi-bilet-zakanchivaetsya-registraciya-na-fest-otkryvaetsya-registraciya-na-seminar" className="button alert fest">{tt('g.golos_fest')}</Link></li>;
     const feedLink = `/@${username}/feed`;
     const repliesLink = `/@${username}/recent-replies`;
     const walletLink = `/@${username}/transfers`;

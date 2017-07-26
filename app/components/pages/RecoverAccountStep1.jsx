e2f4d0fddc2252c8e7425562d4b85bcb95a0f83b
b1acksun <tkachenko.igor88@gmail.com>
2017-07-26 13:25:33 +0300

    fix missing

--- app/components/pages/RecoverAccountStep1.jsx
+++ app/components/pages/RecoverAccountStep1.jsx
@@ -5,6 +5,7 @@ import constants from 'app/redux/constants';
 import {PrivateKey} from 'golos-js/lib/auth/ecc';
 import tt from 'counterpart';
 import { FormattedHTMLMessage } from 'react-intl';
+import { APP_NAME, APP_DOMAIN } from 'app/client_config';
 import {api} from 'golos-js';
 
 const email_regex = /^([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x22([^\x0d\x22\x5c\x80-\xff]|\x5c[\x00-\x7f])*\x22))*\x40([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d)(\x2e([^\x00-\x20\x22\x28\x29\x2c\x2e\x3a-\x3c\x3e\x40\x5b-\x5d\x7f-\xff]+|\x5b([^\x0d\x5b-\x5d\x80-\xff]|\x5c[\x00-\x7f])*\x5d))*$/;
@@ -164,7 +165,7 @@ class RecoverAccountStep1 extends React.Component {
                     <div className="column large-4">
                         <h2>{tt('navigation.stolen_account_recovery')}</h2>
                         <p>
-                            {tt('recoveraccountstep1_jsx.recover_account_intro')}
+                            {tt('recoveraccountstep1_jsx.recover_account_intro', {APP_NAME, APP_URL: APP_DOMAIN})}
                         </p>
                         <form onSubmit={this.onSubmit} noValidate>
                             <div className={name_error ? 'error' : ''}>

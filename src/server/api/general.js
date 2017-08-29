6c971747ea3fe21c493639150c30a31bf1221d47
Gandalf <gandalf@storm.pl>
2017-08-29 15:53:37 +0200

    test remote addresses from behind ALB point of view

--- src/server/api/general.js
+++ src/server/api/general.js
@@ -289,6 +289,10 @@ export default function useGeneralApi(app) {
         try {
             const params = this.request.body;
             const remote_address = this.request.headers['x-forwarded-for'] || this.request.connection.remoteAddress;
+            const test_remote_address1 = this.request.headers['x-forwarded-for'];
+            const test_remote_address2 = this.request.connection.remoteAddress;
+            console.log('DEBUG: test1: ', test_remote_address1);
+            console.log('DEBUG: test2: ', test_remote_address2);
             const {csrf, type, value} = typeof(params) === 'string' ? JSON.parse(params) : params;
             if (!checkCSRF(this, csrf)) return;
             console.log('-- /record_event -->', this.session.uid, remote_address, type, value);

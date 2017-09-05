1fea0a211f73c3f19fcec8cbdd7ce847683f42c2
Donovan Walker <donovan@donovanwalker.com>
2017-09-05 15:06:55 -0500

    code climate complaint

--- src/app/utils/ProxifyUrl.test.js
+++ src/app/utils/ProxifyUrl.test.js
@@ -1,12 +1,11 @@
-/*global describe, before, it */
+/*global describe, global, before:false, it*/
 import assert from 'assert'
 import proxifyImageUrl from './ProxifyUrl'
 
 describe('ProxifyUrl', () => {
-    before(function (){
+    before(() => {
         global.$STM_Config = {img_proxy_prefix: 'https://steemitimages.com/'};
     });
-
     it('naked URL', () => {
         testCase('https://example.com/img.png', '100x200', 'https://steemitimages.com/100x200/https://example.com/img.png')
         testCase('https://example.com/img.png', '0x0', 'https://steemitimages.com/0x0/https://example.com/img.png')
@@ -20,7 +19,6 @@ describe('ProxifyUrl', () => {
         testCase('https://steemitimages.com/0x0/https://example.com/img.png', '100x200', 'https://steemitimages.com/100x200/https://example.com/img.png')
         testCase('https://steemitimages.com/256x512/https://peopledotcom.files.wordpress.com/2017/09/grumpy-harvey-cat.jpg?w=2000', '100x200', 'https://steemitimages.com/100x200/https://peopledotcom.files.wordpress.com/2017/09/grumpy-harvey-cat.jpg?w=2000')
         testCase('https://steemitimages.com/0x0/https://example.com/img.png', false, 'https://example.com/img.png')
-
     })
     it('double-proxied URL', () => {
         testCase('https://steemitimages.com/0x0/https://steemitimages.com/0x0/https://example.com/img.png', '100x200', 'https://steemitimages.com/100x200/https://example.com/img.png')

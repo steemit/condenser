6d8c5a6207a06467ee94e9d86e9ea8088be75447
Donovan Walker <donovan@donovanwalker.com>
2017-09-05 13:57:46 -0500

    1153 updated tests to capture potential regex issues

--- src/app/utils/ProxifyUrl.test.js
+++ src/app/utils/ProxifyUrl.test.js
@@ -1,4 +1,4 @@
-/*global describe, before */
+/*global describe, before, it */
 import assert from 'assert'
 import proxifyImageUrl from 'app/utils/ProxifyUrl'
 
@@ -12,10 +12,15 @@ describe('ProxifyUrl', () => {
         testCase('https://example.com/img.png', '0x0', 'https://steemitimages.com/0x0/https://example.com/img.png')
         testCase('https://example.com/img.png', false, 'https://example.com/img.png')
     })
+    it('naked steemit proxy URL', () => {
+        testCase('https://steemitimages.com/DQmaJe2Tt5kmVUaFhse1KTEr4N1g9piMgD3YjPEQhkZi3HR/30day-positivity-challenge.jpg', '256x512', 'https://steemitimages.com/256x512/https://steemitimages.com/DQmaJe2Tt5kmVUaFhse1KTEr4N1g9piMgD3YjPEQhkZi3HR/30day-positivity-challenge.jpg')
+        testCase('https://steemitimages.com/DQmaJe2Tt5kmVUaFhse1KTEr4N1g9piMgD3YjPEQhkZi3HR/30day-positivity-challenge.jpg', false, 'https://steemitimages.com/DQmaJe2Tt5kmVUaFhse1KTEr4N1g9piMgD3YjPEQhkZi3HR/30day-positivity-challenge.jpg')
+    })
     it('proxied URL', () => {
         testCase('https://steemitimages.com/0x0/https://example.com/img.png', '100x200', 'https://steemitimages.com/100x200/https://example.com/img.png')
         testCase('https://steemitimages.com/256x512/https://peopledotcom.files.wordpress.com/2017/09/grumpy-harvey-cat.jpg?w=2000', '100x200', 'https://steemitimages.com/100x200/https://peopledotcom.files.wordpress.com/2017/09/grumpy-harvey-cat.jpg?w=2000')
         testCase('https://steemitimages.com/0x0/https://example.com/img.png', false, 'https://example.com/img.png')
+
     })
     it('double-proxied URL', () => {
         testCase('https://steemitimages.com/0x0/https://steemitimages.com/0x0/https://example.com/img.png', '100x200', 'https://steemitimages.com/100x200/https://example.com/img.png')

85197b3b6759ca9852c40bf3bb07ded160900f3f
pavelit <info@simdo.ru>
2017-09-06 11:34:23 +0300

    Add currency & load it from cookie

--- app/redux/User.js
+++ app/redux/User.js
@@ -1,6 +1,7 @@
 import {fromJS} from 'immutable';
 import createModule from 'redux-modules';
-import { DEFAULT_LANGUAGE, DEFAULT_DOMESTIC, DEFAULT_THEME } from 'app/client_config';
+import {DEFAULT_CURRENCY, CURRENCY_COOKIE_KEY, DEFAULT_LANGUAGE, LOCALE_COOKIE_KEY, DEFAULT_DOMESTIC, DEFAULT_THEME} from 'app/client_config';
+import cookie from "react-cookie";
 
 const defaultState = fromJS({
     current: null,
@@ -9,10 +10,18 @@ const defaultState = fromJS({
     show_promote_post_modal: false,
     show_signup_modal: false,
     pub_keys_used: null,
+    currency: DEFAULT_CURRENCY,
     locale: DEFAULT_LANGUAGE,
     domestic: DEFAULT_DOMESTIC,
     theme: DEFAULT_THEME
 });
+
+if (process.env.BROWSER) {
+    const locale = cookie.load(LOCALE_COOKIE_KEY)
+    if (locale) defaultState.locale = locale;
+    const currency = cookie.load(CURRENCY_COOKIE_KEY)
+    if (currency) defaultState.currency = currency;
+}
 
 export default createModule({
     name: 'user',
@@ -56,6 +65,9 @@ export default createModule({
             state = state.setIn(['authority', username, 'owner'], 'none')
             return state
         }},
+        { action: 'CHANGE_CURRENCY', reducer: (state, {payload}) => {
+            return state.set('currency', payload)}
+        },
         { action: 'CHANGE_LANGUAGE', reducer: (state, {payload}) => {
             return state.set('locale', payload)}
         },

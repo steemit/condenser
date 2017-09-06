996d576e1f741fe541990fa7ca0d0a847edd203d
pavelit <info@simdo.ru>
2017-09-06 11:35:06 +0300

    Add error types

--- server/utils/currency.js
+++ server/utils/currency.js
@@ -33,7 +33,7 @@ var exports = module.exports = {};
  *       Bid: '0.8935' } ]
  * });
  */
- var getRates = exports.getRates = function(symbols, cb) {
+var getRates = exports.getRates = function(symbols, cb) {
   if (!(Array.isArray(symbols) || typeof symbols === 'string')) {
     return cb(new TypeError('Invalid parameter types supplied.'), null);
   }
@@ -55,7 +55,8 @@ var exports = module.exports = {};
   var options = {
     host: 'query.yahooapis.com',
     path: '/v1/public/yql?q=select%20*%20from%20yahoo.finance.xchange%20where%20pair%3D%22'
-        + query +'%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',
+      + query
+      + '%22&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys',
     method: 'GET'
   };
   http.get(options, function(res) {
@@ -68,15 +69,15 @@ var exports = module.exports = {};
         return cb(new Error('The result is broken. Unexpected some token in JSON'), null);
       }
       if (parsed.error) {
-        return cb(new Error('The result is undefined. Make sure that you are using correct currency symbols'), null);
+        return cb(new Error('[0] The result is undefined. Make sure that you are using correct currency symbols'), null);
       }
       var vals = parsed.query.results.rate;
       if (typeof vals == 'object' && vals.Name == 'N/A') {
-        return cb(new Error('The result is undefined. Make sure that you are using correct currency symbols'), null);
+        return cb(new Error('[1] The result is undefined. Make sure that you are using correct currency symbols'), null);
       } else if (Array.isArray(vals)) {
         for (var i = 0; i < vals.length; i++) {
           if (vals[i].Name == 'N/A') {
-            cb(new Error('The result is undefined. Make sure that you are using correct currency symbols'), null);
+            cb(new Error('[2] The result is undefined. Make sure that you are using correct currency symbols'), null);
           }
         }
       }

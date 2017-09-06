08bbe230cd2ad1d2294e82ef89243253b3dc730d
pavelit <info@simdo.ru>
2017-09-06 11:27:12 +0300

    Remove unused logic

--- app/Translator.js
+++ app/Translator.js
@@ -19,34 +19,29 @@ tt.registerTranslations('sr', require('app/locales/sr.json'));
 tt.registerTranslations('ro', require('app/locales/ro-RO.json'));
 
 class Translator extends React.Component {
-    render() {
-      console.log('Translator reload')
-        const fromCookie = process.env.BROWSER && Object.keys(LANGUAGES).indexOf(cookie.load(LOCALE_COOKIE_KEY)) !== -1 ? cookie.load(LOCALE_COOKIE_KEY) : DEFAULT_LANGUAGE
-        const locale = process.env.BROWSER ? fromCookie || DEFAULT_LANGUAGE : $GLS_Config.locale
-        // const locale = process.env.BROWSER ? localStorage.getItem('language') || DEFAULT_LANGUAGE : DEFAULT_LANGUAGE
-        // const locale = this.props.locale || DEFAULT_LANGUAGE;
-        const localeWithoutRegionCode = locale.toLowerCase().split(/[_-]+/)[0];
-        tt.setLocale(localeWithoutRegionCode)
-		    tt.setFallbackLocale('en');
-        return <IntlProvider
-            // to ensure dynamic language change, "key" property with same "locale" info must be added
-            // see: https://github.com/yahoo/react-intl/wiki/Components#multiple-intl-contexts
-            key={localeWithoutRegionCode}
-            locale={localeWithoutRegionCode}
-            defaultLocale={DEFAULT_LANGUAGE}
-        >
-            {this.props.children}
-        </IntlProvider>
-    }
+  render() {
+    const localeWithoutRegionCode = this.props.locale.toLowerCase().split(/[_-]+/)[0];
+    tt.setLocale(localeWithoutRegionCode);
+		tt.setFallbackLocale('en');
+    return <IntlProvider
+      // to ensure dynamic language change, "key" property with same "locale" info must be added
+      // see: https://github.com/yahoo/react-intl/wiki/Components#multiple-intl-contexts
+      key={localeWithoutRegionCode}
+      locale={localeWithoutRegionCode}
+      defaultLocale={DEFAULT_LANGUAGE}
+    >
+      {this.props.children}
+    </IntlProvider>
+  }
 }
 
 export default connect(
-    (state, ownProps) => {
-        const locale = state.user.get('locale');
-        return {...ownProps, locale};
-    }
+  (state, ownProps) => {
+    const locale = state.user.get('locale');
+    return {...ownProps, locale};
+  }
 )(Translator)
 
 export const FormattedHTMLMessage = ({id, params, className}) => (
-    <div className={'FormattedHTMLMessage' + (className ? ` ${className}` : '')} dangerouslySetInnerHTML={ { __html: tt(id, params) } }></div>
+  <div className={'FormattedHTMLMessage' + (className ? ` ${className}` : '')} dangerouslySetInnerHTML={ { __html: tt(id, params) } }></div>
 );

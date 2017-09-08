d7611cde72f407c41e00ff5d7730f0046ff8b297
valzav <i@valzav.com>
2017-09-08 10:54:58 -0400

    update tt regexp; don't go too deep into pluralization json

--- scripts/check_translations.js
+++ scripts/check_translations.js
@@ -2,14 +2,18 @@ const fs = require('fs');
 
 function jsonToKeys(keys, prefix, json) {
     if (typeof json === 'object') {
+        if (json.one && json.other) {
+            keys[prefix] = true;
+            return;
+        }
         for (const k in json) {
             const new_prefix = prefix ? prefix + '.' + k : k;
             jsonToKeys(keys, new_prefix, json[k]);
         }
-    } else {
-        if (keys[prefix]) throw new Error('Duplicate translation: ' + prefix);
-        keys[prefix] = true;
+        return;
     }
+    if (keys[prefix]) throw new Error('Duplicate translation: ' + prefix);
+    keys[prefix] = true;
 }
 
 function readTranslationKeys(path) {
@@ -37,12 +41,13 @@ function loadTranslationFiles(path) {
 function processFile(used_keys, path) {
     const lines = fs.readFileSync(path, 'utf8').split(/\r?\n/);
     for (const l of lines) {
-        const tts = l.match(/(tt\(['.-_\w]+\))/g);
+        const tts = l.match(/(tt\([\"\'\.\-\_\w]+)/g);
         if (tts) {
             // if(tts.length > 1) console.log('-- tt -->', path, l, tts.length, JSON.stringify(tts, null, 4));
             for (const t of tts) {
-                const m = t.match(/tt\(['"](['.-_\w]+)['"]\)/);
-                if (!m) throw new Error('Wrong format: ' + t);
+                if (t === 'tt(id') continue; // this is exception
+                const m = t.match(/tt\([\'\"]([\.\-\_\w]+)/);
+                if (!m) throw new Error('Wrong format: "' + t + '" in "' + l + '"');
                 const key = m[1];
                 if (used_keys[key]) used_keys[key] += 1;
                 else used_keys[key] = 1;

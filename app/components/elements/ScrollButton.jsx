88ce0dadcafa38a04201075d106885e0e27b1545
b1acksun <tkachenko.igor88@gmail.com>
2017-09-11 22:51:45 +0300

    use react-scroll-up

--- app/components/elements/ScrollButton.jsx
+++ app/components/elements/ScrollButton.jsx
@@ -1,40 +1,17 @@
 import React from 'react';
 import Icon from 'app/components/elements/Icon'
 import tt from 'counterpart';
+import ScrollToTop from 'react-scroll-up'
 
 export default class ScrollButton extends React.Component {
-    constructor() {
-        super();
-
-        this.state = {
-            intervalId: 0
-        };
-    }
-
-    scrollStep() {
-        const scrollStepInPx = 50;
-        if (window.pageYOffset === 0) {
-            clearInterval(this.state.intervalId);
-            this.setState({ intervalId: 0 });
-        }
-        window.scroll(0, window.pageYOffset - scrollStepInPx);
-    }
-
-    scrollToTop() {
-        const delayInMs = 5;
-        let intervalId = setInterval(this.scrollStep.bind(this), delayInMs);
-        this.setState({ intervalId: intervalId });
-    }
 
     render () {
         return (
-            <button
-                title={tt('g.back_to_top')}
-                className='ScrollButton'
-                onClick={ () => { this.scrollToTop(); }}
-            >
-                <Icon name="arrow" className="arrow-up" size="3x"/>
-            </button>
+            <ScrollToTop showUnder={160}>
+                <span className='ScrollButton' title={tt('g.back_to_top')}>
+                    <Icon name="arrow" className="arrow-up" size="3x"/>
+                </span>
+            </ScrollToTop>
         );
     }
 }

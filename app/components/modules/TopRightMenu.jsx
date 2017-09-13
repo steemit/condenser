77591ef2376a035ee838d179eac4d04085197cfc
pavelit <info@simdo.ru>
2017-09-13 12:52:46 +0300

    Add golos fest button

--- app/components/modules/TopRightMenu.jsx
+++ app/components/modules/TopRightMenu.jsx
@@ -78,6 +78,7 @@ function TopRightMenu({account, savings_withdraws, price_per_golos, globalprops,
     const submitStoryPencil = $STM_Config.read_only_mode ? null : <li className="show-for-small-only">
       <Link to="/submit.html"><Icon name="pencil" /></Link>
     </li>;
+    const golosFest = <li className={lcn + ' buttons'}><Link to="/ru--golos/@aroundb/3ev66k-golos-fest-ukraine-2017-pervye-spikery-festivalya" className="button alert fest">{tt('g.golos_fest')}</Link></li>;
     const feedLink = `/@${username}/feed`;
     const repliesLink = `/@${username}/recent-replies`;
     const walletLink = `/@${username}/transfers`;
@@ -158,6 +159,7 @@ function TopRightMenu({account, savings_withdraws, price_per_golos, globalprops,
         ];
         return (
             <ul className={mcn + mcl}>
+                {!inIco && golosFest}
                 {inIco && ico_menu.map((o,i) => {return <li key={i} className={lcn}><a href={o.link}>{o.value}</a></li>})}
                 {!inIco && aboutItem}
                 {!inIco && !vertical && submitFeedback}
@@ -187,6 +189,7 @@ function TopRightMenu({account, savings_withdraws, price_per_golos, globalprops,
     }
     return (
         <ul className={mcn + mcl}>
+            {!inIco && golosFest}
             {inIco && ico_menu.map((o,i) => {return <li key={i} className={lcn}><a href={o.link}>{o.value}</a></li>})}
             {!inIco && aboutItem}
             {!inIco && !vertical && <li>

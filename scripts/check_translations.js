810368adb57dedfbdc2633e568e67c776539aed9
valzav <i@valzav.com>
2017-09-14 15:28:14 -0400

    match FormattedHTMLMessage lines

--- scripts/check_translations.js
+++ scripts/check_translations.js
@@ -41,12 +41,12 @@ function loadTranslationFiles(path) {
 function processFile(used_keys, path) {
     const lines = fs.readFileSync(path, 'utf8').split(/\r?\n/);
     for (const l of lines) {
-        const tts = l.match(/(tt\([\"\'\.\-\_\w]+)/g);
+        const tts = l.match(/(tt\([\"\'\.\-\_\w]+)/g) || l.match(/(FormattedHTMLMessage.+id\=[\"\'\.\-\_\w]+)/g);
         if (tts) {
             // if(tts.length > 1) console.log('-- tt -->', path, l, tts.length, JSON.stringify(tts, null, 4));
             for (const t of tts) {
                 if (t === 'tt(id') continue; // this is exception
-                const m = t.match(/tt\([\'\"]([\.\-\_\w]+)/);
+                const m = t.match(/tt\([\'\"]([\.\-\_\w]+)/) || t.match(/id\=[\'\"]([\.\-\_\w]+)[\'\"]/);
                 if (!m) throw new Error('Wrong format: "' + t + '" in "' + l + '"');
                 const key = m[1];
                 if (used_keys[key]) used_keys[key] += 1;

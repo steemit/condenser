5c20f0604075447802901cd35b622c79fc48950b
Donovan Walker <donovan@donovanwalker.com>
2017-09-23 16:51:03 -0500

    notif - Notification List items using connect() using app.yotifications

---
+++ src/app/components/elements/notification/type.js
@@ -0,0 +1,8 @@
+//notify even types
+export const COMMENT_REPLY = 'commentReply';
+export const POST_REPLY = 'postReply';
+export const RECEIVE_STEEM = 'receiveSteem';
+export const RESTEEM = 'resteem';
+export const TAG = 'tag';
+export const VOTE = 'vote';
+

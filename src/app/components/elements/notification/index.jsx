9ef7976c7b45f81c51f7fbc41876847411e70421
Donovan Walker <donovan@donovanwalker.com>
2017-10-04 15:16:22 -0500

    notif - badge stroke/fill color for unread notifications

--- src/app/components/elements/notification/index.jsx
+++ src/app/components/elements/notification/index.jsx
@@ -4,8 +4,10 @@ import Notification from './Notification'
 export const makeNotificationList = notifications => {
     const notificationList = []
     notifications.forEach( notification => {
-        let classNames = "item" + ( notification.read ? '' : ' unread' )
-        notificationList.push(<li className={ classNames } key={notification.id}><Notification { ...notification } /></li> )
+        if(!notification.hide) {
+            let classNames = "item" + ( notification.read ? '' : ' unread' )
+            notificationList.push(<li className={ classNames } key={notification.id}><Notification { ...notification } /></li> )
+        }
     })
     return notificationList
 }

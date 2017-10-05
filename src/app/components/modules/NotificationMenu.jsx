a7fb2c6260150e50ffa6cdda60825c81f4afc739
Benjamin Chodoroff <bc@thermitic.net>
2017-10-05 10:48:07 -0400

    for dev only: load test data into store

--- src/app/components/modules/NotificationMenu.jsx
+++ src/app/components/modules/NotificationMenu.jsx
@@ -19,11 +19,16 @@ class NotificationMenu extends React.Component {
         ]),
         markAllRead: React.PropTypes.func,
         unreadNotifications: React.PropTypes.array,
+        getSomeGetSomeGetSomeYeahYeah: React.PropTypes.func, // Todo: for dev only! Do not merge if present!
     }
 
     markAllRead = (e) => {
         e.preventDefault()
         this.props.markAllRead()
+    }
+
+    loadTestData = () => { // Todo: for dev only! Do not merge if present!
+        this.props.getSomeGetSomeGetSomeYeahYeah();
     }
 
     closeMenu = (e) => {
@@ -40,6 +45,7 @@ class NotificationMenu extends React.Component {
             <li className="title">{tt('g.notifications')}
                 <span className="controls-right">
                     <button className="ptc" onClick={ this.markAllRead }>Mark All as Read</button>
+                    <button className="ptc" onClick={ this.loadTestData }>Load test data</button> {/* Todo: for dev only! Do not merge if present!*/}
                     <Link href={account_link}><Icon name="cog" /></Link>
                 </span>
             </li>
@@ -60,6 +66,22 @@ export default connect(
         }
     },
     dispatch => ({
+        getSomeGetSomeGetSomeYeahYeah: () => {  // Todo: for dev only! Do not merge if present!
+            dispatch({
+                type: 'notification/RECEIVE_ALL',
+                payload: [
+                    {"id":"UID","read":true,"shown":true,"notificationType":"powerDown","created":"2010-09-19T16:19:48","author":"roadscape","amount":10000.2},
+                    {"id":"UID1","read":false,"shown":false,"notificationType":"resteem","created":"2010-08-19T18:59:00","author":"roadscape","item":{"author":"wolfcat","category":"introduceyourself","depth":0,"permlink":"from-the-hills-of-ireland-to-planet-steem-a-wolfy-hello","summary":"From the Hills of Ireland to Planet Steem, A Wolfy Hello!"}},
+                    {"id":"UID2","read":false,"shown":true,"notificationType":"vote","notificationTime":3,"author":"beanz","created":"2017-09-19T18:59:00","item":{"author":"wolfcat","category":"introduceyourself","depth":0,"permlink":"from-the-hills-of-ireland-to-planet-steem-a-wolfy-hello","summary":"From the Hills of Ireland to Planet Steem, A Wolfy Hello!"}},
+                    {"id":"UID3","read":true,"shown":true,"notificationType":"receiveSteem","created":"2017-09-19T16:19:48","author":"roadscape","amount":10000.2},
+                    {"id":"UID4","read":true,"shown":true,"notificationType":"tag","created":"2020-09-19T07:48:03","author":"lovejoy","item":{"author":"lovejoy","category":"introduceyourself","depth":2,"permlink":"re-steemcleaners-re-steemcleaners-re-wolfcat-from-the-hills-of-ireland-to-planet-steem-a-wolfy-hello-20170919t120245144z","summary":"@wolfcat is a new user who normally doesn't spend a lot of time online, plus we are "},"rootItem":{"author":"wolfcat","category":"introduceyourself","permlink":"from-the-hills-of-ireland-to-planet-steem-a-wolfy-hello","summary":"From the Hills of Ireland to Planet Steem, A Wolfy Hello!"}},
+                    {"id":"UID5","read":false,"shown":false,"notificationType":"vote","created":"2020-11-19T11:59:39","author":"roadscape","item":{"author":"wolfcat","category":"introduceyourself","depth":0,"permlink":"from-the-hills-of-ireland-to-planet-steem-a-wolfy-hello","summary":"From the Hills of Ireland to Planet Steem, A Wolfy Hello!"}},
+                    {"id":"UID6","read":true,"shown":true,"notificationType":"postReply","created":"2017-09-19T14:24:51","author":"lovejoy","item":{"author":"lovejoy","category":"introduceyourself","depth":2,"permlink":"re-steemcleaners-re-steemcleaners-re-wolfcat-from-the-hills-of-ireland-to-planet-steem-a-wolfy-hello-20170919t120245144z","summary":"@wolfcat is a new user who normally doesn't spend a lot of time online, plus we are ","parentSummary":"You may want to retract your votes.The account has ignored our many requests to confirm the identity. It seems to be another case of fake identity. Thanks."},"rootItem":{"author":"wolfcat","category":"introduceyourself","permlink":"from-the-hills-of-ireland-to-planet-steem-a-wolfy-hello","summary":"From the Hills of Ireland to Planet Steem, A Wolfy Hello!"}},
+                    {"id":"UID6.1","read":true,"shown":true,"notificationType":"securityNewMobileDevice","created":"2017-09-19T14:24:51","author":"security"},
+                    {"id":"UID7","read":false,"shown":true,"notificationType":"commentReply","created":"2017-09-18T17:21:18","author":"dbzfan4awhile","item":{"author":"dbzfan4awhile","category":"introduceyourself","depth":3,"permlink":"re-wolfcat-re-dbzfan4awhile-re-wolfcat-from-the-hills-of-ireland-to-planet-steem-a-wolfy-hello-20170918t172118886z","summary":"Awesome!","parentSummary":"Yes! Ill look for you there :)"},"rootItem":{"author":"wolfcat","category":"introduceyourself","permlink":"from-the-hills-of-ireland-to-planet-steem-a-wolfy-hello","summary":"From the Hills of Ireland to Planet Steem, A Wolfy Hello!"}}
+                ],
+            });
+        },
         markAllRead: () => {
             const action = {
                 type: 'notification/MARK_ALL_READ',

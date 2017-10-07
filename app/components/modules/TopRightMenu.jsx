6f1195d014f625c0d03a4adc2d995b3515004ff3
pavelit <info@simdo.ru>
2017-10-07 12:12:16 +0300

    Update fest url

--- app/components/modules/TopRightMenu.jsx
+++ app/components/modules/TopRightMenu.jsx
@@ -78,7 +78,7 @@ function TopRightMenu({account, savings_withdraws, price_per_golos, globalprops,
     const submitStoryPencil = $STM_Config.read_only_mode ? null : <li className="show-for-small-only">
       <Link to="/submit.html"><Icon name="pencil" /></Link>
     </li>;
-    const golosFest = <li className={lcn + ' buttons'}><Link to="/ru--golos/@ituber/anons-golos-fest-ukraine-s-natalei-gavrilenko-kiev-13-10-2017" className="button alert fest">{tt('g.golos_fest')}</Link></li>;
+    const golosFest = <li className={lcn + ' buttons'}><Link to="/ru--blokcheijn/@aroundb/programma-golos-fest-ukraine-konstruktivnyi-dialog-media-s-innovatorami" className="button alert fest">{tt('g.golos_fest')}</Link></li>;
     const feedLink = `/@${username}/feed`;
     const repliesLink = `/@${username}/recent-replies`;
     const walletLink = `/@${username}/transfers`;

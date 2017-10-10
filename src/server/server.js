e628465f4929010e19bdea0aa366572681c031db
Donovan Walker <donovan@donovanwalker.com>
2017-10-09 19:20:58 -0500

    1800 - re-remove server-side redirect on login

--- src/server/server.js
+++ src/server/server.js
@@ -82,12 +82,6 @@ app.use(function* (next) {
         return;
     }
 
-    // redirect to home page/feed if known account
-    if (this.method === 'GET' && this.url === '/' && this.session.a) {
-        this.status = 302;
-        this.redirect(`/@${this.session.a}/feed`);
-        return;
-    }
     // normalize user name url from cased params
     if (
         this.method === 'GET' &&

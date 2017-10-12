7ecd0d5b83be65d4505f543516888e46880469ee
Donovan Walker <donovan@donovanwalker.com>
2017-10-12 15:25:36 -0500

    notif - eslint

--- src/app/components/elements/YotifiCounter.jsx
+++ src/app/components/elements/YotifiCounter.jsx
@@ -2,15 +2,21 @@ import React from 'react';
 import {connect} from 'react-redux';
 
 class YotifiCounter extends React.Component {
-    static propTypes = {
-        value: React.PropTypes.number
-    };
 
     render() {
         const value = this.props.value;
         if (!value) return null;
         return <div className="NotifiCounter">{value}</div>;
     }
+}
+
+YotifiCounter.propTypes = {
+    value: React.PropTypes.number
+}
+
+
+YotifiCounter.defaultProps = {
+    value: 0
 }
 
 export default connect(

948d582d119e8d3d931f176d86ad7cc86daff744
pavelit <info@simdo.ru>
2017-10-18 19:42:31 +0300

    Update golosFest button

--- app/components/modules/TopRightMenu.jsx
+++ app/components/modules/TopRightMenu.jsx
@@ -78,7 +78,7 @@ function TopRightMenu({account, savings_withdraws, price_per_golos, globalprops,
     const submitStoryPencil = $STM_Config.read_only_mode ? null : <li className="show-for-small-only">
       <Link to="/submit.html"><Icon name="pencil" /></Link>
     </li>;
-    const golosFest = <li className={lcn + ' buttons'}><Link to="/ru--blokcheijn/@aroundb/programma-golos-fest-ukraine-konstruktivnyi-dialog-media-s-innovatorami" className="button alert fest">{tt('g.golos_fest')}</Link></li>;
+    const golosFest = <li className={lcn + ' buttons'}><Link to="/created/ru--godgolosu" className="button alert fest">{tt('g.golos_fest')}</Link></li>;
     const feedLink = `/@${username}/feed`;
     const repliesLink = `/@${username}/recent-replies`;
     const walletLink = `/@${username}/transfers`;

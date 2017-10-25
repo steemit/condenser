f1340a4b8813fe9ac8753621233d851cbfe05916
Donovan Walker <donovan@donovanwalker.com>
2017-10-25 12:52:15 -0500

    809 - best not to code jsx in js...

--- src/app/Main.js
+++ src/app/Main.js
@@ -90,7 +90,7 @@ function runApp(initial_state) {
     }
 
     const location = `${window.location.pathname}${window.location.search}${window.location.hash}`;
-    universalRender({history, location, initial_state, setStore: { setStore }})
+    universalRender({history, location, initial_state, setStore })
     .catch(error => {
         console.error(error);
         serverApiRecordEvent('client_error', error);

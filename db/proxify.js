812d9b717415007351ef7de38d399444924b5a87
pavelit <info@simdo.ru>
2017-10-26 11:47:30 +0300

    Add proxifier logic

---
+++ db/proxify.js
@@ -0,0 +1,30 @@
+async function proxify(method, context, proxy, lifetime /*, options */) {
+  const options = [].slice.call(arguments).splice(4);
+  const proxyKey = method + JSON.stringify(options);
+  console.log('proxyKey', proxyKey);
+  let res = [];
+  try {
+    const cache = await proxy.call('chaindb_get', proxyKey);
+    if (cache && cache.length >= 1)
+      res = cache[0].slice(2);
+  }
+  catch (e) {
+    console.error('-- /api/v1/proxy/method error -->', method, e.message);
+  }
+  if (typeof res === 'object' && res.length) {
+    res = res[0];
+  }
+  else {
+    console.log('CHAIN.Get');
+    if (typeof options[0] !== 'undefined') {
+      res = await context[method].apply(context, options);
+    }
+    else {
+      res = await context[method].apply(context);
+    }
+    await proxy.call('chaindb_set', lifetime, proxyKey, res);
+  }
+  return res;
+}
+
+export default proxify;

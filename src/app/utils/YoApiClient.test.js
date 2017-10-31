f3400131d4e0f6fe27ec2ecb6825fc95a310e6e1
Benjamin Chodoroff <bc@thermitic.net>
2017-10-31 08:34:08 -0400

    add test coverage for YoApiClient.normalize

--- src/app/utils/YoApiClient.test.js
+++ src/app/utils/YoApiClient.test.js
@@ -8,6 +8,7 @@ import { fromJS, Set } from 'immutable';
 import {
     normalizeSettingsFromApi,
     denormalizeSettingsToApi,
+    normalize,
 } from './YoApiClient';
 
 chai.use(chaiImmutable);
@@ -73,45 +74,120 @@ const apiMockData = {
             }
         },
     },
+    get_notifications: {
+        jsonrpc: '2.0',
+        id: 1,
+        result: [
+            {
+                notify_id: 39,
+                notify_type: 'power_down',
+                created: '2017-10-27T01:31:29.382749',
+                updated: '2017-10-27T01:31:29.382749',
+                read: false,
+                shown: false,
+                username: 'test_user',
+                data: {
+                    author: 'roadscape',
+                    amount: 10000.2
+                }
+            },
+            {
+                notify_id: 55,
+                notify_type: 'power_down',
+                created: '2017-10-27T01:15:29.383842',
+                updated: '2017-10-27T01:15:29.383842',
+                read: false,
+                shown: false,
+                username: 'test_user',
+                data: {
+                    author: 'roadscape',
+                    amount: 10000.2,
+                    item: {
+                        parent_summary: 'whatever',
+                    },
+                }
+            }
+        ]
+    },
 };
 
 const desired = {
-    email: {
-        notification_types: {
-            security: true,
-            wallet: true,
-            postReplies: true,
-            commentReplies: false,
-            mentions: false,
-            newPosts: false,
-            resteems: false
+    settings: {
+        email: {
+            notification_types: {
+                security: true,
+                wallet: true,
+                postReplies: true,
+                commentReplies: false,
+                mentions: false,
+                newPosts: false,
+                resteems: false
+            },
+            sub_data: 'test@example.com',
         },
-        sub_data: 'test@example.com',
-    },
-    wwwpoll: {
-        notification_types: {
-            security: true,
-            wallet: true,
-            postReplies: false,
-            commentReplies: false,
-            mentions: true,
-            newPosts: true,
-            resteems: true,
+        wwwpoll: {
+            notification_types: {
+                security: true,
+                wallet: true,
+                postReplies: false,
+                commentReplies: false,
+                mentions: true,
+                newPosts: true,
+                resteems: true,
+            },
+            sub_data: '',
         },
-        sub_data: '',
     },
+    get_notifications: [
+        {
+            created: '2017-10-27T01:31:29.382749',
+            data: {
+                amount: 10000.2,
+                author: 'roadscape',
+            },
+            id: '39',
+            notificationType: 'power_down',
+            read: false,
+            shown: false,
+            updated: '2017-10-27T01:31:29.382749',
+            username: 'test_user',
+        },
+        {
+            created: '2017-10-27T01:15:29.383842',
+            data: {
+                amount: 10000.2,
+                author: 'roadscape',
+                item: {
+                    parentSummary: 'whatever',
+                }
+            },
+            id: '55',
+            notificationType: 'power_down',
+            read: false,
+            shown: false,
+            updated: '2017-10-27T01:15:29.383842',
+            username: 'test_user',
+        },
+    ],
 };
 
 describe('normalizeSettingsFromApi', () => {
     it('should take api output & turn it into something useful in the frontend', () => {
         const normalized = normalizeSettingsFromApi(apiMockData.get_transports.result);
-        expect(normalized.toJS()).to.deep.equal(desired);
+        expect(normalized.toJS()).to.deep.equal(desired.settings);
     });
 });
 
 describe('denormalizeSettingsToApi', () => {
     it('should take redux state and turn it into something the api can gobble up', () => {
-        const denormalized = denormalizeSettingsToApi(fromJS(desired));
+        const denormalized = denormalizeSettingsToApi(fromJS(desired.settings));
         expect(denormalized).to.deep.equal(apiMockData.saved_transports.result);
+    });
+});
+
+describe('normalize', () => {
+    it('should clean up incoming notification data', () => {
+        const normalized = normalize(apiMockData.get_notifications.result);
+        expect(normalized).to.deep.equal(desired.get_notifications);
     });
 });

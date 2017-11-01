83b4cf4b3d176591c8cdb01aab9c62bc73ad154c
Donovan Walker <donovan@donovanwalker.com>
2017-11-01 12:49:50 +0000

    1890 - add size support to Userpic

--- src/app/components/elements/Userpic.jsx
+++ src/app/components/elements/Userpic.jsx
@@ -3,26 +3,41 @@ import { connect } from 'react-redux';
 import shouldComponentUpdate from 'app/utils/shouldComponentUpdate';
 import { imageProxy } from 'app/utils/ProxifyUrl';
 
+export const SIZE_SMALL = 'small';
+export const SIZE_MED = 'medium';
+export const SIZE_LARGE = 'large';
+
+const sizeList = [SIZE_SMALL, SIZE_MED, SIZE_LARGE]
+
+export const avatarSize = {
+    small: SIZE_SMALL,
+    medium: SIZE_MED,
+    large: SIZE_LARGE
+};
+
 class Userpic extends Component {
 
     shouldComponentUpdate = shouldComponentUpdate(this, 'Userpic')
 
     render() {
-        const {account, json_metadata} = this.props
+        const {account, json_metadata, size} = this.props
         const hideIfDefault = this.props.hideIfDefault || false
+        const avSize = (size && sizeList.indexOf(size) > -1)? '/' + size : '';
 
 
         // try to extract image url from users metaData
-        try {
-            const md = JSON.parse(json_metadata);
-            if(!/^(https?:)\/\//.test(md.profile.profile_image) && hideIfDefault) {
+        if(hideIfDefault) {
+            try {
+                const md = JSON.parse(json_metadata);
+                if (!/^(https?:)\/\//.test(md.profile.profile_image)) {
+                    return null;
+                }
+            } catch (e) {
                 return null;
             }
-        } catch (e) {
-            /* eslint-disable-line no-empty */
         }
 
-        const style = {backgroundImage: 'url(' + imageProxy() + `u/${account}/avatar)` };
+        const style = {backgroundImage: 'url(' + imageProxy() + `u/${account}/avatar${avSize})` };
 
         return (<div className="Userpic" style={style} />)
     }

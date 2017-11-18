2ae488c99cefe8e156acb74d1a680b023cf012aa
Originate <originated@users.noreply.github.com>
2017-11-17 22:56:19 -0800

    Correct issue with naming on meta tags not rendering properly

--- src/app/utils/ExtractMeta.js
+++ src/app/utils/ExtractMeta.js
@@ -41,15 +41,15 @@ export default function extractMeta(chain_data, rp) {
             metas.push({name: 'description', content: desc});
 
             // Open Graph data
-            metas.push({property: 'og:title',        content: title});
-            metas.push({property: 'og:type',         content: 'article'});
-            metas.push({property: 'og:url',          content: url});
-            metas.push({property: 'og:image',        content: image || 'https://steemit.com/images/steemit.png'});
-            metas.push({property: 'og:description',  content: desc});
-            metas.push({property: 'og:site_name',    content: 'Steemit'});
-            metas.push({property: 'fb:app_id',       content: $STM_Config.fb_app});
-            metas.push({property: 'article:tag',     content: category});
-            metas.push({property: 'article:published_time', content: created});
+            metas.push({name: 'og:title',        content: title});
+            metas.push({name: 'og:type',         content: 'article'});
+            metas.push({name: 'og:url',          content: url});
+            metas.push({name: 'og:image',        content: image || 'https://steemit.com/images/steemit.png'});
+            metas.push({name: 'og:description',  content: desc});
+            metas.push({name: 'og:site_name',    content: 'Steemit'});
+            metas.push({name: 'fb:app_id',       content: $STM_Config.fb_app});
+            metas.push({name: 'article:tag',     content: category});
+            metas.push({name: 'article:published_time', content: created});
 
             // Twitter card data
             metas.push({name: 'twitter:card',        content: image ? 'summary_large_image' : 'summary'});

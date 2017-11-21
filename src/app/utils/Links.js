7a48c2fc6cc5b440a05398f5242e2e4c3719379b
Donovan Walker <donovan@donovanwalker.com>
2017-11-21 15:45:05 -0600

    #1989 PickAccount passes view_mode param to enter_email page

--- src/app/utils/Links.js
+++ src/app/utils/Links.js
@@ -35,6 +35,22 @@ export default {
     ipfsPrefix: /(https?:\/\/.*)?\/ipfs/i,
 }
 
+//possible this should go somewhere else.
+export const makeParams = (params, prefix) => {
+    let paramsList = [];
+    if(params.constructor === Array) {
+        paramsList = params;
+    } else {
+        Object.entries(params).forEach(([key, value]) => {
+            paramsList.push(`${key}=${value}`);
+        });
+    }
+    if(paramsList.length > 0) {
+        return ((prefix)? ((typeof prefix === 'string')? prefix : '?') : '') + paramsList.join('&');
+    }
+    return '';
+}
+
 // Original regex
 // const urlRegex = '^(?!mailto:)(?:(?:http|https|ftp)://)(?:\\S+(?::\\S*)?@)?(?:(?:(?:[1-9]\\d?|1\\d\\d|2[01]\\d|22[0-3])(?:\\.(?:1?\\d{1,2}|2[0-4]\\d|25[0-5])){2}(?:\\.(?:[0-9]\\d?|1\\d\\d|2[0-4]\\d|25[0-4]))|(?:(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)(?:\\.(?:[a-z\\u00a1-\\uffff0-9]+-?)*[a-z\\u00a1-\\uffff0-9]+)*(?:\\.(?:[a-z\\u00a1-\\uffff]{2,})))|localhost)(?::\\d{2,5})?(?:(/|\\?|#)[^\\s]*)?$';
 

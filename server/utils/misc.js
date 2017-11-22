8e84e869d11d4c1c18abec005db8fa7ac6d4df00
pavelit <info@simdo.ru>
2017-11-22 11:05:54 +0300

    Add limit seconds as option

--- server/utils/misc.js
+++ server/utils/misc.js
@@ -9,14 +9,15 @@ function getRemoteIp(req) {
 }
 
 var ip_last_hit = new Map();
-function rateLimitReq(ctx, req) {
+function rateLimitReq(ctx, req, limit) {
+    limit = limit ? limit : 1
     const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
     const now = Date.now()
 
     // purge hits older than minutes_max
     ip_last_hit.forEach((v, k) => {
         const seconds = (now - v) / 1000;
-        if (seconds > 1) {
+        if (seconds > limit) {
             ip_last_hit.delete(ip)
         }
     })
@@ -26,7 +27,7 @@ function rateLimitReq(ctx, req) {
     if (ip_last_hit.has(ip)) {
         // console.log(`api rate limited for ${ip}: ${req}`);
         // throw new Error(`Rate limit reached: one call per ${minutes_max} minutes allowed.`);
-        console.error(`Rate limit reached: one call per 1 second allowed.`);
+        console.error(`Rate limit reached: one call per ${limit} second allowed.`);
         ctx.status = 429;
         ctx.body = 'Too Many Requests';
         result = true;

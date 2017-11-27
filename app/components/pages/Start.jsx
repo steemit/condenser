ccffda49f01eaae4ac8b05eeb51241bd3eab841d
Nick Shtefan <nick.shtefan@gmail.com>
2017-11-27 23:44:01 +0300

    Landing page /start

--- app/components/pages/Start.jsx
+++ app/components/pages/Start.jsx
@@ -3,9 +3,150 @@ import React from 'react';
 class Start extends React.Component {
     render() {
         return (
-            <div className="row">
-                <div className="column large-8 medium-10 small-12">
-                    <h1>START</h1>
+            <div className='landing-start'>
+                <div className='landing-start-block'>
+                    <div className='column large-12 medium-12 small-12'>
+                        <h1>Golos.io</h1>
+                        <h2>Блог-платформа следующего поколения</h2>
+                    </div>
+                    <div className='row'>
+                        <div className='column small-6 medium-6 large-6'>
+                            <div>
+                                <iframe
+                                    width="400"
+                                    height="220"
+                                    src="https://www.youtube.com/embed/8a0TPACOu2k"
+                                    frameBorder="0"
+                                    allowFullScreen/>
+                            </div>
+                        </div>
+                        <div className='column small-6 medium-6 large-6'>
+                            <p className='landing-start-block-text'>Пишите, фотографируйте, комментируйте и
+                                получайте вознаграждение за любое действие</p>
+                            <a href="/create_account" className="button">Создать аккаунт</a>
+                        </div>
+                    </div>
+                </div>
+                <div className='landing-start-block'>
+                    <div className='column large-12 medium-12 small-12'>
+                        <h2>Как это работает?</h2>
+                        <div className='column large-12 medium-12 small-12'>
+                            <hr/>
+                        </div>
+                        <div className='column large-12 medium-12 small-12'>
+                            <div className='row'>
+                                <div className='column large-12 medium-12 small-12'>
+                                    <h3>Простой ответ / Сложный ответ</h3>
+                                </div>
+                                <div className='column large-12 medium-12 small-12'>
+                                    <p className='landing-start-block-text'>
+                                        Golos.io — это блог-платформа на блокчейне*, где пользователи могут публиковать
+                                        записи и голосовать за понравившиеся статьи. Голосуя за понравившуюся статью,
+                                        они вознаграждают ее своими «голосами», которые можно перевести в реальные
+                                        деньги.
+                                    </p>
+                                </div>
+                                <div className='landing-start-block-center column large-12 medium-12 small-12'>
+                                    <a href="/create_account" className="button">Создать аккаунт</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='landing-start-block'>
+                    <div className='column large-12 medium-12 small-12'>
+                        <h2>Иконки</h2>
+                        <div className='landing-start-block-center column large-12 medium-12 small-12'>
+                            <a href="/create_account" className="button">Создать аккаунт</a>
+                        </div>
+                    </div>
+                </div>
+                <div className='landing-start-block'>
+                    <div className='column large-12 medium-12 small-12'>
+                        <h2>О нас говорят</h2>
+                        <div className='column large-12 medium-12 small-12'>
+                            <hr/>
+                        </div>
+                        <div className='column large-12 medium-12 small-12'>
+                            <div className='row'>
+                                <div className='landing-start-person column large-3 medium-3 small-3'>
+                                    <img
+                                        className='landing-start-round-img'
+                                        src='https://imgp.golos.io/120x120/https://images.golos.io/DQmQh3z15LkKqHcBUUfZedzed6Y263rvzJ9F7gn3beUgXrR/vv_ava.jpg'/>
+                                    <p>
+                                        <b>Познер Online</b>
+                                    </p>
+                                    <a href="/@pozneronline">@pozneronline</a>
+                                </div>
+                                <div className='column large-9 medium-9 small-9'>
+                                    <p className='landing-start-block-text'>
+                                        «Считаем «Голос» перспективным проектом и постараемся привлечь на него как можно
+                                        больше пользователей»
+                                    </p>
+                                </div>
+                                <div className='landing-start-block-center column large-12 medium-12 small-12'>
+                                    <a href="/create_account" className="button">Создать аккаунт</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <div className='landing-start-block'>
+                    <div className='column large-12 medium-12 small-12'>
+                        <h2>Зарегистрируйтесь,</h2>
+                        <div className='row'>
+                            <div className='column large-12 medium-12 small-12'>
+                                <p className='landing-start-block-text landing-start-block-center'>чтобы начать
+                                    делиться своими историями, подписываться на интересных авторов, оценивать
+                                    публикации, знакомиться и получать вознаграждения</p>
+                            </div>
+                            <div className='landing-start-panel column large-5 medium-5 small-5'>
+                                <div className='row'>
+                                    <div className='column large-2 medium-2 small-2'>
+                                        <h1>1</h1>
+                                    </div>
+                                    <div className='column large-10 medium-10 small-10'>
+                                        <h3 className=''>
+                                            Регистрация бесплатна
+                                        </h3>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='column large-2 medium-2 small-2'></div>
+                            <div className='landing-start-panel column large-5 medium-5 small-5'>
+                                <div className='row'>
+                                    <div className='column large-2 medium-2 small-2'>
+                                        <h1>2</h1>
+                                    </div>
+                                    <div className='column large-10 medium-10 small-10'>
+                                        <h3 className=''>
+                                            Вознаграждаем с первой публикации
+                                        </h3>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='landing-start-block-center column large-12 medium-12 small-12'>
+                        <a href="/create_account" className="button">Создать аккаунт</a>
+                    </div>
+                </div>
+                <div className='landing-start-block'>
+                    <div className='column large-12 medium-12 small-12'>
+                        <h2>Остались вопросы?</h2>
+                        <div className='column large-12 medium-12 small-12'>
+                            <hr/>
+                        </div>
+                        <div className='row'>
+                            <div className='column large-12 medium-12 small-12'>
+                                <p className='landing-start-block-text landing-start-block-center'>чтобы начать
+                                    У нас есть собственная Википедия, со всей информацией про блог-платформу
+                                    Golos.io. Кроме того, мы оперативно ответим на любой технический вопрос в нашей
+                                    группе в Телеграме.</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         );

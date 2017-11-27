05f6fa4dad6d24f50bedd0dc9d58772f9f4154e2
Nick Shtefan <nick.shtefan@gmail.com>
2017-11-27 15:19:11 +0300

    Init. Базовый роутинг, пустая страница

---
+++ app/components/pages/Start.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Start extends React.Component {
+    render() {
+        return (
+            <div className="row">
+                <div className="column large-8 medium-10 small-12">
+                    <h1>START</h1>
+                </div>
+            </div>
+        );
+    }
+}
+
+module.exports = {
+    path: 'start',
+    component: Start
+};

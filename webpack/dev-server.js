e17c623bfe895c8f7bd7c1b6cd93ce3734c52b56
Iain Maitland <imaitland@gmail.com>
2017-11-27 22:02:53 -0500

    stradler

--- webpack/dev-server.js
+++ webpack/dev-server.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 if(!fs.existsSync('tmp'))
     fs.mkdirSync('tmp');
 
-process.env.BABEL_ENV = 'browser';
+// process.env.BABEL_ENV = 'browser';
 // process.env.NODE_ENV = 'development';
 
 import Koa from 'koa';

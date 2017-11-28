07b96781f7d43c65186bd6bd82d11dc6e4300c2c
Nick Shtefan <nick.shtefan@gmail.com>
2017-11-28 17:35:51 +0300

    Toggle answers, more styles, more links

--- app/components/pages/Start.jsx
+++ app/components/pages/Start.jsx
@@ -1,7 +1,38 @@
 import React from 'react';
 
 class Start extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            simple: true
+        }
+        this.toggleAnswer = this
+            .toggleAnswer
+            .bind(this);
+    }
+
+    toggleAnswer(e, answr) {
+        e.preventDefault()
+        this.setState({simple: answr})
+    }
+
     render() {
+
+        let {simple} = this.state
+        let simpleAnswer = <p className='landing-start-block-text'>
+            <b>Golos.io</b>
+            — это блог-платформа на блокчейне*, где пользователи могут публиковать записи и
+            голосовать за понравившиеся статьи. Голосуя за понравившуюся статью, они
+            вознаграждают ее своими «голосами», которые можно перевести в реальные деньги.
+        </p>
+
+        let difAnswer = <p className='landing-start-block-text'>
+            Каждый день мы отдаем все намайненое** в пул вознаграждения авторов. Он
+            распределяется в зависимости от количества «голосов» пользователей под каждой
+            публикацией.
+        </p>
+
         return (
             <div className='landing-start'>
                 <div className='landing-start-block'>
@@ -36,15 +67,14 @@ class Start extends React.Component {
                         <div className='column large-12 medium-12 small-12'>
                             <div className='row'>
                                 <div className='column large-12 medium-12 small-12'>
-                                    <h3>Простой ответ / Сложный ответ</h3>
+                                    <h3>
+                                        <a href='#' onClick={(e) => this.toggleAnswer(e, true)}>Простой ответ</a>
+                                        /
+                                        <a href='#' onClick={(e) => this.toggleAnswer(e, false)}>Сложный ответ</a>
+                                    </h3>
                                 </div>
                                 <div className='column large-12 medium-12 small-12'>
-                                    <p className='landing-start-block-text'>
-                                        <b>Golos.io</b>
-                                        — это блог-платформа на блокчейне*, где пользователи могут публиковать записи и
-                                        голосовать за понравившиеся статьи. Голосуя за понравившуюся статью, они
-                                        вознаграждают ее своими «голосами», которые можно перевести в реальные деньги.
-                                    </p>
+                                {simple ? simpleAnswer : difAnswer}
                                 </div>
                                 <div className='landing-start-block-center column large-12 medium-12 small-12'>
                                     <a href="/create_account" className="button">Создать аккаунт</a>
@@ -58,7 +88,7 @@ class Start extends React.Component {
                         <div className='row'>
 
                             <div className='column large-3 medium-3 small-3'>
-                            <p className='landing-start-block-center'><img
+                                <p className='landing-start-block-center'><img
                                     className='landing-start-round-img'
                                     src='/images/landing/start/icons_01.png'/></p>
                                 <h5>Социальная значимость</h5>
@@ -66,11 +96,12 @@ class Start extends React.Component {
 
                             </div>
                             <div className='column large-3 medium-3 small-3'>
-                            <p className='landing-start-block-center'><img
+                                <p className='landing-start-block-center'><img
                                     className='landing-start-round-img'
                                     src='/images/landing/start/icons_02.png'/></p>
                                 <h5>Выгодно для блогинга</h5>
-                                <p className='landing-start-block-icon-text'>Мы вознаграждаем как авторов, так и читателей  — тех, кто оценивает и комментирует публикации</p>
+                                <p className='landing-start-block-icon-text'>Мы вознаграждаем как авторов, так и
+                                    читателей — тех, кто оценивает и комментирует публикации</p>
 
                             </div>
                             <div className='column large-3 medium-3 small-3'>
@@ -78,15 +109,17 @@ class Start extends React.Component {
                                     className='landing-start-round-img'
                                     src='/images/landing/start/icons_03.png'/></p>
                                 <h5>В интересах каждого</h5>
-                                <p className='landing-start-block-icon-text'>Писать можно обо всем на свете — расскажите о ракетостроении или выложите фотографию любимого кота</p>
+                                <p className='landing-start-block-icon-text'>Писать можно обо всем на свете —
+                                    расскажите о ракетостроении или выложите фотографию любимого кота</p>
 
                             </div>
                             <div className='column large-3 medium-3 small-3'>
-                            <p className='landing-start-block-center'><img
+                                <p className='landing-start-block-center'><img
                                     className='landing-start-round-img'
                                     src='/images/landing/start/icons_04.png'/></p>
                                 <h5>Инновационная модель</h5>
-                                <p className='landing-start-block-icon-text'>Golos.io работает на блокчейне, а значит информация пользователей принадлежит только им</p>
+                                <p className='landing-start-block-icon-text'>Golos.io работает на блокчейне, а
+                                    значит информация пользователей принадлежит только им</p>
 
                             </div>
 
@@ -135,7 +168,8 @@ class Start extends React.Component {
                                     делиться своими историями, подписываться на интересных авторов, оценивать
                                     публикации, знакомиться и получать вознаграждения</p>
                             </div>
-                            <div className='landing-start-panel column large-5 medium-5 small-5'>
+                            <div className='column large-1 medium-1 small-1'></div>
+                            <div className='landing-start-panel column large-4 medium-4 small-4'>
                                 <div className='row'>
                                     <div className='column large-2 medium-2 small-2'>
                                         <h1>1</h1>
@@ -148,7 +182,7 @@ class Start extends React.Component {
                                 </div>
                             </div>
                             <div className='column large-2 medium-2 small-2'></div>
-                            <div className='landing-start-panel column large-5 medium-5 small-5'>
+                            <div className='landing-start-panel column large-4 medium-4 small-4'>
                                 <div className='row'>
                                     <div className='column large-2 medium-2 small-2'>
                                         <h1>2</h1>
@@ -175,9 +209,9 @@ class Start extends React.Component {
                         <div className='row'>
                             <div className='column large-12 medium-12 small-12'>
                                 <p className='landing-start-block-text landing-start-block-center'>чтобы начать
-                                    У нас есть собственная Википедия, со всей информацией про блог-платформу
+                                    У нас есть собственная <a href='https://wiki.golos.io/'> Википедия</a>, со всей информацией про блог-платформу
                                     Golos.io. Кроме того, мы оперативно ответим на любой технический вопрос в нашей
-                                    группе в Телеграме.</p>
+                                    группе в <a href='https://t.me/golos_support'>Телеграме</a>.</p>
                             </div>
                         </div>
                     </div>

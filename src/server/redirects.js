92b07484262cd474cb16ba81b7dc9740ff6ca7eb
valzav <i@valzav.com>
2017-11-30 12:21:56 -0500

    moving redirects to nginx

--- src/server/redirects.js
+++ src/server/redirects.js
@@ -2,31 +2,6 @@ import koa_router from 'koa-router';
 
 const redirects = [
     // example: [/\/about(\d+)-(.+)/, '/about?$0:$1', 302],
-    [/^\/about.html$/, '/s/about'],
-    [/^\/welcome$/, '/s/welcome'],
-    [/^\/faq.html$/, '/s/faq'],
-    [/^\/about.html$/, '/s/about'],
-    [/^\/login.html$/, '/c/login'],
-    [/^\/privacy.html$/, '/s/privacy'],
-    [/^\/support.html$/, '/s/support'],
-    [/^\/tags\/?$/, '/c/tags'],
-    [/^\/tos.html$/, '/s/tos'],
-    [/^\/change_password$/, '/c/change-password'],
-    [/^\/create_account$/, '/c/create-account'],
-    [/^\/approval$/, '/c/approval'],
-    [/^\/pick_account$/, '/c/pick-account'],
-    [/^\/recover_account_step_1$/, '/c/recover-account-step-1'],
-    [/^\/recover_account_step_2$/, '/c/recover-account-step-2'],
-    [/^\/market$/, '/c/market'],
-    [/^\/~witnesses$/, '/c/witnesses'],
-    [/^\/submit.html$/, '/c/submit'],
-    [/^\/@([\w.\d-]+)\/?$/, '/$0'],
-    [/^\/@([\w.\d-]+)\.json$/, '/$0.json'],
-    [/^\/@([\w.\d-]+)\/([\w\d-]+)\/?$/, '/$0/$1'],
-    [/^\/([\w\d-/]+)\/@([\w.\d-]+)\/([\w\d-]+)\/?$/, '/$1/$2'],
-    [/^\/([\w\d-/]+)\/@([\w.\d-]+)\/([\w\d-]+)\.json$/, '/$1/$2.json'],
-    [/^\/(hot|votes|responses|trending|trending30|promoted|cashout|payout|payout_comments|created|active)\/?$/, '/t/all/$0'],
-    [/^\/(hot|votes|responses|trending|trending30|promoted|cashout|payout|payout_comments|created|active)\/([\w\d-]+)\/?$/, '/t/$1/$0'],
 ];
 
 export default function useRedirects(app) {

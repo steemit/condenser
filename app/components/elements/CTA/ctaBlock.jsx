9ab6babe188708b0380ba784ac76ea4e169b158b
Nick Shtefan <nick.shtefan@gmail.com>
2017-12-05 22:01:32 +0300

    CTA block. Добавлены условия показа блока. Реализована возможность показывать блок для особых постов. Округление суммы пыплат. Показ блока при условии достижения минимального значения выплат

--- app/components/elements/CTA/ctaBlock.jsx
+++ app/components/elements/CTA/ctaBlock.jsx
@@ -2,6 +2,7 @@ import React, {Component, PropTypes} from "react";
 import { connect } from 'react-redux';
 import Userpic from 'app/components/elements/Userpic';
 import {parsePayoutAmount} from 'app/utils/ParsersAndFormatters';
+import posts from './specialPostsList'
 
 
 class CTABlock extends Component {
@@ -10,32 +11,35 @@ class CTABlock extends Component {
         user: React.PropTypes.string.isRequired,
         post: React.PropTypes.string.isRequired,
         payout: React.PropTypes.number,
-        visible: React.PropTypes.bool, 
+        visible: React.PropTypes.bool,
+        isSpecial: React.PropTypes.string, 
     };
 
     constructor(props) {
         super(props);
-    }
+    }    
 
     render() {
-        let {user, post, payout, visible} = this.props
+        let {user, post, payout, visible, isSpecial} = this.props
         
+        let ctablock = <div className='ctablock'>
+        <div className='row'>
+            <div className=' column large-2 medium-2 small-2'>
+                <Userpic account={user}/>   
+            </div>
+            <div className='column large-7 medium-7 small-7'>
+                <p className='left cta-block-text'>
+                   Сообщество Golos.io высоко оценило этот пост! <a href={'/@' + user}>{user}</a> заработал более {payout} рублей.<a href={'/start'}>Делитесь своими мыслями и получайте вознаграждение.</a> 
+                </p>
+            </div>
+            <div className='column large-3 medium-3 small-3'>
+                <a href="/create_account" className="button">Создать аккаунт</a>
+            </div>
+        </div>
+        </div>
+
         return (
-            <div className='ctablock'>
-            <div className='row'>
-                <div className=' column large-2 medium-2 small-2'>
-                    <Userpic account={user}/>   
-                </div>
-                <div className='column large-7 medium-7 small-7'>
-                    <p className='left cta-block-text'>
-                       Сообщество Golos.io высоко оценило этот пост! <a href={'/@' + user}>{user}</a> заработал более {payout} рублей.<a href={'/start'}>Делитесь своими мыслями и получайте вознаграждение.</a> 
-                    </p>
-                </div>
-                <div className='column large-3 medium-3 small-3'>
-                    <a href="/create_account" className="button">Создать аккаунт</a>
-                </div>
-            </div>
-            </div>
+          visible ? ctablock : null
         )
     }
 }
@@ -45,21 +49,40 @@ export default connect(
     (state, ownProps) => {
         const post = state.global.getIn(['content', ownProps.post])
         if (!post) return ownProps
-        let user = post.get('author')
+        
         let current_account = state.user.get('current')
+        let user = post.get('author')
 
+        let link = post.get('category') + '/@' + user + '/' + post.get('permlink')
+
+        function compareLinks(specialLink, incomingLink) {
+            return specialLink == incomingLink;
+        }
+
+        function isSpecialPost(array, link){
+            for (let index = 0; index < array.length; index++) {
+                const element = array[index];
+                if(compareLinks(array[index], link)){
+                    return  array[index]
+                    break;
+                } else return null
+            }
+        }
+
+        let isSpecial = isSpecialPost(posts.posts, link)
         let pending_payout = parsePayoutAmount(post.get('pending_payout_value'))
         let total_author_payout = parsePayoutAmount(post.get('total_payout_value'))
         let total_curator_payout = parsePayoutAmount(post.get('curator_payout_value'))
 
-        let payout = pending_payout + total_author_payout + total_curator_payout
-        let visible = current_account == null
+        let payout = ((pending_payout + total_author_payout + total_curator_payout)/1000|0)*1000
+        let visible = (current_account == null) && ((payout >= 2000) || isSpecial != null)
 
         return {
             post: ownProps.post,
             user, 
             payout,
-            visible
+            visible,
+            isSpecial
         }
     }
 )(CTABlock)
---
+++ app/components/elements/CTA/specialPostsList.js
@@ -0,0 +1,25 @@
+export default { 
+    posts: [       
+            "ru--golos/@aleco/prosto-i-dostupno-o-blockchain-chto-eto-i-kak-rabotaet",
+            "ru--kriptovalyuty/@vasyl73/priekhala-rx-570-testy-dlya-maininga",
+            "ru--blokcheijn/@cryptodiyor/kak-poluchit-bitcoin-cash-obzor-koshelkov-i-birzh",
+            "ru--kriptovalyuty/@cryptosted/podborka-telegramm-kanalov-i-chatov-o-kriptovalyute-treiderstve-birzhakh-ico-maininge-investirovanii-novostei",
+            "ru--kriptovalyuty/@jurgan/perspektivy-maininga-kriptovalyuty-v-2017-godu",
+            "ru--zarabotok/@rbrown/statistika-po-pribyli-ot-maininga-zcash-na-praktike",
+            "ru--maijning/@rossomaha.xyz/slyshen-kheshei-legkikh-shelest-kriptolokh-poshel-na-nerest",
+            "bitcoin/@satoshifund/evolyuciya-satoshi-pie",
+            "ru--telekom/@abloud/novyi-tarif-khaip-ot-mts-plyusy-i-podvodnye-kamni",
+            "ru--progolosuij/@dimon85/kak-dovesti-devushku-do-struinogo-orgazma",
+            "ru--blokchein/@rolik/bitkoin-koshelki-gaid-kak-sdelat-kholodnyi-koshelok",
+            "ru--blokcheijn/@nastrom/kakaya-kriptovalyuta-yavlyaetsya-samoi-krutoi-v-2017godu",
+            "telegram/@konstantins/kak-raskrutit-telegram-kanal-s-nulya-do-populyarnogo-za-7-shagov",
+            "ethereum/@litvintech/obnovlyaetsya-multisig-koshelek-ethereum-satoshi-pie-vzloman-uyazvimost-kontrakta-multisig-by-parity",
+            "ru--kriptovalyuta/@morandi89/nimses-socialnaya-set-i-kriptovalyuta-na-odnoi-platforme-kod-0ehaemef1s-i-poluchi-1-baks",
+            "ru--golos/@bitcoinfo/samyi-polnyi-f-a-q-o-golose-spisok-luchshykh-postov-raskryvayushikh-vse-aspekty-proekta-bonusy-v-vide-kreativa",
+            "ru--blokcheijn/@tristamoff/maining-monero-na-windows-s-videokartoi-ili-bez",
+            "ru--golos/@golos/golos-russkoyazychnaya-socialno-mediinaya-blokchein-platforma",
+            "ru--kriptovalyuty/@mrgreen/lichnyi-opyt-ispolzovaniya-torgovogo-bota-na-poloniex",
+            "ru--kriptoinvestor/@aleco/kriptoinvestor-koshelki-dlya-ethereum-mist-vs-myetherwallet",
+            "ru--kraudspisok/@sxiii/kraudspisok-telegram-chaty-kanaly-i-boty-o-kriptovalyutakh-tema-8"
+        ]
+}

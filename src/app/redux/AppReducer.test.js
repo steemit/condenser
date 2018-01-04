d1c51105b018ba754b93b71adb9b78455b5e146d
Benjamin Chodoroff <bc@thermitic.net>
2018-01-04 15:05:34 -0500

    add feature flag reducer
    refs #2179

--- src/app/redux/AppReducer.test.js
+++ src/app/redux/AppReducer.test.js
@@ -1,6 +1,11 @@
 import { Map, OrderedMap, getIn } from 'immutable';
 
-import reducer, { defaultState, appActions } from './AppReducer';
+import reducer, {
+    defaultState,
+    appActions,
+    receiveFeatureFlags,
+    selectors,
+} from './AppReducer';
 
 const mockActions = {
     LOCATION_CHANGE: {
@@ -164,5 +169,20 @@ describe('App reducer', () => {
         let actual = reducer(initial, mockActions[appActions.TOGGLE_BLOGMODE]);
         const after = actual.getIn(['user_preferences', 'blogmode']);
         expect(after).toEqual(!before);
+    });
+    test('should receive feature flags', () => {
+        // Arrange
+        const initial = reducer();
+
+        // Act
+        const withFlags = reducer(
+            initial,
+            receiveFeatureFlags({
+                flying: true,
+            })
+        );
+
+        // Assert
+        expect(selectors.getFeatureFlags(withFlags)).toEqual({ flying: true });
     });
 });

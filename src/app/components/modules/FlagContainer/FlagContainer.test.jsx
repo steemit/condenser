de26b2437b112f423930a08a289de38c1f84ecad
Iain Maitland <imaitland@protonmail.com>
2018-01-04 16:24:49 -0500

    missed export of MapStateToProps for test

--- src/app/components/modules/FlagContainer/FlagContainer.test.jsx
+++ src/app/components/modules/FlagContainer/FlagContainer.test.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-15';
-import { Map } from 'immutable'
-import FlagContainer, {mapStateToProps} from 'app/components/modules/FlagContainer';
+import { Map } from 'immutable';
+import FlagContainer, {
+    mapStateToProps,
+} from 'app/components/modules/FlagContainer';
 import { defaultState } from 'app/redux/AppReducer';
 import LoadingIndicator from 'app/components/elements/LoadingIndicator';
 import Icon from 'app/components/elements/Icon';
@@ -11,7 +13,7 @@ configure({ adapter: new Adapter() });
 
 describe('FlagContainer', () => {
     it('maps state to props', () => {
-        const expected = mapStateToProps(defaultState)
-        expect(expected).toEqual({flags: undefined})
-    })
-})
+        const expected = mapStateToProps(defaultState);
+        expect(expected).toEqual({ flags: undefined });
+    });
+});
--- src/app/components/modules/FlagContainer/index.jsx
+++ src/app/components/modules/FlagContainer/index.jsx
@@ -1,18 +1,22 @@
 import { connect } from 'react-redux';
 import React from 'react';
-import {
-    selectors,
-} from 'app/redux/AppReducer';
+import { selectors } from 'app/redux/AppReducer';
 
 import Flag from 'app/components/modules/Flag';
+
+export const mapStateToProps = state => ({
+    flags: selectors.getFeatureFlags(state),
+});
 
 const connectFlag = (flagName, component) => {
     const FlagComponent = Flag(component);
     // If flags is a Map
-    const wrapped = flags => <FlagComponent flag={flags.find(flag => flag.get(flagName) === true )} />
+    const wrapped = flags => (
+        <FlagComponent flag={flags.find(flag => flag.get(flagName) === true)} />
+    );
     // If flags is an object
     //const wrapped = flags => <FlagComponent flag={Object.keys(flags).find(flag => flags[flag] === true)}
     return connect(mapStateToProps, null)(wrapped);
-}
+};
 
 export default connectFlag;

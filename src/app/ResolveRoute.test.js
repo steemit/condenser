2046e88972fdf738cc5d587fabd8c1cd3afebfd9
Iain Maitland <imaitland@protonmail.com>
2018-01-05 12:29:36 -0500

    add test for ResolveRoute.js, refs #2269

---
+++ src/app/ResolveRoute.test.js
@@ -0,0 +1,70 @@
+import { expect } from 'chai';
+import resolveRoute, { routeRegex } from "./ResolveRoute";
+
+describe ( 'routeRegex', () => {
+    it('should produce the desired regex patterns', () => {
+        const test_cases = [
+            ['PostsIndex', /^\/(@[\w\.\d-]+)\/feed\/?$/ ],
+            ['UserProfile1', /^\/(@[\w\.\d-]+)\/?$/ ],
+            ['UserProfile2', /^\/(@[\w\.\d-]+)\/(blog|posts|comments|recommended|transfers|curation-rewards|author-rewards|permissions|created|recent-replies|feed|password|followed|followers|settings)\/?$/],
+            ['UserProfile3', /^\/(@[\w\.\d-]+)\/[\w\.\d-]+/],
+            ['CategoryFilters', /^\/(hot|votes|responses|trending|trending30|promoted|cashout|payout|payout_comments|created|active)\/?$/ig],
+            ['PostNoCategory', /^\/(@[\w\.\d-]+)\/([\w\d-]+)/],
+            ['Post',     /^\/([\w\d\-\/]+)\/(\@[\w\d\.-]+)\/([\w\d-]+)\/?($|\?)/],
+            ['PostJson', /^\/([\w\d\-\/]+)\/(\@[\w\d\.-]+)\/([\w\d-]+)(\.json)$/],
+            ['UserJson', /^\/(@[\w\.\d-]+)(\.json)$/],
+            ['UserNameJson', /^.*(?=(\.json))/],
+        ];
+
+        test_cases.forEach(r => {
+            expect(routeRegex[r[0]]).to.deep.equal(r[1]);
+        });
+    });
+});
+
+describe('resolveRoute', () => {
+    const test_cases = [
+        ['/', {page: 'PostsIndex', params: ['trending']}],
+        ['/about.html', {page: 'About'}],
+        ['/faq.html', {page:'Faq'}],
+        ['/login.html', {page:'Login'}],
+        ['/privacy.html', {page:'Privacy'}],
+        ['/support.html', {page:'Support'}],
+        ['/tos.html', {page:'Tos'}],
+        ['/change_password', {page:'ChangePassword'}],
+        ['/create_account', {page:'CreateAccount'}],
+        ['/approval', {page:'Approval'}],
+        ['/pick_account', {page:'PickAccount'}],
+        ['/recover_account_step_1', {page:'RecoverAccountStep1'}],
+        ['/recover_account_step_2', {page:'RecoverAccountStep2'}],
+        ['/waiting_list.html', {page:'WaitingList'}],
+        ['/market', {page:'Market'}],
+        ['/~witnesses', {page:'Witnesses'}],
+        ['/submit.html', {page:'SubmitPost'}],
+        ['/@maitland/feed', {page: 'PostsIndex', params: ['home', '@maitland']}],
+        ['/@maitland/blog', {page: 'UserProfile', params: ['@maitland', "blog"]}],
+        ['/@cool/nice345', {page: 'PostNoCategory', params: ['@cool', "nice345"]}],
+        ['/ceasar/@salad/circa90', {page: 'Post', params: ['ceasar', '@salad', 'circa90', '']}],
+    ];
+    test_cases.forEach(r => {
+        it(`should resolve the ${r[0]} route`, () => {
+            expect(resolveRoute(r[0])).to.deep.equal(r[1]);
+        });
+    });
+
+    it('should resolve xss test route in development environment', () => {
+        expect(resolveRoute('/xss/test')).to.deep.equal({page:'NotFound'});
+        process.env.NODE_ENV = 'development';
+        expect(resolveRoute('/xss/test')).to.deep.equal({page:'XSSTest'});
+        delete process.env.NODE_ENV;
+    });
+    it('should resolve benchmark route in development environment', () => {
+        expect(resolveRoute('/benchmark')).to.deep.equal({page:'NotFound'});
+        process.env.NODE_ENV = 'development';
+        expect(resolveRoute('/benchmark')).to.deep.equal({page:'Benchmark'});
+        delete process.env.NODE_ENV;
+    });
+    it('should resolve an unknown route to NotFound', () => {
+        expect(resolveRoute('/randomness')).to.deep.equal({page:'NotFound'});
+    });
+});

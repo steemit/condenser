72b62cf1a92cc0db877e32e0740f6dc6f78530ec
Benjamin Chodoroff <bc@thermitic.net>
2018-01-09 17:19:38 -0500

    use a regular popup & share link rather than fb-sdk for facebook sharing
    Closes #1990

--- src/app/utils/JsPlugins.js
+++ src/app/utils/JsPlugins.js
@@ -27,25 +27,4 @@ export default function init(config) {
             sampleRate: 5
         });
     }
-
-    if (config.facebook_app_id) {
-        window.fbAsyncInit = function() {
-            FB.init({
-                appId: config.facebook_app_id,
-                xfbml: true,
-                version: 'v2.6',
-            });
-        };
-        (function(d, s, id) {
-            var js,
-                fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) {
-                return;
-            }
-            js = d.createElement(s);
-            js.id = id;
-            js.src = '//connect.facebook.net/en_US/sdk.js';
-            fjs.parentNode.insertBefore(js, fjs);
-        })(document, 'script', 'facebook-jssdk');
-    }
 }

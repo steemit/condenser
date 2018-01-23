577ec374163ab553341043eadcefd530c4b23b4b
Humberto Aquino <35669147+menkarbit@users.noreply.github.com>
2018-01-23 15:33:49 -0200

    Update Main.js

--- src/app/Main.js
+++ src/app/Main.js
@@ -117,6 +117,7 @@ if (!window.Intl) {
             require('intl/locale-data/jsonp/fr.js');
             require('intl/locale-data/jsonp/it.js');
             require('intl/locale-data/jsonp/ko.js');
+            require('intl/locale-data/jsonp/pt.js');
             Iso.bootstrap(runApp);
         },
         'IntlBundle'

46be7dd0d3458e4f65788a47d991a176791e591d
Nick Shtefan <nick.shtefan@gmail.com>
2018-02-06 19:27:00 +0300

    #571 check browser lang

--- app/components/elements/MobileBanners/AndroidMarket.jsx
+++ app/components/elements/MobileBanners/AndroidMarket.jsx
@@ -5,17 +5,17 @@ import tt from 'counterpart';
 
 export default class AndroidMarket extends Component {
 
-    render() {
-        if(process.env.BROWSER)
-        {
-            let language = (localStorage.getItem('language')) 
-            let imgsrc
-            if(language == 'ru')
-                imgsrc = settings.android.img_url_ru
-            else
-                imgsrc = settings.android.img_url
+    componentWillMount(){
+        if(process.env.BROWSER){
+            let language = localStorage.getItem('language')
+            language = language ? language : (window.navigator.userLanguage || window.navigator.language)
+            this.setState({language: language})
+        }
+    }
 
-            return <a href={settings.android.market_source + "&utm_source=" + settings.android.utm_source + "&utm_campaign=" + settings.android.utm_campaign +"&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"}><img alt={tt('about_jsx.mobileBannerAlt')} src={imgsrc}/></a>    
-        } else return null
+    render() {
+        let {language} = this.state
+        let imgsrc = (language == 'ru') ? settings.android.img_url_ru : settings.android.img_url
+            return <a href={settings.android.market_source + "&utm_source=" + settings.android.utm_source + "&utm_campaign=" + settings.android.utm_campaign +"&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"}><img alt={tt('about_jsx.mobileBannerAlt')} src={imgsrc}/></a>
     }
 }

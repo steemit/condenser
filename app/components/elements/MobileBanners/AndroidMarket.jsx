d172d0711d060709fd0489b06a6d0ddf15a3c2f5
Nick Shtefan <nick.shtefan@gmail.com>
2018-02-07 20:25:52 +0300

    #571 fix 500 err

--- app/components/elements/MobileBanners/AndroidMarket.jsx
+++ app/components/elements/MobileBanners/AndroidMarket.jsx
@@ -5,17 +5,29 @@ import tt from 'counterpart';
 
 export default class AndroidMarket extends Component {
 
-    componentWillMount(){
+    constructor(props) {
+        super(props)
+        this.state = {language: false}
+    }
+
+
+    componentDidMount(){
         if(process.env.BROWSER){
-            let language = localStorage.getItem('language')
-            language = language ? language : (window.navigator.userLanguage || window.navigator.language)
-            this.setState({language: language})
+            let lang = localStorage.getItem('language')
+            if (!lang){
+                lang = ((navigator.languages && navigator.languages[0]) || navigator.language || navigator.userLanguage)
+            }
+            this.setState({language: lang})
         }
     }
 
     render() {
         let {language} = this.state
-        let imgsrc = (language == 'ru') ? settings.android.img_url_ru : settings.android.img_url
-            return <a href={settings.android.market_source + "&utm_source=" + settings.android.utm_source + "&utm_campaign=" + settings.android.utm_campaign +"&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"}><img alt={tt('about_jsx.mobileBannerAlt')} src={imgsrc}/></a>
+        if (!language) 
+            return null
+        else{
+            let imgsrc = (language == 'ru') ? settings.android.img_url_ru : settings.android.img_url
+            return <a href={settings.android.market_source + "&utm_source=" + settings.android.utm_source + "&utm_campaign=" + settings.android.utm_campaign +"&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"}><img alt={tt('about_jsx.mobileBannerAlt')} src={imgsrc}/></a>    
+        }
     }
 }

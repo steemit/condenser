1eed4d6b3b6d662c383f1c35e2db10e7953e30a6
Nick Shtefan <nick.shtefan@gmail.com>
2018-02-08 18:02:15 +0300

    browsers language support

--- app/components/elements/MobileBanners/AndroidMarket.jsx
+++ app/components/elements/MobileBanners/AndroidMarket.jsx
@@ -26,7 +26,7 @@ export default class AndroidMarket extends Component {
         if (!language) 
             return null
         else{
-            let imgsrc = (language == 'ru') ? settings.android.img_url_ru : settings.android.img_url
+            let imgsrc = (language == 'ru' || language == 'ru-RU') ? settings.android.img_url_ru : settings.android.img_url
             return <a href={settings.android.market_source + "&utm_source=" + settings.android.utm_source + "&utm_campaign=" + settings.android.utm_campaign +"&pcampaignid=MKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1"}><img alt={tt('about_jsx.mobileBannerAlt')} src={imgsrc}/></a>    
         }
     }

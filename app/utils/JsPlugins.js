059b2c0b3f444e51205e8a813124ab44a20c9d8e
b1acksun <tkachenko.igor88@gmail.com>
2018-03-01 03:06:22 +0200

    #595 upd yametrika

--- app/utils/JsPlugins.js
+++ app/utils/JsPlugins.js
@@ -65,26 +65,28 @@ export default function init(config) {
     /* Yandex.Metrika counter */
     /* NOTE dont't forget to remove <img /> tag of yandex metrika (down below) */
     (function (d, w, c) {
-      (w[c] = w[c] || []).push(function() {
-        try {
-          w.yaCounter41829924 = new Ya.Metrika({
-            id:41829924,
-            clickmap:true,
-            trackLinks:true,
-            accurateTrackBounce:true
-          });
-        } catch(e) { }
-      });
-      var n = d.getElementsByTagName("script")[0],
-      s = d.createElement("script"),
-      f = function () { n.parentNode.insertBefore(s, n); };
-      s.type = "text/javascript";
-      s.async = true;
-      s.src = "https://mc.yandex.ru/metrika/watch.js";
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter41829924 = new Ya.Metrika2({
+                    id:41829924,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+                });
+            } catch(e) { }
+        });
 
-      if (w.opera == "[object Opera]") {
-        d.addEventListener("DOMContentLoaded", f, false);
-      } else { f(); }
-    })(window.document, window, "yandex_metrika_callbacks")
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/tag.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks2");
     /* /Yandex.Metrika counter */
 }
--- server/analylics.jsx
+++ server/analylics.jsx
@@ -8,27 +8,29 @@ const gaScript = `(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r
     ga('create', '${config.google_analytics_id}', 'auto');ga('send', 'pageview');`
 
 const metrikaScript = `(function (d, w, c) {
-    (w[c] = w[c] || []).push(function() {
+  (w[c] = w[c] || []).push(function() {
       try {
-        w.yaCounter41829924 = new Ya.Metrika({
-          id:41829924,
-          clickmap:true,
-          trackLinks:true,
-          accurateTrackBounce:true
-        });
+          w.yaCounter41829924 = new Ya.Metrika2({
+              id:41829924,
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true,
+              webvisor:true
+          });
       } catch(e) { }
-    });
-    var n = d.getElementsByTagName("script")[0],
-    s = d.createElement("script"),
-    f = function () { n.parentNode.insertBefore(s, n); };
-    s.type = "text/javascript";
-    s.async = true;
-    s.src = "https://mc.yandex.ru/metrika/watch.js";
+  });
 
-    if (w.opera == "[object Opera]") {
+  var n = d.getElementsByTagName("script")[0],
+      s = d.createElement("script"),
+      f = function () { n.parentNode.insertBefore(s, n); };
+  s.type = "text/javascript";
+  s.async = true;
+  s.src = "https://mc.yandex.ru/metrika/tag.js";
+
+  if (w.opera == "[object Opera]") {
       d.addEventListener("DOMContentLoaded", f, false);
-    } else { f(); }
-  })(window.document, window, "yandex_metrika_callbacks")`
+  } else { f(); }
+})(document, window, "yandex_metrika_callbacks2");`
 
 export default function AnalyticsScripts() {
     return (

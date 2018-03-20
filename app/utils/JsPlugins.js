7a0ee100ff594a321a91951c43224aa241814aba
b1acksun <tkachenko.igor88@gmail.com>
2018-03-20 18:02:40 +0200

    rollback metrika script

--- app/utils/JsPlugins.js
+++ app/utils/JsPlugins.js
@@ -66,27 +66,25 @@ export default function init(config) {
     /* NOTE dont't forget to remove <img /> tag of yandex metrika (down below) */
     (function (d, w, c) {
         (w[c] = w[c] || []).push(function() {
-            try {
-                w.yaCounter41829924 = new Ya.Metrika2({
-                    id:41829924,
-                    clickmap:true,
-                    trackLinks:true,
-                    accurateTrackBounce:true,
-                    webvisor:true
-                });
-            } catch(e) { }
+          try {
+            w.yaCounter41829924 = new Ya.Metrika({
+              id:41829924,
+              clickmap:true,
+              trackLinks:true,
+              accurateTrackBounce:true
+            });
+          } catch(e) { }
         });
-
         var n = d.getElementsByTagName("script")[0],
-            s = d.createElement("script"),
-            f = function () { n.parentNode.insertBefore(s, n); };
+        s = d.createElement("script"),
+        f = function () { n.parentNode.insertBefore(s, n); };
         s.type = "text/javascript";
         s.async = true;
-        s.src = "https://mc.yandex.ru/metrika/tag.js";
-
+        s.src = "https://mc.yandex.ru/metrika/watch.js";
+  
         if (w.opera == "[object Opera]") {
-            d.addEventListener("DOMContentLoaded", f, false);
+          d.addEventListener("DOMContentLoaded", f, false);
         } else { f(); }
-    })(document, window, "yandex_metrika_callbacks2");
+  })(window.document, window, "yandex_metrika_callbacks")
     /* /Yandex.Metrika counter */
 }

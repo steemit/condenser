baed420fcdc596eba6d49ba2f3bdf2510f442017
George <codefaster@yandex.ru>
2018-04-09 17:24:59 +0300

    Voting power indicator (#624)
    * add voting_indicator
    
    * fix
    
    * code style refactoring
    
    * indentation fix

--- app/components/elements/Userpic.jsx
+++ app/components/elements/Userpic.jsx
@@ -1,25 +1,66 @@
-import React, {Component, PropTypes} from 'react';
-import {connect} from 'react-redux';
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import cn from 'classnames';
+import { connect } from 'react-redux';
+import CircularProgress from './CircularProgress'
 import shouldComponentUpdate from 'app/utils/shouldComponentUpdate';
 
 class Userpic extends Component {
+
     static propTypes = {
-        account: PropTypes.string
+        account: PropTypes.string,
+        votingPower: PropTypes.number
+    }
+
+    static defaultProps = {
+        width: 48,
+        height: 48,
+        hideIfDefault: false
+    }
+
+    constructor() {
+        super()
+
+        this.state = {
+            showVotePow: false
+        }
+    }
+
+    componentDidMount() {
+        this.mounted = true
+        document.addEventListener('click', this.handleDocumentClick, false)
     }
 
     shouldComponentUpdate = shouldComponentUpdate(this, 'Userpic')
 
-    render() {
-        const {json_metadata, width, height} = this.props
-        const hideIfDefault = this.props.hideIfDefault || false
+    componentWillUnmount() {
+        document.removeEventListener('click', this.handleDocumentClick, false)
+    }
+
+    /**
+     * Обработчик клика по документу
+     * @param {object} target - DOM элемент по которому был клик
+     */
+    handleDocumentClick = ({ target }) => {
+        if (this.mounted && !ReactDOM.findDOMNode(this).contains(target)) {
+            this.setState({ showVotePow: false })
+        }
+    }
+
+    extractUrl = () => {
+        const { json_metadata, width, hideIfDefault } = this.props
 
         let url = null;
+
+        // TODO: переписать блок ниже
 
         // try to extract image url from users metaData
         try {
             const md = JSON.parse(json_metadata);
             if(md.profile) url = md.profile.profile_image;
-        } catch (e) {}
+        } catch (e) {
+          console.warn('Try to extract image url from users metaData failed!')
+        }
 
         if (url && /^(https?:)\/\//.test(url)) {
             const size = width && width > 48 ? '320x320' : '120x120';
@@ -33,17 +74,49 @@ class Userpic extends Component {
             url = require('app/assets/images/user.png');
         }
 
-        const style = {backgroundImage: 'url(' + url + ')',
-                       width: (width || 48) + 'px',
-                       height: (height || 48) + 'px'}
+        return url
+    }
 
-        return <div className="Userpic" style={style} />;
+    votingPowerToPercents = power => power / 100
+
+    toggleVotePow = () => this.setState({ showVotePow: !this.state.showVotePow })
+
+    getVotingIndicator = (percentage) => {
+      const { showVotePow } = this.state
+
+      const votingClasses = cn('voting_power', {
+        show: showVotePow
+      })
+
+      return (
+        <div className={votingClasses}>
+          <CircularProgress percentage={percentage} show={showVotePow} size={this.props.width} strokeWidth={4} />
+        </div>
+      )
+    }
+
+    render() {
+        const { width, height, votingPower } = this.props
+
+        const style = {
+          width: `${width}px`,
+          height: `${height}px`,
+          backgroundImage: `url(${this.extractUrl()})`
+        }
+
+        const percentage = this.votingPowerToPercents(votingPower)
+
+        return (
+          <div ref={n => { this.userPick = n }} className="Userpic" onClick={this.toggleVotePow} style={style}>
+            {percentage ? this.getVotingIndicator(percentage) : null}
+          </div>
+        )
     }
 }
 
 export default connect(
     (state, ownProps) => {
-        const {account, width, height, hideIfDefault} = ownProps
+        const { account, width, height, hideIfDefault } = ownProps
         return {
             json_metadata: state.global.getIn(['accounts', account, 'json_metadata']),
             width,

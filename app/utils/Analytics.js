dedf916b9fc17ad107413de857ee783a7d755e02
b1acksun <tkachenko.igor88@gmail.com>
2018-04-09 20:25:35 +0300

    #595 fix ga targets

--- app/utils/Analytics.js
+++ app/utils/Analytics.js
@@ -2,33 +2,49 @@
 function send(target) {
     if (process.env.BROWSER) {
         if (window.ga) {
-            if (target === 'SUCCESS_REG') {
-                window.ga('send', 'pageview', `/SUCCESS_REG1`)
-                window.ga('send', 'pageview', `/SUCCESS_REG2`)
-                window.ga('send', 'pageview', `/SUCCESS_REG3`)
-                window.ga('send', 'pageview', `/SUCCESS_REG4`)
-            } else {
-                window.ga('send', 'pageview', `/${target}`)
+            switch(target) {
+                case 'POPUP_CLICK_BUTTON': 
+                    window.ga('send', 'pageview', '/POPUP_CLICK_BUTTON')
+                    window.ga('send', 'pageview', '/SUCCESS_REG2')
+                break
+
+                case 'POPUP_CLICK_URL':
+                    window.ga('send', 'pageview', '/POPUP_CLICK_URL')
+                    window.ga('send', 'pageview', '/SUCCESS_REG3')
+                break
+
+                case 'BUTTON_CLICK':
+                    window.ga('send', 'pageview', '/BUTTON_CLICK')
+                    window.ga('send', 'pageview', '/SUCCESS_REG4')
+                break
+
+                default:
+                    window.ga('send', 'pageview', '/SUCCESS_REG1')
             }
         }
         if (window.yaCounter41829924) {
             window.yaCounter41829924.reachGoal(target)
+            window.yaCounter41829924.reachGoal('SUCCESS_REG')
         }
+        localStorage.removeItem('_atarget')
     }
 }
 
 export function successReg() {
-    send('SUCCESS_REG')
+    const target = localStorage.getItem('_atarget')
+    if (target) {
+        send(target)
+    }
 }
 
 export function popupClickButton() {
-    send('POPUP_CLICK_BUTTON')
+    localStorage.setItem('_atarget', 'POPUP_CLICK_BUTTON')
 }
 
 export function popupClickUrl() {
-    send('POPUP_CLICK_URL')
+    localStorage.setItem('_atarget', 'POPUP_CLICK_URL')
 }
 
 export function buttonClick() {
-    send('BUTTON_CLICK')
+    localStorage.setItem('_atarget', 'BUTTON_CLICK')
 }

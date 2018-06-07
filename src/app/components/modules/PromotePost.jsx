8d902a640a3b24d7a975ecd1683d126cbf2207c0
Iain Maitland <imaitland@protonmail.com>
2018-06-07 15:13:08 -0500

    prop-types modules components

--- src/app/components/modules/PromotePost.jsx
+++ src/app/components/modules/PromotePost.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes, Component } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import ReactDOM from 'react-dom';
 import * as transactionActions from 'app/redux/TransactionReducer';

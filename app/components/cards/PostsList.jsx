4522c3812e3fd31b6dd8e4014d8848dd78c2b741
Ilya Lebedev <bacherko@gmail.com>
2018-07-05 14:09:54 +0300

    reformat PostsList.jsx

--- app/components/cards/PostsList.jsx
+++ app/components/cards/PostsList.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import PropTypes from 'prop-types'
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import debounce from 'lodash.debounce';
+import tt from 'counterpart';
+import CloseButton from 'react-foundation-components/lib/global/close-button';
 import PostSummary from 'app/components/cards/PostSummary';
 import Post from 'app/components/pages/Post';
 import LoadingIndicator from 'app/components/elements/LoadingIndicator';
-import debounce from 'lodash.debounce';
-import CloseButton from 'react-foundation-components/lib/global/close-button';
-import {findParent} from 'app/utils/DomUtils';
+import { findParent } from 'app/utils/DomUtils';
 import Icon from 'app/components/elements/Icon';
-import shouldComponentUpdate from 'app/utils/shouldComponentUpdate';
-import {connect} from 'react-redux';
-import tt from 'counterpart';
 import { isPostVisited, getVisitedPosts, visitPost } from 'app/utils/helpers';
 import KEYS from 'app/utils/keyCodes';
 
@@ -20,8 +19,7 @@ function topPosition(domElt) {
     return domElt.offsetTop + topPosition(domElt.offsetParent);
 }
 
-class PostsList extends React.Component {
-
+class PostsList extends PureComponent {
     static propTypes = {
         posts: PropTypes.object.isRequired,
         loading: PropTypes.bool.isRequired,
@@ -30,46 +28,44 @@ class PostsList extends React.Component {
         showSpam: PropTypes.bool,
         fetchState: PropTypes.func.isRequired,
         pathname: PropTypes.string,
+        ignoreResult: PropTypes.any,
     };
 
     static defaultProps = {
         showSpam: false,
-    }
+    };
 
-    constructor() {
-        super();
-        this.state = {
-            // _isMounted: false,
-            thumbSize: 'desktop',
-            showNegativeComments: false,
-            nsfwPref: 'warn',
-            showPost: null,
-            visitedPosts : []
-        }
-        this.scrollListener = this.scrollListener.bind(this);
-        this.onPostClick = this.onPostClick.bind(this);
-        this.onBackButton = this.onBackButton.bind(this);
-        this.closeOnOutsideClick = this.closeOnOutsideClick.bind(this);
-        this.shouldComponentUpdate = shouldComponentUpdate(this, 'PostsList')
-    }
+    state = {
+        thumbSize: 'desktop',
+        nsfwPref: 'warn',
+        showPost: null,
+        visitedPosts: [],
+    };
 
     readNsfwPref() {
-        if(!process.env.BROWSER) return
-        const {username} = this.props
-        const key = 'nsfwPref' + (username ? '-' + username : '')
-        const nsfwPref = localStorage.getItem(key) || 'warn'
-        this.setState({nsfwPref})
+        if (!process.env.BROWSER) {
+            return;
+        }
+        const { username } = this.props;
+        const key = 'nsfwPref' + (username ? '-' + username : '');
+
+        this.setState({
+            nsfwPref: localStorage.getItem(key) || 'warn',
+        });
     }
 
     componentDidMount() {
-        this.readNsfwPref()
+        this.readNsfwPref();
+        this.scrollListener();
         this.attachScrollListener();
     }
 
     componentWillUpdate() {
-        const location = `${window.location.pathname}${window.location.search}${window.location.hash}`;
-        if (this.state.showPost && (location !== this.post_url)) {
-            this.setState({showPost: null});
+        console.log('Will update');
+
+        const path = `${location.pathname}${location.search}${location.hash}`;
+        if (this.state.showPost && path !== this.post_url) {
+            this.setState({ showPost: null });
         }
         this.readNsfwPref();
     }
@@ -79,10 +75,10 @@ class PostsList extends React.Component {
             document.body.classList.add('with-post-overlay');
             window.addEventListener('popstate', this.onBackButton);
             window.addEventListener('keydown', this.onBackButton);
-            const post_overlay = document.getElementById('post_overlay');
-            if (post_overlay) {
-                post_overlay.addEventListener('click', this.closeOnOutsideClick);
-                post_overlay.focus();
+            const postOverlay = document.getElementById('post_overlay');
+            if (postOverlay) {
+                postOverlay.addEventListener('click', this.closeOnOutsideClick);
+                postOverlay.focus();
             }
         }
         if (!this.state.showPost && prevState.showPost) {
@@ -92,10 +88,10 @@ class PostsList extends React.Component {
         }
     }
 
-    componentWillReceiveProps(nextProps) {
+    componentWillReceiveProps() {
         const visited = getVisitedPosts();
-        if (this.state.visitedPosts != visited) {
-            this.setState({visitedPosts: visited});
+        if (this.state.visitedPosts !== visited) {
+            this.setState({ visitedPosts: visited });
         }
     }
 
@@ -103,139 +99,203 @@ class PostsList extends React.Component {
         this.detachScrollListener();
         window.removeEventListener('popstate', this.onBackButton);
         window.removeEventListener('keydown', this.onBackButton);
-        const post_overlay = document.getElementById('post_overlay');
-        if (post_overlay) post_overlay.removeEventListener('click', this.closeOnOutsideClick);
+        const postOverlay = document.getElementById('post_overlay');
+        if (postOverlay) {
+            postOverlay.removeEventListener('click', this.closeOnOutsideClick);
+        }
         document.body.classList.remove('with-post-overlay');
     }
 
-    onBackButton(e) {
-        if ('keyCode' in e && e.keyCode !== KEYS.ESCAPE) return;
-        window.removeEventListener('popstate', this.onBackButton);
-        window.removeEventListener('keydown', this.onBackButton);
-        this.closePostModal();
-    }
+    onBackButton = e => {
+        if (!e.keyCode || e.keyCode === KEYS.ESCAPE) {
+            window.removeEventListener('popstate', this.onBackButton);
+            window.removeEventListener('keydown', this.onBackButton);
+            this.closePostModal();
+        }
+    };
 
-    closeOnOutsideClick(e) {
+    closeOnOutsideClick = e => {
         const inside_post = findParent(e.target, 'PostsList__post_container');
         if (!inside_post) {
-            const inside_top_bar = findParent(e.target, 'PostsList__post_top_bar');
+            const inside_top_bar = findParent(
+                e.target,
+                'PostsList__post_top_bar'
+            );
             if (!inside_top_bar) {
                 const post_overlay = document.getElementById('post_overlay');
-                if (post_overlay) post_overlay.removeEventListener('click', this.closeOnOutsideClick);
+                if (post_overlay) {
+                    post_overlay.removeEventListener(
+                        'click',
+                        this.closeOnOutsideClick
+                    );
+                }
                 this.closePostModal();
             }
         }
-    }
+    };
 
     closePostModal = () => {
         window.document.title = this.state.prevTitle;
-        this.setState({showPost: null, prevTitle: null});
-    }
+        this.setState({ showPost: null, prevTitle: null });
+    };
 
     fetchIfNeeded() {
         this.scrollListener();
     }
 
-    toggleNegativeReplies = () => {
-        this.setState({
-            showNegativeComments: !this.state.showNegativeComments
-        });
-    }
-
     scrollListener = debounce(() => {
-      // if (! this.state._isMounted) return;
         const el = window.document.getElementById('posts_list');
         if (!el) return;
-        const scrollTop = (window.pageYOffset !== undefined) ? window.pageYOffset :
-            (document.documentElement || document.body.parentNode || document.body).scrollTop;
-        if (topPosition(el) + el.offsetHeight - scrollTop - window.innerHeight < 10) {
-            const {loadMore, posts, category} = this.props;
-            if (loadMore && posts && posts.size) loadMore(posts.last(), category);
+        const scrollTop =
+            window.pageYOffset !== undefined
+                ? window.pageYOffset
+                : (
+                      document.documentElement ||
+                      document.body.parentNode ||
+                      document.body
+                  ).scrollTop;
+
+        if (
+            topPosition(el) + el.offsetHeight - scrollTop - window.innerHeight <
+            10
+        ) {
+            const { loadMore, posts, category } = this.props;
+
+            if (loadMore && posts && posts.size) {
+                loadMore(posts.last(), category);
+            }
         }
 
         // Detect if we're in mobile mode (renders larger preview imgs)
         const mq = window.matchMedia('screen and (max-width: 39.9375em)');
-        if(mq.matches) {
-            this.setState({thumbSize: 'mobile'})
+        if (mq.matches) {
+            this.setState({ thumbSize: 'mobile' });
         } else {
-            this.setState({thumbSize: 'desktop'})
+            this.setState({ thumbSize: 'desktop' });
         }
-    }, 150)
+    }, 150);
 
     attachScrollListener() {
         window.addEventListener('scroll', this.scrollListener);
         window.addEventListener('resize', this.scrollListener);
-        this.scrollListener();
     }
 
     detachScrollListener() {
+        this.scrollListener.cancel();
         window.removeEventListener('scroll', this.scrollListener);
         window.removeEventListener('resize', this.scrollListener);
     }
 
-    onPostClick(post, url) {
+    onPostClick = (post, url) => {
         this.post_url = url;
         this.props.fetchState(url);
         this.props.removeHighSecurityKeys();
-        this.setState({showPost: post, prevTitle: window.document.title});
+        this.setState({ showPost: post, prevTitle: window.document.title });
         window.history.pushState({}, '', url);
         visitPost(post);
-    }
+    };
 
     render() {
-        const {posts, showSpam, loading, category, content,
-            ignore_result, account} = this.props;
-        const {thumbSize, showPost, nsfwPref} = this.state
+        console.log('PostsList render');
+        const {
+            posts,
+            showSpam,
+            loading,
+            category,
+            content,
+            ignoreResult,
+            account,
+        } = this.props;
+
+        const { thumbSize, showPost, nsfwPref } = this.state;
+
         const postsInfo = [];
         let aiPosts = [];
-        posts.forEach((item) => {
-            showPost && aiPosts.push(item);
-            const cont = content.get(item);
-            if(!cont) {
-                console.error('PostsList --> Missing cont key', item)
-                return
+
+        posts.forEach(item => {
+            if (showPost) {
+                aiPosts.push(item);
             }
-            const ignore = ignore_result && ignore_result.has(cont.get('author'))
-            const hide = cont.getIn(['stats', 'hide'])
-            if(!(ignore || hide) || showSpam) // rephide
-                postsInfo.push({item, ignore})
+            const cont = content.get(item);
+            if (!cont) {
+                console.error('PostsList --> Missing cont key', item);
+                return;
+            }
+            const ignore = ignoreResult && ignoreResult.has(cont.get('author'));
+            const hide = cont.getIn(['stats', 'hide']);
+
+            if (!(ignore || hide) || showSpam) {
+                postsInfo.push({ item, ignore });
+            }
         });
+
         if (showPost) {
-          const sliceCount = 5, index = aiPosts.indexOf(showPost);
-          aiPosts = aiPosts.slice(index < sliceCount ? 0 : index - sliceCount, index + sliceCount + 1);
+            const sliceCount = 5;
+            const index = aiPosts.indexOf(showPost);
+            aiPosts = aiPosts.slice(
+                index < sliceCount ? 0 : index - sliceCount,
+                index + sliceCount + 1
+            );
         }
-        const renderSummary = items => items.map(item => <li key={item.item}>
-            <PostSummary
-                account={account}
-                post={item.item}
-                currentCategory={category}
-                thumbSize={thumbSize}
-                ignore={item.ignore}
-                onClick={this.onPostClick}
-                nsfwPref={nsfwPref}
-                visited={isPostVisited(item.item)}
-            />
-        </li>)
+
+        const renderSummary = items =>
+            items.map(item => (
+                <li key={item.item}>
+                    <PostSummary
+                        account={account}
+                        post={item.item}
+                        currentCategory={category}
+                        thumbSize={thumbSize}
+                        ignore={item.ignore}
+                        onClick={this.onPostClick}
+                        nsfwPref={nsfwPref}
+                        visited={isPostVisited(item.item)}
+                    />
+                </li>
+            ));
 
         return (
             <div id="posts_list" className="PostsList">
-                <ul className="PostsList__summaries hfeed" itemScope itemType="http://schema.org/blogPosts">
+                <ul
+                    className="PostsList__summaries hfeed"
+                    itemScope
+                    itemType="http://schema.org/blogPosts"
+                >
                     {renderSummary(postsInfo)}
                 </ul>
-                {loading && <center><LoadingIndicator type="circle" /></center>}
-                {showPost && <div id="post_overlay" className="PostsList__post_overlay" tabIndex={0}>
-                    <div className="PostsList__post_top_overlay">
-                        <div className="PostsList__post_top_bar">
-                            <button className="back-button" type="button" title={tt('g.back')} onClick={() => { this.setState({showPost: null}) }}>
-                                <span aria-hidden="true"><Icon name="chevron-left" /></span>
-                            </button>
-                            <CloseButton onClick={this.closePostModal} />
+                {loading && (
+                    <center>
+                        <LoadingIndicator type="circle" />
+                    </center>
+                )}
+                {showPost && (
+                    <div
+                        id="post_overlay"
+                        className="PostsList__post_overlay"
+                        tabIndex={0}
+                    >
+                        <div className="PostsList__post_top_overlay">
+                            <div className="PostsList__post_top_bar">
+                                <button
+                                    className="back-button"
+                                    type="button"
+                                    title={tt('g.back')}
+                                    onClick={() => {
+                                        this.setState({ showPost: null });
+                                    }}
+                                >
+                                    <span aria-hidden="true">
+                                        <Icon name="chevron-left" />
+                                    </span>
+                                </button>
+                                <CloseButton onClick={this.closePostModal} />
+                            </div>
+                        </div>
+                        <div className="PostsList__post_container">
+                            <Post post={showPost} aiPosts={aiPosts} />
                         </div>
                     </div>
-                    <div className="PostsList__post_container">
-                        <Post post={showPost} aiPosts={aiPosts} />
-                    </div>
-                </div>}
+                )}
             </div>
         );
     }
@@ -243,19 +303,31 @@ class PostsList extends React.Component {
 
 export default connect(
     (state, props) => {
-        const pathname = state.app.get('location').pathname;
-        const current = state.user.get('current')
-        const username = current ? current.get('username') : state.offchain.get('account')
-        const content = state.global.get('content');
-        const ignore_result = state.global.getIn(['follow', 'getFollowingAsync', username, 'ignore_result']);
-        return {...props, username, content, ignore_result, pathname};
+        const current = state.user.get('current');
+        const username = current
+            ? current.get('username')
+            : state.offchain.get('account');
+
+        return {
+            ...props,
+            username,
+            content: state.global.get('content'),
+            ignoreResult: state.global.getIn([
+                'follow',
+                'getFollowingAsync',
+                username,
+                'ignore_result',
+            ]),
+            pathname: state.app.get('location').pathname,
+        };
     },
-    dispatch => ({
-        fetchState: (pathname) => {
-            dispatch({type: 'FETCH_STATE', payload: {pathname}})
-        },
-        removeHighSecurityKeys: () => {
-            dispatch({type: 'user/REMOVE_HIGH_SECURITY_KEYS'})
-        }
-    })
-)(PostsList)
+    {
+        fetchState: pathname => ({
+            type: 'FETCH_STATE',
+            payload: { pathname },
+        }),
+        removeHighSecurityKeys: () => ({
+            type: 'user/REMOVE_HIGH_SECURITY_KEYS',
+        }),
+    }
+)(PostsList);

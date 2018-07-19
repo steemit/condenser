19848dc1779dd570c074a3b27f17b6c1f15328d3
Ilya Lebedev <bacherko@gmail.com>
2018-07-19 18:12:49 +0300

    fix ProfileReducer #669

--- app/redux/ProfileReducer.js
+++ app/redux/ProfileReducer.js
@@ -1,16 +1,24 @@
 import { fromJS } from 'immutable';
 
 export const CHANGE_LAYOUT = 'CHANGE_LAYOUT';
+const LAYOUT_STORAGE_KEY = 'profile.layout';
 
 export default function(state, { type, payload }) {
-    if (!state) {
-        state = fromJS({
-            layout: getSavedLayout() || 'list',
-        });
+    if (!process.env.BROWSER) {
+        return null;
     }
+
+    if (!state) {
+        state = {
+            layout: getSavedLayout() || 'list',
+        };
+    }
+
+    state = fromJS(state);
 
     switch (type) {
         case CHANGE_LAYOUT:
+            localStorage.setItem(LAYOUT_STORAGE_KEY, payload.layout);
             return state.set('layout', payload.layout);
     }
 
@@ -18,9 +26,7 @@ export default function(state, { type, payload }) {
 }
 
 function getSavedLayout() {
-    if (process.env.BROWSER) {
-        try {
-            return localStorage.getItem('profile.layout');
-        } catch (err) {}
-    }
+    try {
+        return localStorage.getItem(LAYOUT_STORAGE_KEY);
+    } catch (err) {}
 }

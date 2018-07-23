70075efd807e63bf6d32b1f6b615c6f8a0ef0a83
Iain Maitland <imaitland@protonmail.com>
2018-07-23 07:43:05 -0400

    more refactoring

--- src/app/components/notifications/Notification/index.jsx
+++ src/app/components/notifications/Notification/index.jsx
@@ -5,8 +5,9 @@ import tt from 'counterpart';
 import Userpic from 'app/components/elements/Userpic';
 import TimeAgoWrapper from 'app/components/elements/TimeAgoWrapper';
 import Url from 'app/utils/Url';
-import * as type from './type';
-import badges from './icon';
+import InlineSVG from 'svg-inline-react';
+import vizOn from 'assets/icons/visibility_on.svg';
+import vizOff from 'assets/icons/visibility_off.svg';
 import * as notificationActions from 'app/redux/NotificationReducer';
 
 class Notification extends React.Component {
@@ -34,7 +35,7 @@ class Notification extends React.Component {
     render() {
         const {
             notification,
-            notification: { notify_type, read } = {},
+            notification: { notify_type, read },
         } = this.props;
 
         const switchTypes = types => key =>
@@ -55,12 +56,27 @@ class Notification extends React.Component {
                                 className="updated"
                             />
                         </div>
+                        <div
+                            className="rightControls"
+                            onClick={
+                                notification.read
+                                    ? this.markUnread
+                                    : this.markRead
+                            }
+                        >
+                            <InlineSVG
+                                src={notification.read ? vizOn : vizOff}
+                            />
+                        </div>
                     </div>
                 );
             },
             account_update: notification => {
                 return (
                     <div className="item-panel">
+                        {!notification.shown ? (
+                            <span className="unseenIndicator">●</span>
+                        ) : null}
                         <div className={'Comment__Userpic show-for-medium '}>
                             <Userpic account={notification.data.account} />
                         </div>
@@ -81,6 +97,9 @@ class Notification extends React.Component {
             comment_reply: notification => {
                 return (
                     <div className="item-panel">
+                        {!notification.shown ? (
+                            <span className="unseenIndicator">●</span>
+                        ) : null}
                         <div className={'Comment__Userpic show-for-medium '}>
                             <Userpic account={notification.data.author} />
                         </div>
@@ -107,6 +126,9 @@ class Notification extends React.Component {
             feed: notification => {
                 return (
                     <div className="item-panel">
+                        {!notification.shown ? (
+                            <span className="unseenIndicator">●</span>
+                        ) : null}
                         <div className={'Comment__Userpic show-for-medium '}>
                             <Userpic account={notification.username} />
                         </div>
@@ -132,128 +154,14 @@ class Notification extends React.Component {
         const notificationInner = switchTypes(notificationTypes)(notify_type)(
             notification
         );
-        // const amount = this.props.notification.amount;
-        // const author = this.props.notification.author;
-        // const classNames = this.props.notification.read ? '' : 'unread';
-        // const created = this.props.notification.created;
-        // const item = this.props.notification.item;
-        // const read = this.props.notification.read;
+
+        // TODO: Make notif link to relevant content
+        // TODO: Display badge representing notification type.
+        /*
         const post = this.props.notification.rootItem;
         const badge = badges[type] ? badges[type] : null;
-
-        /*
-        switch (notificationType) {
-            case type.POST_REPLY:
-                headerContent = (
-                    <span>
-                        <span className="user">{author}</span>{' '}
-                        {tt(localeAction)} <strong>{post.summary}</strong>
-                    </span>
-                );
-                bodyContent = item.summary;
-                break;
-            case type.COMMENT_REPLY:
-                headerContent = (
-                    <span>
-                        <span className="user">{author}</span>{' '}
-                        {tt(localeAction)} <strong>{item.parentSummary}</strong>
-                    </span>
-                );
-                bodyContent = item.summary;
-                break;
-            case type.ANNOUNCEMENT:
-            case type.ANNOUNCEMENT_IMPORTANT:
-            //todo: use announcement comment 'image' as icon post steemfest. This will require addl info from yo.
-            case type.FEED:
-                headerContent = (
-                    <span>
-                        <span className="user">{author}</span>{' '}
-                        {tt(localeAction)}{' '}
-                    </span>
-                );
-                bodyContent = item.summary;
-                break;
-            case type.RECEIVE_STEEM:
-                headerContent = (
-                    <span>
-                        <span className="user">{author}</span>{' '}
-                        {tt(localeAction)}
-                    </span>
-                );
-                bodyContent = (
-                    <span>
-                        {amount}{' '}
-                        <span className="subject"> {tt('g.steem')}</span>
-                    </span>
-                );
-                break;
-            case type.RESTEEM:
-                headerContent = (
-                    <span>
-                        <span className="user">{author}</span>{' '}
-                        {tt(localeAction)}
-                    </span>
-                );
-                bodyContent = item.summary;
-                link = Url.comment(item);
-                break;
-            case type.SECURITY_PWD_CHANGE:
-            case type.SECURITY_WITHDRAWAL:
-            case type.SECURITY_NEW_MOBILE:
-            case type.POWER_DOWN:
-                headerContent = (
-                    <span>
-                        <span className="subject">
-                            {tt(`${localeRoot}.subject`)}
-                        </span>{' '}
-                        {tt(localeAction)}
-                    </span>
-                );
-                bodyContent = tt(`${localeRoot}.body`);
-                picture = (
-                    <div
-                        className="Userpic"
-                        dangerouslySetInnerHTML={{ __html: badges.important }}
-                    />
-                );
-                break;
-            case type.MENTION:
-            case type.VOTE:
-                localeAction = localeRoot + '.actionComment';
-                if (0 === item.depth) {
-                    localeAction = localeRoot + '.actionPost';
-                    link = Url.comment(item);
-                }
-                headerContent = (
-                    <span>
-                        <span className="user">{author}</span>{' '}
-                        {tt(localeAction)}
-                    </span>
-                );
-                bodyContent = item.summary;
-                break;
-            default:
-                console.log(
-                    `no option for this notification ${notificationType}`,
-                    this.props
-                );
-                return null;
-        }
         */
 
-        /*
-        if (!picture) {
-            switch (type) {
-                //case type.ANNOUNCEMENT_IMPORTANT :
-                //todo: special image - unknown json format in notification
-                case type.POWER_DOWN:
-                    //todo: blank circle image
-                    break;
-                default:
-                    picture = <Userpic account={author} badge={badge} />;
-            }
-        }
-        */
         return (
             <li className={`item ${!read && 'unread'}`}>
                 <Link
@@ -265,58 +173,10 @@ class Notification extends React.Component {
                         this.markReadDefault(e);
                     }}
                 >
-                    {!this.props.shown ? (
-                        <span
-                            className="unseenIndicator"
-                            dangerouslySetInnerHTML={{ __html: '&#9679' }}
-                        />
-                    ) : null}
                     {notificationInner}
-                    <div
-                        className="rightControls"
-                        onClick={read ? this.markUnread : this.markRead}
-                        dangerouslySetInnerHTML={{
-                            __html: read
-                                ? badges.visibilityOn
-                                : badges.visibilityOff,
-                        }}
-                    />
                 </Link>
             </li>
         );
-        /*
-        return (
-            <Link
-                to={link}
-                className={classNames}
-                onClick={e => {
-                    if (this.props.onClick) {
-                        this.props.onClick(e);
-                    }
-                    this.markReadDefault(e);
-                }}
-            >
-                {!this.props.shown ? (
-                    <span
-                        className="unseenIndicator"
-                        dangerouslySetInnerHTML={{ __html: '&#9679' }}
-                    />
-                ) : null}
-                <div className="item-panel">
-                    <div className={'Comment__Userpic show-for-medium ' + type}>
-                        {picture}
-                    </div>
-                    <div className="item-header">{headerContent}</div>
-                    {bodyContent ? (
-                        <div className="item-body">{bodyContent}</div>
-                    ) : null}
-                    <div className="item-footer">
-                        <TimeAgoWrapper date={created} className="updated" />
-                    </div>
-                </div>
-                {readControl}
-            </Link>
-        );*/
     }
 }
 

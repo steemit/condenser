2c737292c1240505e2593b26d78d7b569da5e4d1
Alexey Elizarov <felex-ae@ya.ru>
2018-07-26 17:10:33 +0300

    remove unused slate editor, refactored global reducer state #669

--- app/redux/GlobalReducer.js
+++ app/redux/GlobalReducer.js
@@ -1,10 +1,45 @@
 import { Map, Set, List, fromJS, Iterable } from 'immutable';
 import createModule from 'redux-modules';
-import { emptyContent } from 'app/redux/EmptyState';
 import constants from './constants';
 import { contentStats, fromJSGreedy } from 'app/utils/StateFunctions';
+import { LIQUID_TICKER, DEBT_TICKER } from 'app/client_config'
 
-const emptyContentMap = Map(emptyContent);
+const emptyContentMap = Map({
+    fetched: new Date(), /// the date at which this data was requested from the server
+    id: '2.8.0',
+    author: '',
+    permlink: '',
+    category: '',
+    i18n_category: '',
+    parent_author: '',
+    parent_permlink: '',
+    title: '',
+    body: '',
+    json_metadata: '{}',
+    last_update: new Date().toISOString(),
+    created: new Date().toISOString(),
+    depth: 0,
+    children: 0,
+    children_rshares2: '0',
+    net_rshares: 0,
+    abs_rshares: 0,
+    cashout_time: new Date().toISOString(),
+    total_vote_weight: '0',
+    total_payout_value: ['0.000', DEBT_TICKER].join(" "),
+    pending_payout_value: ['0.000', LIQUID_TICKER].join(" "),
+    total_pending_payout_value: ['0.000', LIQUID_TICKER].join(" "),
+    active_votes: [],
+    replies: [],
+    stats: {
+        authorRepLog10: 86,
+        gray: false,
+        hasPendingPayout: false,
+        hasReplies: false,
+        hide: false,
+        netVoteSign: 0,
+        pictures: true,
+    },
+});
 
 export default createModule({
     name: 'global',

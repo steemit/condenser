275757a3c20ce60d02df4ebc069d2ca537c07091
Ilya Lebedev <bacherko@gmail.com>
2018-07-30 19:38:04 +0300

    next #669

--- src/app/components/userProfile/dialogs/TransferDialog/TransferDialog.jsx
+++ src/app/components/userProfile/dialogs/TransferDialog/TransferDialog.jsx
@@ -26,24 +26,64 @@ const Column = styled.div`
     //overflow: hidden;
 `;
 
-const SimpleInput = styled.input``;
+const SimpleInput = styled.input`
+    display: block;
+    width: 100%;
+    height: 34px;
+    padding: 0 11px;
+    border: 1px solid #e1e1e1;
+    outline: none;
+    border-radius: 6px;
+    font-size: 14px;
+    transition: border-color 0.25s;
+
+    &:focus {
+        border-color: #8a8a8a;
+    }
+`;
 
 const Section = styled.div`
     margin-bottom: 10px;
 `;
 
 const Label = styled.div`
-    margin-bottom: 4px;
-    font-weight: 500;
+    margin: 14px 0 9px;
     font-size: 14px;
 `;
 
 const Note = styled.textarea`
-
+    display: block;
+    width: 100%;
+    height: 116px;
+    padding: 7px 11px;
+    border: 1px solid #e1e1e1;
+    outline: none;
+    border-radius: 6px;
+    resize: none;
+    font-size: 14px;
+    box-shadow: none !important;
 `;
 
 export default class TransferDialog extends PureComponent {
+    state = {
+        amount: '',
+        currency: 'gbg',
+    };
+
     render() {
+        const { amount, currency } = this.state;
+
+        const buttons = [
+            {
+                id: 'gbg',
+                title: 'GBG',
+            },
+            {
+                id: 'golos',
+                title: tt('token_names.LIQUID_TOKEN'),
+            },
+        ];
+
         return (
             <DialogFrame
                 className="LinkOptionsDialog"
@@ -71,11 +111,21 @@ export default class TransferDialog extends PureComponent {
                         </Section>
                         <Section>
                             <Label>Сколько</Label>
-                            <ComplexInput />
+                            <ComplexInput
+                                placeholder={'Доступно 5.543'}
+                                value={amount}
+                                activeId={currency}
+                                buttons={buttons}
+                                onChange={this._onAmountChange}
+                                onActiveChange={this._onCurrencyChange}
+                            />
                         </Section>
                     </Column>
                     <Column>
-                        <Note placeholder={'Эта заметка является публичной'} />
+                        <Section>
+                            <Label>Заметка</Label>
+                            <Note placeholder={'Эта заметка является публичной'} />
+                        </Section>
                     </Column>
                 </Body>
             </DialogFrame>
@@ -83,6 +133,18 @@ export default class TransferDialog extends PureComponent {
     }
 
     _onCloseClick = () => {
-        debugger
+        debugger;
     };
+
+    _onAmountChange = value => {
+        this.setState({
+            amount: value,
+        });
+    };
+
+    _onCurrencyChange = currency => {
+        this.setState({
+            currency,
+        });
+    }
 }

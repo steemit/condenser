8487038fea3f367434bf7a1cc0a69f9052436cbe
Smittyvb <smittyvb@gmail.com>
2018-08-10 20:51:16 -0400

    Simplify logic for generating canonicalUrl

--- src/app/utils/ExtractMeta.js
+++ src/app/utils/ExtractMeta.js
@@ -40,20 +40,25 @@ export default function extractMeta(chain_data, rp) {
             // API currently returns 'false' data with id 0.0.0 for posts that do not exist
             const d = extractContent(objAccessor, content, false);
             const url = 'https://steemit.com' + d.link;
-            const canonicalUrl =
+            var canonicalUrl = url;
+            const hasAppTemplateData =
+                d.json_metadata &&
                 d.json_metadata.app &&
                 d.category &&
-                d.json_metadata.app.split('/').length === 2 &&
-                Apps[d.json_metadata.app.split('/')[0]] &&
-                Apps[d.json_metadata.app.split('/')[0]].url_scheme
-                    ? Apps[d.json_metadata.app.split('/')[0]].url_scheme
-                          .split('{category}')
-                          .join(d.category)
-                          .split('{username}')
-                          .join(d.author)
-                          .split('{permlink}')
-                          .join(d.permlink)
-                    : url;
+                d.json_metadata.app.split('/').length === 2;
+            if (hasAppTemplateData) {
+                const app = d.json_metadata.app.split('/')[0];
+                const hasAppData = Apps[app] && Apps[app].url_scheme;
+                if (hasAppData) {
+                    canonicalUrl = Apps[app].url_scheme
+                        .split('{category}')
+                        .join(d.category)
+                        .split('{username}')
+                        .join(d.author)
+                        .split('{permlink}')
+                        .join(d.permlink);
+                }
+            }
             const title = d.title + ' — Steemit';
             const desc = d.desc + ' by ' + d.author;
             const image = d.image_link || profile.profile_image;

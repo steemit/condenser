373379ae9dfd5d2d4bca99646e552576ff29f19a
Alexey Elizarov <felex-ae@ya.ru>
2018-08-20 09:00:07 +0300

    delete import testnet_api #669

--- server/server.js
+++ server/server.js
@@ -12,9 +12,7 @@ import favicon from 'koa-favicon';
 import staticCache from 'koa-static-cache';
 import useRedirects from './redirects';
 import useGeneralApi from './api/general';
-import useTestnetApi from './testnet_api';
 import useAccountRecoveryApi from './api/account_recovery';
-// import useNotificationsApi from './api/notifications';
 import useRegistrationApi from './api/registration';
 import {proxyRoutes as useProxyRoutes} from './api/proxy';
 import {ratesRoutes as useRatesRoutes} from './api/rates';
@@ -193,10 +191,6 @@ useAccountRecoveryApi(app);
 useGeneralApi(app);
 useProxyRoutes(app);
 useRatesRoutes(app);
-
-// if (config.get('is_testnet')) {
-//     useTestnetApi(app);
-// }
 
 // helmet wants some things as bools and some as lists, makes config difficult.
 // our config uses strings, this splits them to lists on whitespace.

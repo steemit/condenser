677014c36de5a319be5d6b0cab8cef3b6fd86373
Alexey Elizarov <felex-ae@ya.ru>
2018-09-11 17:31:56 +0300

    link to new profile for master #782

--- app/components/pages/UserProfile.jsx
+++ app/components/pages/UserProfile.jsx
@@ -476,6 +476,9 @@ export default class UserProfile extends React.Component {
                                 <Icon name="calendar" /> <DateJoinWrapper date={accountjoin} />
                             </p>
                         </div>
+                        <div>
+                            <a href={`https://preview.golos.io/@${accountname}`} class="button small">Перейти на новый профиль</a>
+                        </div>
                         <div className="UserProfile__buttons-mobile">
                             <Follow follower={username} following={accountname} what="blog" />
                         </div>

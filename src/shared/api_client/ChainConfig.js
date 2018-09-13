584adf555f5a58f40b4010a51f951fbef9711839
Andrew Chaney (netuoso) <andrewc@pobox.com>
2018-09-12 20:57:10 -0500

    pull chain info from config

--- src/shared/api_client/ChainConfig.js
+++ src/shared/api_client/ChainConfig.js
@@ -1,12 +1,15 @@
 import * as steem from '@steemit/steem-js';
+import config from 'config';
 
-steem.config.set('address_prefix', 'STM');
+steem.config.set('address_prefix', config.get('chain_prefix'));
 
 let chain_id = '';
-for (let i = 0; i < 32; i++) chain_id += '00';
+if (config.has('chain_id') && config.get('chain_id').length)
+    chain_id = config.get('chain_id');
+else for (let i = 0; i < 32; i++) chain_id += '00';
 
 module.exports = {
-    address_prefix: 'STM',
+    address_prefix: config.get('chain_prefix'),
     expire_in_secs: 15,
     chain_id,
 };

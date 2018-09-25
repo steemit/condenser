505e44304228831c380bbc222ea87acee232f83e
Iain Maitland <imaitland@protonmail.com>
2018-09-25 14:03:26 -0400

    add Steem

--- src/app/components/modules/Header/index.jsx
+++ src/app/components/modules/Header/index.jsx
@@ -261,7 +261,7 @@ class Header extends React.Component {
             <header className="Header">
                 <div className="annoucement-banner">
                     <p className="announcement-banner__text">
-                        The blockchain was upgraded earlier today. You may
+                        The Steem blockchain was upgraded earlier today. You may
                         experience trouble posting and transacting while the new
                         bandwidth system stabilizes.{' '}
                         <a

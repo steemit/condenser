00e59d04da07d65611868a1090b2da62b561e7c5
Pon Kattera <kattera@gmail.com>
2018-10-17 17:02:26 -0700

    Update banner to Ned's Steem Needs You post

--- src/app/components/elements/Announcement.jsx
+++ src/app/components/elements/Announcement.jsx
@@ -4,12 +4,12 @@ import React from 'react';
 export const Announcement = ({ onClose }) => (
     <div className="annoucement-banner">
         <p className="announcement-banner__text">
-            HF20 Update: Operations Stable, click{' '}
+            Steem needs your votes to win big.{' '}
             <a
                 className="announcement-banner__link"
-                href="https://steemit.com/steem/@steemitblog/hf20-update-operations-stable"
+                href="https://steemit.com/steem/@ned/steem-needs-you"
             >
-                here.
+                Learn more.
             </a>
         </p>
         <button className="close-button" type="button" onClick={onClose}>

ca44db80d8e44daa852f4e1d3a36172eecb69f54
Evan <choue@google.com>
2019-01-04 04:39:36 +0000

    Save and detect if logged in with keychain, and use this information

--- src/app/utils/SteemKeychain.js
+++ src/app/utils/SteemKeychain.js
@@ -1,3 +1,9 @@
+import { isLoggedIn, extractLoginData } from 'app/utils/UserUtil';
+
+/**
+ *
+ * @returns {boolean}
+ */
 export function hasCompatibleKeychain() {
     return (
         window.steem_keychain &&
@@ -5,4 +11,23 @@ export function hasCompatibleKeychain() {
         window.steem_keychain.requestBroadcast &&
         window.steem_keychain.requestSignedCall
     );
+}
+
+/**
+ *
+ * @returns {boolean}
+ */
+export function isLoggedInWithKeychain() {
+    if (!isLoggedIn()) {
+        return false;
+    }
+    const data = localStorage.getItem('autopost2');
+    const [
+        username,
+        password,
+        memoWif,
+        login_owner_pubkey,
+        login_with_keychain,
+    ] = extractLoginData(data);
+    return !!login_with_keychain;
 }

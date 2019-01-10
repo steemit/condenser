6b19e963397d9bf265bce1e2c4f05b6d703c33ab
kodxana <romanrex19@int.pl>
2019-01-10 06:19:49 +0100

    Update Links.js

--- src/app/utils/Links.js
+++ src/app/utils/Links.js
@@ -49,6 +49,7 @@ export default {
     // simpleLink: new RegExp(`<a href="(.*)">(.*)<\/a>`, 'ig'),
     ipfsPrefix: /(https?:\/\/.*)?\/ipfs/i,
     twitch: /https?:\/\/(?:www.)?twitch.tv\/(?:(videos)\/)?([a-zA-Z0-9][\w]{3,24})/i,
+    spee.ch: /https?:\/\/(?:www.)?spee.ch\/(?:(videos)\/)?([a-zA-Z0-9][\w]{3,24})/i,
 };
 
 //TODO: possible this should go somewhere else.

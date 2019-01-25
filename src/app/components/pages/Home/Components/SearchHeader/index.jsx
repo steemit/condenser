7221b57640e064befcad882d2f50f952132771c9
bring2real <bring2real@aol.com>
2019-01-26 04:39:57 +1000

    has search

--- src/app/components/pages/Home/Components/SearchHeader/index.jsx
+++ src/app/components/pages/Home/Components/SearchHeader/index.jsx
@@ -4,41 +4,43 @@ import PropTypes from 'prop-types';
 import { DropDown } from 'app/components/pages/_Common';
 
 class SearchHeader extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            // loading: true,
-            sortBy: null,
-        };
-    }
+    this.state = {
+      // loading: true,
+      sortBy: null,
+    };
+  }
 
-    getQuery(key) {
-        return key;
-    }
+  getQuery(key) {
+    return key;
+  }
 
-    render() {
-        return (
-            <div className="SearchHeaderWrapper">
-                <div className="Title">
-                    Search Results:{' '}
-                    <span>{`"Who's really pushing '${this.getQuery(
-                        'search'
-                    )}'?"`}</span>
-                </div>
-                <DropDown
-                    valueKey="value"
-                    defaultValue="Sort By"
-                    value={this.state.sortBy}
-                    options={[
-                        { label: 'Latest', value: 'latest' },
-                        { label: 'Top Votes', value: 'top_votes' },
-                    ]}
-                    onChange={value => this.setState({ sortBy: value })}
-                />
+  render() {
+    const { hasSearch } = this.props;
+    return (
+      <div className="SearchHeaderWrapper">
+        <div>
+          {hasSearch && (
+            <div className="Title">
+              Search Results: <span>{`"${hasSearch}"`}</span>
             </div>
-        );
-    }
+          )}
+        </div>
+        <DropDown
+          valueKey="value"
+          defaultValue="Sort By"
+          value={this.state.sortBy}
+          options={[
+            { label: 'Latest', value: 'latest' },
+            { label: 'Top Votes', value: 'top_votes' },
+          ]}
+          onChange={value => this.setState({ sortBy: value })}
+        />
+      </div>
+    );
+  }
 }
 
 export default SearchHeader;

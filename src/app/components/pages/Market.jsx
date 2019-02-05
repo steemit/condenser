1fafd7696c91f840b15abea794d91970d0b27246
roadscape <roadscape@users.noreply.github.com>
2019-02-05 12:46:06 -0600

    wallet_url config

--- src/app/components/pages/Market.jsx
+++ src/app/components/pages/Market.jsx
@@ -5,11 +5,12 @@ import tt from 'counterpart';
 class Market extends React.Component {
     render() {
         return (
-            <div>
-                <div className="row">
-                    <div className="column">
-                        <Link to="https://steemit.com/">{tt('navigation.currency_market')}</Link>
-                    </div>
+            <div className="row">
+                <div className="column">
+                    {tt('g.external_link_message')}{': '}
+                    <Link to={`{$STM_Config.wallet_url}/market`}>
+                        {tt('navigation.currency_market')}
+                    </Link>
                 </div>
             </div>
         );

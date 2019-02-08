720afd062c9e3073bec6fbcec4ef8d473d2c4b47
bring2real <bring2real@aol.com>
2019-02-09 03:04:26 +1000

    side menu detail node

--- src/app/components/pages/_Common/SideMenu/index.jsx
+++ src/app/components/pages/_Common/SideMenu/index.jsx
@@ -1,54 +1,34 @@
 import React, { Component } from 'react';
 // import PropTypes from 'prop-types';
-import {
-    ThumbsUp,
-    ThumbsDown,
-    Feather,
-    MessageSquare,
-    Bookmark,
-    Download,
-} from 'react-feather';
+import * as FeatherIcons from 'react-feather';
 
 class SideMenu extends Component {
-    render() {
-        return (
-            <div className="Sticky">
-                <div className="SideMenu">
-                    <div className="Vote">
-                        <span className="Icon">
-                            <ThumbsUp />
-                        </span>
-                        <span className="Value">1234</span>
-                        <span className="Icon">
-                            <ThumbsDown />
-                        </span>
-                    </div>
-                    <div className="Social">
-                        <div className="Item">
-                            <span className="Icon">
-                                <Feather />
-                            </span>
-                        </div>
-                        <div className="Item">
-                            <span className="Icon">
-                                <MessageSquare />
-                            </span>
-                        </div>
-                        <div className="Item">
-                            <span className="Icon">
-                                <Bookmark />
-                            </span>
-                        </div>
-                        <div className="Item">
-                            <span className="Icon">
-                                <Download />
-                            </span>
-                        </div>
-                    </div>
-                </div>
+  render() {
+    return (
+      <div className="Sticky">
+        <div className="SideMenu">
+          <div className="Social">
+            <div className="Item" title="Create New Question">
+              <span className="Icon">Q+</span>
             </div>
-        );
-    }
+            <div className="Item" title="Create New Review">
+              <span className="Icon">R+</span>
+            </div>
+            <div className="Item" title="Share">
+              <span className="Icon">
+                <FeatherIcons.Share2 />
+              </span>
+            </div>
+            <div className="Item" title="Bookmark">
+              <span className="Icon">
+                <FeatherIcons.Star />
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 SideMenu.defaultProps = {};

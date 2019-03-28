0faa54fd97e68729a3d39f2c3bc55b029fd4af23
Roger Jungemann <roger@thefifthcircuit.com>
2019-03-27 22:55:29 -0700

    Make steem-market code handle errors gracefully

--- src/server/utils/SteemMarket.js
+++ src/server/utils/SteemMarket.js
@@ -11,29 +11,39 @@ export function SteemMarket() {
     cache.on('expired', (k, v) => {
         console.log('Cache key expired', k);
         if (key === k) {
-            this.promise = this.refresh();
+            this.refresh();
         }
     });
     this.cache = cache;
-    this.promise = this.refresh();
+    // Store empty data while we wait for the network request to complete
+    this.storeEmpty().then(() => this.refresh());
 }
 
-SteemMarket.prototype.get = async function() {
-    await this.promise;
-
+SteemMarket.prototype.storeEmpty = function() {
     const key = config.steem_market_cache.key;
     return new Promise((res, rej) => {
-        this.cache.get(key, (err, value) => {
-            if (err) {
-                console.error('Could not retrieve Steem Market data');
-                return;
-            }
-            res(value);
+        this.cache.set(key, {}, (err, success) => {
+            console.info('Storing empty Steem Market data...');
+            res();
         });
     });
 };
 
-SteemMarket.prototype.refresh = function() {
+SteemMarket.prototype.get = async function() {
+    return new Promise((res, rej) => {
+        const key = config.steem_market_cache.key;
+        this.cache.get(key, (err, value) => {
+            if (err) {
+                console.error('Could not retrieve Steem Market data');
+                res({});
+                return;
+            }
+            res(value || {});
+        });
+    });
+};
+
+SteemMarket.prototype.refresh = async function() {
     console.info('Refreshing Steem Market data...');
 
     const url = config.steem_market_endpoint;
@@ -41,37 +51,28 @@ SteemMarket.prototype.refresh = function() {
     const key = config.steem_market_cache.key;
     if (!url) {
         console.info('No Steem Market endpoint provided...');
-        return new Promise((res, rej) => {
-            this.cache.set(key, {}, (err, success) => {
-                console.info('Stored empty Steem Market data...');
-                res();
-            });
-        });
+        return this.storeEmpty();
     }
 
-    return new Promise((res, rej) => {
-        const options = {
-            url: url,
-            method: 'GET',
-            headers: {
-                Authorization: `Token ${token}`,
-            },
-        };
-
-        axios(options)
-            .then(response => {
-                console.info('Received Steem Market data from endpoint...');
-                this.cache.set(key, response.data, (err, success) => {
-                    if (err) {
-                        console.error('Error storing currencies in cache', err);
-                        return;
-                    }
-                    console.info('Steem Market data refreshed...');
-                    res();
-                });
-            })
-            .catch(err => {
-                console.error('Could not fetch Steem Market data', err);
+    return await axios({
+        url: url,
+        method: 'GET',
+        headers: {
+            Authorization: `Token ${token}`,
+        },
+    })
+        .then(response => {
+            console.info('Received Steem Market data from endpoint...');
+            this.cache.set(key, response.data, (err, success) => {
+                if (err) {
+                    rej(err);
+                    return;
+                }
+                console.info('Steem Market data refreshed...');
             });
-    });
+        })
+        .catch(err => {
+            console.error('Could not fetch Steem Market data', err);
+            return this.storeEmpty();
+        });
 };

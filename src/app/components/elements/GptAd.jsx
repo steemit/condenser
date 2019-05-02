de4b32086b6d7c848e8eea84f5287c49797135bd
Jonathan Porta <jonathan@jonathanporta.com>
2019-05-02 11:43:05 -0600

    GPT Bidding 3: The Bids are Back in Town
    Ads Coinzilla bidding ads back into a couple of slots.

--- src/app/components/elements/GptAd.jsx
+++ src/app/components/elements/GptAd.jsx
@@ -1,6 +1,57 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 
+const BiddingConfig = {
+    slots: [
+        {
+            id: 'div-gpt-ad-1551233873698-0',
+            path:
+                '/21784675435/steemit_bottom-of-post/steemit_bottom-of-post_prebid',
+            sizes: [[728, 90]],
+            prebid: [
+                {
+                    mediaTypes: {
+                        banner: {
+                            sizes: [[728, 90]],
+                        },
+                    },
+                    bids: [
+                        {
+                            bidder: 'coinzilla',
+                            params: {
+                                placementId: '6425c7b9886e0045972',
+                            },
+                        },
+                    ],
+                },
+            ],
+        },
+        {
+            id: 'div-gpt-ad-1554687231046-0',
+            path:
+                '/21784675435/steemit_left-navigation/steemit_left-navigation_prebid',
+            sizes: [[728, 90], [970, 90]],
+            prebid: [
+                {
+                    mediaTypes: {
+                        banner: {
+                            sizes: [[728, 90], [970, 90]],
+                        },
+                    },
+                    bids: [
+                        {
+                            bidder: 'coinzilla',
+                            params: {
+                                placementId: '3575c7b9886e2cb3619',
+                            },
+                        },
+                    ],
+                },
+            ],
+        },
+    ],
+};
+
 class GptAd extends Component {
     componentDidMount() {
         if (!this.ad.slot_id || !this.enabled) {
@@ -29,7 +80,11 @@ class GptAd extends Component {
 
     constructor(props) {
         super(props);
-        const { ad, enabled } = props;
+        const { ad, enabled, type } = props;
+
+        this.ad = {};
+        this.type = type;
+        this.enabled = false;
 
         if (ad) {
             console.info(
@@ -44,8 +99,6 @@ class GptAd extends Component {
                     props.slotName
                 }' will be disabled because we were unable to find the ad details.`
             );
-            this.enabled = false;
-            this.ad = {};
         }
     }
 
@@ -53,6 +106,14 @@ class GptAd extends Component {
         if (!this.ad || !this.enabled) {
             return <div id="disabled_ad" style={{ display: 'none' }} />;
         }
+        // else if (this.type == 'Bidding') {
+        //   return(
+        //   <AdvertisingProvider config={BiddingConfig}>
+        //     <AdvertisingSlot id="div-gpt-ad-1551233873698-0" >
+        //       <b>div-gpt-ad-1551233873698-0</b>
+        //     </AdvertisingSlot>
+        //   </AdvertisingProvider>)
+        // }
 
         return (
             <div
@@ -67,6 +128,7 @@ class GptAd extends Component {
 GptAd.propTypes = {
     ad: PropTypes.object.isRequired, //TODO: Define this shape
     enabled: PropTypes.bool.isRequired,
+    type: PropTypes.oneOf(['Bidding', 'Category', 'Basic']),
 };
 
 export default connect(
@@ -91,37 +153,40 @@ export default connect(
         //
         const slotName = props.slotName;
 
-        let type = 'basic';
-        let slot = basicSlots.getIn([slotName]);
-        if (categorySlots.getIn([postCategory, slotName])) {
-            console.info(
-                `GPT-[${slotName}]::Overriding type of '${
-                    type
-                }' to be 'category' due to category being set to '${
-                    postCategory
-                }' and the existence of a category named '${
-                    postCategory
-                }' which has a slot named '${slotName}'`
-            );
-            type = 'category';
-            slot = categorySlots.getIn([postCategory, slotName]);
-        } else if (biddingSlots.getIn([slotName])) {
-            console.info(
-                `GPT-[${slotName}]::Overriding type of '${
-                    type
-                }' to be 'bidding' because we have a bidding slot defined for slotName '${
-                    slotName
-                }'`
-            );
-            type = 'bidding';
-            slot = biddingSlots.getIn([slotName]);
-        } else {
-            console.info(
-                `GPT-[${slotName}]::No override for type. Sticking with '${
-                    type
-                }'`
-            );
-        }
+        let type = props.type;
+
+        // let slot = basicSlots.getIn([slotName]);
+        let slot = state.app.getIn(['googleAds', `gpt${type}Slots`, slotName]);
+        console.log('GOT TYPE OF', type, slot);
+        // if (categorySlots.getIn([postCategory, slotName])) {
+        //     console.info(
+        //         `GPT-[${slotName}]::Overriding type of '${
+        //             type
+        //         }' to be 'category' due to category being set to '${
+        //             postCategory
+        //         }' and the existence of a category named '${
+        //             postCategory
+        //         }' which has a slot named '${slotName}'`
+        //     );
+        //     type = 'category';
+        //     slot = categorySlots.getIn([postCategory, slotName]);
+        // } else if (biddingSlots.getIn([slotName])) {
+        //     console.info(
+        //         `GPT-[${slotName}]::Overriding type of '${
+        //             type
+        //         }' to be 'bidding' because we have a bidding slot defined for slotName '${
+        //             slotName
+        //         }'`
+        //     );
+        //     type = 'bidding';
+        //     slot = biddingSlots.getIn([slotName]);
+        // } else {
+        //     console.info(
+        //         `GPT-[${slotName}]::No override for type. Sticking with '${
+        //             type
+        //         }'`
+        //     );
+        // }
 
         return {
             enabled,

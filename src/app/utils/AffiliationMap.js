df6dcc12b21484afcc5d0098e308ab92258e9568
roadscape <roadscape@users.noreply.github.com>
2019-05-14 18:46:53 -0500

    adjust affiliation map

--- src/app/utils/AffiliationMap.js
+++ src/app/utils/AffiliationMap.js
@@ -1,26 +1,21 @@
 const map = {
     //steemit
     ned: 'steemit',
-    sneak: 'steemit',
+    justinw: 'steemit',
+    elipowell: 'steemit',
     vandeberg: 'steemit',
     andrarchy: 'steemit',
-    roelandp: 'steemit',
-    goldibex: 'steemit',
     roadscape: 'steemit',
-    steem: 'steemit',
-    steemit: 'steemit',
     steemitblog: 'steemit',
     steemitdev: 'steemit',
-    admin: 'steemit',
-    steemit1: 'steemit',
-    steemit2: 'steemit',
-    steemit3: 'steemit',
 
+    /*
     //steem monsters
     steemmonsters: 'sm',
     'steem.monsters': 'sm',
     aggroed: 'sm',
     yabapmatt: 'sm',
+*/
 };
 
 export default map;

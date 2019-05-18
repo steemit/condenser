d95c802a376feda51777e604550dff7f3970ee69
Quoc Huy Nguyen Dinh <quochuy@gmail.com>
2019-05-18 18:14:59 +1000

    Fixes #2758 - accessing document.body from componentWillReceiveProps() and componentDidMount()

--- src/app/components/App.jsx
+++ src/app/components/App.jsx
@@ -36,12 +36,30 @@ class App extends React.Component {
         this.setState({ gptBannerHeight: height });
     }
 
+    toggleBodyNightmode(nightmodeEnabled) {
+      if (nightmodeEnabled) {
+        document.body.classList.remove('theme-light');
+        document.body.classList.add('theme-dark');
+      } else {
+        document.body.classList.remove('theme-dark');
+        document.body.classList.add('theme-light');
+      }
+    }
+
+    componentWillReceiveProps(nextProps) {
+        const {nightmodeEnabled} = nextProps;
+        this.toggleBodyNightmode(nightmodeEnabled);
+    }
+
     componentWillMount() {
         if (process.env.BROWSER) localStorage.removeItem('autopost'); // July 14 '16 compromise, renamed to autopost2
         this.props.loginUser();
     }
 
     componentDidMount() {
+        const {nightmodeEnabled} = this.props;
+        this.toggleBodyNightmode(nightmodeEnabled);
+
         window.addEventListener('gptadshown', this.gptadshownListener);
     }
 

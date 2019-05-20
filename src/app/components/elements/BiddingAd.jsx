3148c725c36a626975e1284079759afff3a769af
Jonathan Porta <jonathan@jonathanporta.com>
2019-05-20 09:36:23 -0600

    cleanup

--- src/app/components/elements/BiddingAd.jsx
+++ src/app/components/elements/BiddingAd.jsx
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 
 class BiddingAd extends Component {
     componentDidMount() {
-        window.addEventListener('prebidNoBids', this.prebidNoBids);
         if (!this.ad.path || !this.enabled) return;
 
         googletag.cmd.push(() => {
@@ -21,50 +20,22 @@ class BiddingAd extends Component {
                 googletag.cmd.push(() => {
                     googletag.display(this.ad.path);
                     this.refreshBid(this.ad.path, slot);
-                    //   googletag.pubads().refresh([slot]);
-                    //   googletag
-                    //     .pubads()
-                    //     .addEventListener('slotRenderEnded', event => {
-                    //       console.info('BiddingAd - Slot has been rendered:', event);
-                    //       window.dispatchEvent(new Event('biddingadshown'));
-                    //     });
                 });
             }
         });
-
-        //
-        // googletag.cmd.push(() => {
-        //     googletag.display(this.ad.path);
-        // });
     }
 
     refreshBid(path, slot) {
         pbjs.que.push(() => {
             pbjs.requestBids({
-                timeout: PREBID_TIMEOUT,
+                timeout: 2000,
                 adUnitCodes: [path],
                 bidsBackHandler: function() {
-                    console.log('biddingad-refreshbid-', arguments);
                     pbjs.setTargetingForGPTAsync([path]);
                     googletag.pubads().refresh([slot]);
                 },
             });
         });
-    }
-
-    componentWillUnmount() {
-        window.removeEventListener('prebidNoBids', this.prebidNoBids);
-    }
-
-    prebidNoBids(e) {
-        console.log('BiddingAd - Received a NOBIDS event', e, this.state);
-        if (e.slotId === this.ad.path) {
-            console.log(
-                'BiddingAd - Will hide this ad based on event match id',
-                e
-            );
-            // this.setState({ shown: false });
-        }
     }
 
     constructor(props) {
@@ -76,26 +47,22 @@ class BiddingAd extends Component {
         this.enabled = false;
 
         if (ad) {
-            console.info(
-                `Slot named '${props.slotName}' will render with given data:`,
-                ad
-            );
+            // console.info(
+            //     `Slot named '${props.slotName}' will render with given data:`,
+            //     ad
+            // );
             this.enabled = enabled;
             this.ad = ad.toJS();
         } else {
-            console.info(
-                `Slot named '${
-                    props.slotName
-                }' will be disabled because we were unable to find the ad details.`
-            );
+            // console.info(
+            //     `Slot named '${
+            //         props.slotName
+            //     }' will be disabled because we were unable to find the ad details.`
+            // );
         }
-        this.state = { shown: true };
-
-        this.prebidNoBids = this.prebidNoBids.bind(this);
     }
 
     render() {
-        // if (this.state.shown) {
         return (
             <div
                 className="bidding-ad gpt-ad"
@@ -103,29 +70,8 @@ class BiddingAd extends Component {
                 id={this.ad.path}
             />
         );
-        // } else {
-        // TODO: One Day we will fall back to another ad type here.
-        // console.log('BiddingAd->render() - NOT SHOWING AN AD')
-        // return null;
-        // }
     }
 }
-
-// BiddingAd.propTypes = {
-//     id: PropTypes.string.isRequired, // TODO: This is the naive way, make this better with a sensible config.
-//     type: PropTypes.oneOf(['Bidding', 'Category', 'Basic']),
-// };
-//
-// export default connect(
-//     (state, props) => {
-//         // console.log('GptBidding::connect', props);
-//
-//         return {
-//             ...props,
-//         };
-//     },
-//     dispatch => ({})
-// )(BiddingAd);
 
 BiddingAd.propTypes = {
     ad: PropTypes.shape({

06b9ca921c93223e5f386843acba80e37c0a88a5
roadscape <roadscape@users.noreply.github.com>
2019-05-21 11:33:28 -0500

    remove unused updated class, tidy

--- src/app/components/elements/ContentEditedWrapper/index.jsx
+++ src/app/components/elements/ContentEditedWrapper/index.jsx
@@ -7,23 +7,20 @@ import { injectIntl } from 'react-intl';
 class ContentEditedWrapper extends React.Component {
     render() {
         let { createDate, updateDate, className } = this.props;
-        console.log(createDate, updateDate, className);
-        if (createDate !== updateDate) {
-            if (updateDate && /^\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d$/.test(updateDate)) {
-                updateDate = updateDate + 'Z'; // Firefox really wants this Z (Zulu)
-            }
-            const dt = new Date(updateDate);
-            const date_time = `${this.props.intl.formatDate(
-              dt
-            )} ${this.props.intl.formatTime(dt)}`;
-            return (
-              <Tooltip t={date_time} className={className}>
-                  (edited)
-              </Tooltip>
-            );
-        } else {
-            return (null);
+        if (createDate === updateDate) return null;
+
+        if (updateDate && /^\d{4}-\d\d-\d\dT\d\d:\d\d:\d\d$/.test(updateDate)) {
+            updateDate = updateDate + 'Z'; // Firefox really wants this Z (Zulu)
         }
+        const dt = new Date(updateDate);
+        const date_time = `${this.props.intl.formatDate(
+            dt
+        )} ${this.props.intl.formatTime(dt)}`;
+        return (
+            <Tooltip t={date_time} className={className}>
+                (edited)
+            </Tooltip>
+        );
     }
 }
 

56d9278b715daf28ad77fa9be020117bb4089979
roadscape <roadscape@users.noreply.github.com>
2019-05-22 15:44:02 -0500

    standardize join date, fix #3367

--- src/app/components/elements/DateJoinWrapper.jsx
+++ src/app/components/elements/DateJoinWrapper.jsx
@@ -4,11 +4,17 @@ import { FormattedDate } from 'react-intl';
 
 export default class DateJoinWrapper extends React.Component {
     render() {
-        const date = new Date(this.props.date);
+        let date = this.props.date.replace(' ', 'T');
+        if (date[date.length - 1] != 'Z') date += 'Z';
+
         return (
             <span>
                 {tt('g.joined')}{' '}
-                <FormattedDate value={date} year="numeric" month="long" />
+                <FormattedDate
+                    value={new Date(date)}
+                    year="numeric"
+                    month="long"
+                />
             </span>
         );
     }

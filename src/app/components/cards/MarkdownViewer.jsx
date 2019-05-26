e2c7a74e53d8f7f42acf54f99c556dbfcad7629d
evildido <fraga_jonathan@protonmail.ch>
2019-05-26 19:31:08 +0200

    Construct iframe in MarkdownViewer

--- src/app/components/cards/MarkdownViewer.jsx
+++ src/app/components/cards/MarkdownViewer.jsx
@@ -144,7 +144,7 @@ class MarkdownViewer extends Component {
         // HtmlReady inserts ~~~ embed:${id} type ~~~
         for (let section of cleanText.split('~~~ embed:')) {
             const match = section.match(
-                /^([A-Za-z0-9\?\=\_\-\/\.]+) (youtube|vimeo|twitch|dtube)\s?(\d+)? ~~~/
+                /^([A-Za-z0-9\?\=\_\-\/\.]+) (youtube|vimeo|twitch|dtube|bittube)\s?(\d+)? ~~~/
             );
             if (match && match.length >= 3) {
                 const id = match[1];
@@ -198,6 +198,20 @@ class MarkdownViewer extends Component {
                     );
                 } else if (type === 'dtube') {
                     const url = `https://emb.d.tube/#!/${id}`;
+                    sections.push(
+                        <div className="videoWrapper">
+                            <iframe
+                                key={idx++}
+                                src={url}
+                                width={w}
+                                height={h}
+                                frameBorder="0"
+                                allowFullScreen
+                            />
+                        </div>
+                    );
+                } else if (type === 'bittube') {
+                    const url = `https://bit.tube/playerembed/${id}`;
                     sections.push(
                         <div className="videoWrapper">
                             <iframe

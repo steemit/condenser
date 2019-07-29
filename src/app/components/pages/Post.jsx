4b51b04a153e7ffe19cef71d3b7eaf2c5c513f66
Jonathan Porta <jonathan@jonathanporta.com>
2019-07-29 09:58:59 -0600

    Add in-image Freestar tag

--- src/app/components/pages/Post.jsx
+++ src/app/components/pages/Post.jsx
@@ -286,6 +286,10 @@ class Post extends React.Component {
                             type="Freestar"
                             id="steemit_728x90_468x60_300x250_BelowComments"
                         />
+                        <GptAd
+                            type="Freestar"
+                            id="steemit_1x1_gumgum-inimage"
+                        />
                     </div>
                 ) : null}
             </div>

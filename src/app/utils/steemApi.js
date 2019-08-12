3cbdc2f56879e02adec34b4364f172288fd9d76d
roadscape <roadscape@users.noreply.github.com>
2019-08-12 11:51:42 -0500

    get_state path cleanup

--- src/app/utils/steemApi.js
+++ src/app/utils/steemApi.js
@@ -6,8 +6,15 @@ export async function getStateAsync(url) {
     // strip off query string
     url = url.split('?')[0];
 
-    if (url === '/') url = 'trending';
+    // strip off leading and trailing slashes
+    if (url.lenth > 0 && url[0] == '/') url = url.substring(1, url.length);
+    if (url.length > 0 && url[url.length - 1] == '/')
+        url = url.substring(0, url.length - 1);
 
+    // blank URL defaults to `trending`
+    if (url === '') url = 'trending';
+
+    // curation and author rewards pages are alias of `transfers`
     if (url.indexOf('/curation-rewards') !== -1)
         url = url.replace('/curation-rewards', '/transfers');
     if (url.indexOf('/author-rewards') !== -1)

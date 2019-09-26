d906311eebe4db33f35faf2792fbd459d6bfd576
roadscape <roadscape@users.noreply.github.com>
2019-09-26 15:05:34 -0500

    fix communities list, remove logging

--- src/app/components/modules/CommunityList/Community.jsx
+++ src/app/components/modules/CommunityList/Community.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import SubscribeButtonContainer from 'app/components/elements/SubscribeButtonContainer';
+import { Link } from 'react-router';
 
 export default function Community(props) {
     const { name, title, description } = props;
-    console.log('Community.jsx::constructor()', props);
 
     return (
         <div className="communities__row">
             <div className="communities__names">
-                <a href={`/trending/${name}`}>
-                    {determineNiceTitle(name, title)}
-                </a>
+                <Link to={`/trending/${name}`}>{title}</Link>
             </div>
             <div className="communities__description">{description}</div>
             <div className="communities__subscription">
@@ -20,22 +18,10 @@ export default function Community(props) {
         </div>
     );
 }
+
 //TODO: Move Community type to a proptypes file and use where we need it.
 Community.propTypes = {
     name: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
     description: PropTypes.string.isRequired,
 };
-
-/**
- * Figure out which value to use - this probably will do more soon.
- * @param {string} name - start date
- * @param {string} title - stop date
- * @returns {string} - return fancy string
- */
-function determineNiceTitle(name, title) {
-    if (!title) {
-        return name;
-    }
-    return title;
-}
--- src/app/components/modules/CommunityList/CommunityContainer.jsx
+++ src/app/components/modules/CommunityList/CommunityContainer.jsx
@@ -3,19 +3,8 @@ import PropTypes from 'prop-types';
 import Community from './Community';
 
 class CommunityContainer extends React.Component {
-    constructor(props) {
-        console.log('CommunityContainer.jsx::constructor()', props);
-        super(props);
-        this.state = {};
-    }
-
-    // async componentWillMount() {
-    //     await console.log('CommunityContainer.jsx::componentWillMount()');
-    // }
-
     render() {
         const { community } = this.props;
-        console.log('CommunityContainer.jsx::render()', this.props);
 
         return (
             <Community
@@ -28,7 +17,7 @@ class CommunityContainer extends React.Component {
 }
 
 CommunityContainer.propTypes = {
-    community: PropTypes.shape({}).isRequired, // TODO: Add Shape
+    community: PropTypes.object.isRequired, // TODO: Add Shape
 };
 
 export default CommunityContainer;

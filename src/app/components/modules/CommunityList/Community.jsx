90a3053c7decf24a0730e93b7646499e9d9ac565
roadscape <roadscape@users.noreply.github.com>
2019-09-26 15:10:44 -0500

    render about instead of description

--- src/app/components/modules/CommunityList/Community.jsx
+++ src/app/components/modules/CommunityList/Community.jsx
@@ -4,14 +4,14 @@ import SubscribeButtonContainer from 'app/components/elements/SubscribeButtonCon
 import { Link } from 'react-router';
 
 export default function Community(props) {
-    const { name, title, description } = props;
+    const { name, title, about } = props;
 
     return (
         <div className="communities__row">
             <div className="communities__names">
                 <Link to={`/trending/${name}`}>{title}</Link>
             </div>
-            <div className="communities__description">{description}</div>
+            <div className="communities__description">{about}</div>
             <div className="communities__subscription">
                 <SubscribeButtonContainer community={name} />
             </div>
@@ -23,5 +23,5 @@ export default function Community(props) {
 Community.propTypes = {
     name: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired,
-    description: PropTypes.string.isRequired,
+    about: PropTypes.string,
 };

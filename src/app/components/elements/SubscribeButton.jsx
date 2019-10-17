b393e2513632c1b1320903afed690548f8c4beb6
roadscape <roadscape@users.noreply.github.com>
2019-10-16 19:06:56 -0500

    use SET for subscribe state

--- src/app/components/elements/SubscribeButton.jsx
+++ src/app/components/elements/SubscribeButton.jsx
@@ -1,34 +1,119 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
-export default function SubscribeButton(props) {
-    const { label, loading, onClick } = props;
+import * as transactionActions from 'app/redux/TransactionReducer';
+import * as globalActions from 'app/redux/GlobalReducer';
 
-    if (loading) {
+class SubscribeButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { loading: false };
+    }
+
+    onClick = () => {
+        const { subscribed } = this.props;
+        const community = this.props.community.get('name');
+
+        this.setState({ loading: true });
+
+        this.props.toggleSubscribe(
+            !subscribed,
+            community,
+            this.props.username,
+            () => {
+                const key = ['community', community, 'context', 'subscribed'];
+                this.props.stateSet(key, !subscribed);
+                this.setState({ loading: false });
+            },
+            () => {
+                this.setState({ loading: false });
+            }
+        );
+    };
+
+    render() {
+        const { subscribed } = this.props;
+        const { loading } = this.state;
+
+        if (loading) {
+            return (
+                <button
+                    disabled
+                    className="button slim hollow secondary"
+                    type="button"
+                >
+                    Loading...
+                </button>
+            );
+        }
+
         return (
             <button
-                disabled
+                onClick={this.onClick}
                 className="button slim hollow secondary"
                 type="button"
             >
-                Loading...
+                {subscribed ? 'Unsubscribe' : 'Subscribe'}
             </button>
         );
     }
-
-    return (
-        <button
-            onClick={onClick}
-            className="button slim hollow secondary"
-            type="button"
-        >
-            {label}
-        </button>
-    );
 }
 
 SubscribeButton.propTypes = {
-    label: PropTypes.string.isRequired,
-    loading: PropTypes.bool.isRequired,
-    onClick: PropTypes.func.isRequired,
+    username: PropTypes.string.isRequired,
+    subscribed: PropTypes.bool.isRequired,
+    community: PropTypes.object.isRequired, //TODO: Define this shape
 };
+
+export default connect(
+    (state, ownProps) => {
+        return {
+            ...ownProps,
+            username: state.user.getIn(['current', 'username']),
+            subscribed: state.global.getIn(
+                ['community', ownProps.community, 'context', 'subscribed'],
+                false
+            ),
+            community: state.global.getIn(
+                ['community', ownProps.community],
+                {}
+            ),
+        };
+    },
+    dispatch => ({
+        stateSet: (key, value) => {
+            dispatch(globalActions.set({ key, value }));
+        },
+        toggleSubscribe: (
+            subscribeToCommunity,
+            community,
+            account,
+            successCallback,
+            errorCallback
+        ) => {
+            let action = 'unsubscribe';
+            if (subscribeToCommunity) action = 'subscribe';
+
+            const payload = [
+                action,
+                {
+                    community,
+                },
+            ];
+
+            return dispatch(
+                transactionActions.broadcastOperation({
+                    type: 'custom_json',
+                    operation: {
+                        id: 'community',
+                        required_posting_auths: [account],
+                        json: JSON.stringify(payload),
+                    },
+                    successCallback,
+                    errorCallback,
+                })
+            );
+        },
+    })
+)(SubscribeButton);

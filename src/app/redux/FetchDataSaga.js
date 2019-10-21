b86394669952a94f70e23cf072541cc11f63cab8
/ /\ / /\/ <imaitland@gmail.com>
2019-10-21 17:31:42 -0400

    implement edit role permissions

--- src/app/redux/FetchDataSaga.js
+++ src/app/redux/FetchDataSaga.js
@@ -167,9 +167,16 @@ export function* listCommunities(action) {
  */
 export function* getCommunity(action) {
     if (!action.payload) throw 'no community specified';
+
+    const currentUser = yield select(state => state.user.get('current'));
+    const currentUsername = currentUser && currentUser.get('username');
+
+    // TODO: If no current user is logged in, skip the observer param.
     const community = yield call(callBridge, 'get_community', {
         name: action.payload,
+        observer: currentUsername,
     });
+
     // TODO: Handle error state
     if (community.name)
         yield put(
@@ -334,10 +341,12 @@ export const actions = {
         payload,
     }),
 
-    getCommunity: payload => ({
-        type: GET_COMMUNITY,
-        payload,
-    }),
+    getCommunity: payload => {
+        return {
+            type: GET_COMMUNITY,
+            payload,
+        };
+    },
 
     getAccountNotifications: payload => ({
         type: GET_ACCOUNT_NOTIFICATIONS,

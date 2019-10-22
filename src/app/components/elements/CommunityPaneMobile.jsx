eeb9083b47a4706bd4f2fad1dba850c486a0e845
roadscape <roadscape@users.noreply.github.com>
2019-10-22 15:35:39 -0500

    CommunityPaneMobile styling progress

--- src/app/components/elements/CommunityPaneMobile.jsx
+++ src/app/components/elements/CommunityPaneMobile.jsx
@@ -19,32 +19,27 @@ class CommunityPaneMobile extends Component {
 
         return (
             <div>
-                <div className="c-sidebar__module">
-                    {Role.atLeast(viewer_role, 'mod') && (
-                        <div style={{ float: 'right', fontSize: '0.8em' }}>
-                            <SettingsEditButton
-                                community={community.get('name')}
-                            >
-                                Edit
-                            </SettingsEditButton>
-                        </div>
-                    )}
-                    <div className="c-sidebar__header">
-                        <h3 className="c-sidebar__h3">
-                            {community.get('title')}
-                        </h3>
-                        {community.get('is_nsfw') && (
-                            <span className="affiliation">nsfw</span>
-                        )}
-                    </div>
-                    <div style={{ margin: '-6px 0 12px' }}>
-                        {community.get('about')}
-                    </div>
+                <div className="c-sidebar__module CommunityPaneMobile">
                     <div
                         className="row"
                         style={{ textAlign: 'center', lineHeight: '1em' }}
                     >
-                        <div className="column small-4">
+                        <div
+                            className="column large-5 medium-6 small-12"
+                            style={{ textAlign: 'left' }}
+                        >
+                            <h3 className="c-sidebar__h3">
+                                {community.get('title')}
+                            </h3>
+                            {community.get('is_nsfw') && (
+                                <span className="affiliation">nsfw</span>
+                            )}
+                            <div style={{ margin: '-6px 0 12px' }}>
+                                {community.get('about')}
+                            </div>
+                        </div>
+
+                        <div className="column large-1 medium-2 small-2">
                             {community.get('subscribers')}
                             <br />
                             <small>
@@ -53,38 +48,33 @@ class CommunityPaneMobile extends Component {
                                     : 'subscribers'}
                             </small>
                         </div>
-                        <div className="column small-4">
+                        <div className="column large-1 medium-2 small-2">
                             {'$'}
                             {community.get('sum_pending')}
                             <br />
                             <small>pending rewards</small>
                         </div>
-                        <div className="column small-4">
+                        <div className="column large-1 medium-2 small-2">
                             {community.get('num_pending')}
                             <br />
                             <small>pending posts</small>
                         </div>
-                    </div>
 
-                    <div style={{ margin: '12px 0 0' }}>
-                        <Link
-                            className="button primary"
-                            style={{
-                                minWidth: '6em',
-                                display: 'block',
-                                marginBottom: '8px',
-                            }}
-                            to={`/submit.html?category=${category}`}
-                        >
-                            New Post
-                        </Link>
-                        {community &&
-                            this.props.username && (
-                                <SubscribeButton
-                                    community={community.get('name')}
-                                    display="block"
-                                />
-                            )}
+                        <div className="column large-4 medium-12 small-6 xsmall-12">
+                            <Link
+                                className="button primary"
+                                style={{ minWidth: '6em', marginRight: '16px' }}
+                                to={`/submit.html?category=${category}`}
+                            >
+                                Post
+                            </Link>
+                            {community &&
+                                this.props.username && (
+                                    <SubscribeButton
+                                        community={community.get('name')}
+                                    />
+                                )}
+                        </div>
                     </div>
                 </div>
             </div>

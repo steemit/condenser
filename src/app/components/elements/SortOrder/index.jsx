38a1a8d9db61eba1105c1945d57c7f401b7f7e51
roadscape <roadscape@users.noreply.github.com>
2019-10-23 10:51:49 -0500

    SortOrder tidy

--- src/app/components/elements/SortOrder/index.jsx
+++ src/app/components/elements/SortOrder/index.jsx
@@ -6,13 +6,6 @@ import { browserHistory } from 'react-router';
 import NativeSelect from 'app/components/elements/NativeSelect';
 
 const SortOrder = ({ topic, sortOrder, horizontal, pathname }) => {
-    /*
-     * We do not sort the user feed by anything other than 'new'.
-     * So don't make links to it from the SortOrder component.
-     * Instead fall back to the 'all tags' route when a user attempts to sort from a feed page.
-     * If a user lands on the 'feed' page and the sort order is displayed (e.g. a mobile user) 
-     * display the active sort as 'new'.
-     */
     let tag = topic || '';
     let sort = sortOrder;
 
@@ -21,56 +14,47 @@ const SortOrder = ({ topic, sortOrder, horizontal, pathname }) => {
         sort = 'created';
     }
 
-    // If we are at the homepage, the sort order is 'trending'
     if (pathname === '/') {
         tag = '';
         sort = 'trending';
     }
 
-    const sorts = (tag, topMenu = false, isComm = false) => {
+    const sorts = (tag, topMenu = false) => {
         if (tag != '') tag = `/${tag}`;
 
         let out = [
             {
-                value: 'trending',
                 label: tt('main_menu.trending'),
-                link: `/trending${tag}`,
+                value: `/trending${tag}`,
             },
             {
-                value: 'hot',
                 label: tt('main_menu.hot'),
-                link: `/hot${tag}`,
+                value: `/hot${tag}`,
             },
         ];
 
         if (!topMenu) {
             out.push({
-                value: 'created',
                 label: tt('g.new'),
-                link: `/created${tag}`,
+                value: `/created${tag}`,
             });
 
             /*
             out.push({
-                value: 'promoted',
                 label: tt('g.promoted'),
-                link: `/promoted${tag}`,
+                value: `/promoted${tag}`,
             });
             */
 
             out.push({
-                value: 'payout',
                 label: tt('g.payouts'),
-                link: `/payout${tag}`,
+                value: `/payout${tag}`,
             });
 
-            if (isComm) {
-                out.push({
-                    value: 'muted',
-                    label: 'Muted',
-                    link: `/muted${tag}`,
-                });
-            }
+            out.push({
+                label: 'Muted',
+                value: `/muted${tag}`,
+            });
         }
 
         return out;
@@ -78,16 +62,13 @@ const SortOrder = ({ topic, sortOrder, horizontal, pathname }) => {
 
     // vertical dropdown
     if (!horizontal) {
-        const handleChange = tag => sort => {
-            const url = tag ? `/${sort.value}/${tag}` : `/${sort.value}`;
-            browserHistory.replace(url);
-        };
-
+        const url = (sort, tag = null) =>
+            tag ? `/${sort}/${tag}` : `/${sort}`;
         return (
             <NativeSelect
-                currentlySelected={sort}
-                options={sorts(tag, false, tag.substr(0, 5) == 'hive-')}
-                onChange={handleChange(tag)}
+                currentlySelected={url(sort, tag)}
+                options={sorts(tag, false)}
+                onChange={el => browserHistory.replace(el.value)}
             />
         );
     }
@@ -95,17 +76,12 @@ const SortOrder = ({ topic, sortOrder, horizontal, pathname }) => {
     // site header
     return (
         <ul className="nav__block-list">
-            {sorts('', true, false).map(i => {
+            {sorts('', true).map(i => {
+                const active = i.value === `/${sort}`;
+                const cls = active ? 'nav__block-list-item--active' : '';
                 return (
-                    <li
-                        key={i.value}
-                        className={`nav__block-list-item ${
-                            i.value === sort && !tag
-                                ? 'nav__block-list-item--active'
-                                : ''
-                        }`}
-                    >
-                        <Link to={i.link}>{i.label}</Link>
+                    <li key={i.value} className={`nav__block-list-item ${cls}`}>
+                        <Link to={i.value}>{i.label}</Link>
                     </li>
                 );
             })}

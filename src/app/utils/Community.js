32f1e2c4db52573121faad8a708fdfc3c1d42525
roadscape <roadscape@users.noreply.github.com>
2019-10-25 14:02:51 -0500

    post btn restrictions, close #2384

--- src/app/utils/Community.js
+++ src/app/utils/Community.js
@@ -11,6 +11,24 @@ export class Role {
     static atLeast = (role, target) => {
         return Role.level(role) >= Role.level(target);
     };
+
+    static canPost = (name, role) => {
+        if (!name) return true;
+        // journal/council restriction: only members can post
+        const minRole = Role.parseType(name) == 1 ? 'guest' : 'member';
+        return Role.atLeast(role, minRole);
+    };
+
+    static canComment = (name, role) => {
+        if (!name) return true;
+        // council restriction: only members can comment
+        const minRole = Role.parseType(name) == 3 ? 'member' : 'guest';
+        return Role.atLeast(role, minRole);
+    };
+
+    static parseType = name => {
+        return parseInt(name[5]);
+    };
 }
 
 export function ifHive(category) {

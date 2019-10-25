46a68ebfdca1cfd2dbb86c0d57958f3e9b2616e4
roadscape <roadscape@users.noreply.github.com>
2019-10-25 12:06:36 -0500

    organiza routes

--- src/app/ResolveRoute.js
+++ src/app/ResolveRoute.js
@@ -13,93 +13,74 @@ export const routeRegex = {
 };
 
 export default function resolveRoute(path) {
-    if (path === '/') {
-        return { page: 'PostsIndex', params: ['trending'] };
-    }
-    if (path === '/about.html') {
-        return { page: 'About' };
-    }
-    if (path === '/welcome') {
-        return { page: 'Welcome' };
-    }
-    if (path === '/faq.html') {
-        return { page: 'Faq' };
-    }
-    if (path === '/login.html') {
-        return { page: 'Login' };
-    }
-    if (path === '/privacy.html') {
-        return { page: 'Privacy' };
-    }
-    if (path === '/support.html') {
-        return { page: 'Support' };
-    }
-    if (path === '/xss/test' && process.env.NODE_ENV === 'development') {
-        return { page: 'XSSTest' };
-    }
-    if (path === '/benchmark' && process.env.OFFLINE_SSR_TEST) {
-        return { page: 'Benchmark' };
-    }
-    if (path === '/tags') {
-        return { page: 'Tags' };
-    }
-    if (path === '/communities') {
-        return { page: 'Communities' };
-    }
-    if (path === '/tos.html') {
-        return { page: 'Tos' };
-    }
-    if (path === '/change_password') {
-        return { page: 'ChangePassword' };
-    }
-    if (path === '/recover_account_step_1') {
-        return { page: 'RecoverAccountStep1' };
-    }
-    if (path === '/market') {
-        return { page: 'Market' };
-    }
-    if (path === '/~witnesses') {
-        return { page: 'Witnesses' };
-    }
-    if (path === '/submit.html') {
-        return { page: 'SubmitPost' };
-    }
-    let match = path.match(routeRegex.CommunityRoles);
-    if (match) {
-        return { page: 'CommunityRoles', params: [match[0].split('/')[2]] };
-    }
-    match = path.match(routeRegex.UserFeed);
-    if (match) {
-        if (GDPRUserList.includes(match[1].substring(1))) {
-            return { page: 'NotFound' };
-        }
-        return { page: 'PostsIndex', params: ['home', match[1]] };
-    }
+    // index
+    if (path === '/') return { page: 'PostsIndex', params: ['trending'] };
 
+    // static
+    if (path === '/about.html') return { page: 'About' };
+    if (path === '/welcome') return { page: 'Welcome' };
+    if (path === '/faq.html') return { page: 'Faq' };
+    if (path === '/privacy.html') return { page: 'Privacy' };
+    if (path === '/support.html') return { page: 'Support' };
+    if (path === '/tos.html') return { page: 'Tos' };
+
+    // moved to wallet
+    if (path === '/change_password') return { page: 'ChangePassword' };
+    if (path === '/recover_account_step_1')
+        return { page: 'RecoverAccountStep1' };
+    if (path === '/market') return { page: 'Market' };
+    if (path === '/~witnesses') return { page: 'Witnesses' };
+
+    // developer
+    if (path === '/xss/test' && process.env.NODE_ENV === 'development')
+        return { page: 'XSSTest' };
+    if (path === '/benchmark' && process.env.OFFLINE_SSR_TEST)
+        return { page: 'Benchmark' };
+
+    // general functions
+    if (path === '/login.html') return { page: 'Login' };
+    if (path === '/submit.html') return { page: 'SubmitPost' };
+    if (path === '/tags') return { page: 'Tags' };
+    if (path === '/communities') return { page: 'Communities' };
+
+    let match;
+
+    // /roles/hive-123
+    match = path.match(routeRegex.CommunityRoles);
+    if (match)
+        return { page: 'CommunityRoles', params: [match[0].split('/')[2]] };
+
+    // /@user/feed
+    match = path.match(routeRegex.UserFeed);
+    if (match)
+        return GDPRUserList.includes(match[1].substring(1))
+            ? { page: 'NotFound' }
+            : { page: 'PostsIndex', params: ['home', match[1]] };
+
+    /// @user, /@user/blog, /@user/settings
     match = path.match(routeRegex.UserProfile);
-    if (match) {
-        if (GDPRUserList.includes(match[1].substring(1))) {
-            return { page: 'NotFound' };
-        }
-        return { page: 'UserProfile', params: match.slice(1) };
-    }
+    if (match)
+        return GDPRUserList.includes(match[1].substring(1))
+            ? { page: 'NotFound' }
+            : { page: 'UserProfile', params: match.slice(1) };
+
+    // /@user/permlink (redirects to /category/@user/permlink)
     match = path.match(routeRegex.PostNoCategory);
-    if (match) {
-        if (GDPRUserList.includes(match[1].substring(1))) {
-            return { page: 'NotFound' };
-        }
-        return { page: 'PostNoCategory', params: match.slice(1) };
-    }
+    if (match)
+        return GDPRUserList.includes(match[1].substring(1))
+            ? { page: 'NotFound' }
+            : { page: 'PostNoCategory', params: match.slice(1) };
+
+    // /category/@user/permlink
     match = path.match(routeRegex.Post);
-    if (match) {
-        if (GDPRUserList.includes(match[2].substring(1))) {
-            return { page: 'NotFound' };
-        }
-        return { page: 'Post', params: match.slice(1) };
-    }
+    if (match)
+        return GDPRUserList.includes(match[2].substring(1))
+            ? { page: 'NotFound' }
+            : { page: 'Post', params: match.slice(1) };
+
+    // /trending, /trending/category
     match = path.match(routeRegex.CategoryFilters);
-    if (match) {
-        return { page: 'PostsIndex', params: match.slice(1) };
-    }
+    if (match) return { page: 'PostsIndex', params: match.slice(1) };
+
     return { page: 'NotFound' };
 }

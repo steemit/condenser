324eed8c73a56ffc016b3b1d9e1fbde608f0c9ac
roadscape <roadscape@users.noreply.github.com>
2019-11-15 09:39:59 -0600

    remove old components

--- src/app/components/modules/Header/index.jsx
+++ src/app/components/modules/Header/index.jsx
@@ -172,8 +172,6 @@ class Header extends React.Component {
             page_title = tt('navigation.terms_of_service');
         } else if (route.page == 'CommunityRoles') {
             page_title = 'Community Roles';
-        } else if (route.page == 'RecoverAccountStep1') {
-            page_title = tt('header_jsx.stolen_account_recovery');
         } else if (route.page === 'UserProfile') {
             const user_name = route.params[0].slice(1);
             const user_title = display_name

49cf9b7a3af2f8069d85572a93fde670d92bacbe
roadscape <roadscape@users.noreply.github.com>
2019-11-20 10:24:31 -0600

    catch login.html for redirect

--- src/app/redux/UserSaga.js
+++ src/app/redux/UserSaga.js
@@ -468,8 +468,9 @@ function* usernamePasswordLogin2({
     if (afterLoginRedirectToWelcome) {
         console.log('Redirecting to welcome page');
         browserHistory.push('/welcome');
+    } else if (feedURL && document.location.pathname === '/login.html') {
+        browserHistory.push('/trending/my');
     } else if (feedURL && document.location.pathname === '/') {
-        console.log('Redirecting to feed page', feedURL);
         //browserHistory.push(feedURL);
         browserHistory.push('/trending/my');
     }

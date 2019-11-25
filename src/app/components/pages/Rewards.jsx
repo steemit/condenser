0d1e3ed797b10ad61ce46a64e8c6ed572520a83c
/ /\ / /\/ <imaitland@gmail.com>
2019-11-25 12:42:19 -0500

    add rewards route

--- src/app/components/pages/Rewards.jsx
+++ src/app/components/pages/Rewards.jsx
@@ -1,32 +1,31 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { actions as fetchDataSagaActions } from 'app/redux/FetchDataSaga';
 
 class Rewards extends Component {
-    static propTypes = {
-        fetchRewardsData: PropTypes.func.isRequired,
-    };
     render() {
         const { fetchRewardsData } = this.props;
-
         return <div>HelloWORLD</div>;
     }
 }
-
-export default connect(
-    // mapStateToProps
-    (state, ownProps) => {
-        let rewards = {};
-        if (state.global.hasIn(['rewards'])) {
-            rewards = state.global.getIn(['rewards'], null);
-        }
-        return {
-            rewards,
-        };
-    },
-    // mapDispatchToProps
-    dispatch => ({
-        fetchRewardsData: payload =>
-            dispatch(fetchDataSagaActions.getRewardsData()),
-    })
-)(Rewards);
+module.exports = {
+    path: 'rewards',
+    component: connect(
+        // mapStateToProps
+        (state, ownProps) => {
+            let rewards = {};
+            if (state.global.hasIn(['rewards'])) {
+                rewards = state.global.getIn(['rewards'], null);
+            }
+            return {
+                rewards,
+            };
+        },
+        // mapDispatchToProps
+        dispatch => ({
+            fetchRewardsData: payload =>
+                dispatch(fetchDataSagaActions.getRewardsData()),
+        })
+    )(Rewards),
+};

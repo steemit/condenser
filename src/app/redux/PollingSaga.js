ad39897da0c556d735f80548224688cddb121696
/ /\ / /\/ <imaitland@gmail.com>
2019-12-06 08:55:13 -0500

    pared down poll saga

--- src/app/redux/PollingSaga.js
+++ src/app/redux/PollingSaga.js
@@ -1,78 +1,18 @@
-import { call, take, race } from 'redux-saga/effects';
+import { put, call, take, race } from 'redux-saga/effects';
 import { delay } from 'redux-saga';
 
-// Action constants
 const START_POLLING = 'START_POLLING';
 const STOP_POLLING = 'STOP_POLLING';
 
 function* poll(action) {
-    const params = { ...action.params };
-    const stats = {
-        isPolling: false,
-        fetching: false,
-        nextPollEta: null,
-        retries: null,
-        lastResponseStatus: null,
-    };
-    debugger;
-
+    const params = { ...action.payload };
     while (true) {
-        // Start polling
-        stats.isPolling = true;
-
         try {
-            // Make the API call
-            stats.fetching = true;
-            params.onStatsChange(stats);
-            debugger;
-            const response = yield call(params.asyncFetch);
-            stats.fetching = false;
-            stats.nextPollEta = params.delay;
-            const shouldContinue = params.callback(response, stats);
-
-            // API call was successful
-            if (shouldContinue) {
-                stats.retries = 0;
-                stats.lastResponseStatus = 'success';
-                params.onStatsChange(stats);
-            } else {
-                params.onStatsChange(stats);
-                throw new Error('Error while fetching data.');
-            }
-
-            for (let i = 1; i <= params.delay; ++i) {
-                yield call(delay, 1000);
-                stats.nextPollEta = params.delay - i;
-                params.onStatsChange(stats);
-            }
+            yield put(params.pollAction(params.pollPayload));
+            yield call(delay, params.delay);
         } catch (e) {
-            // API call was unsuccessful
-            console.log(e);
-            const shouldRetry =
-                params.retryOnFailure &&
-                stats.retries < params.stopAfterRetries;
-
-            stats.fetching = false;
-            stats.lastResponseStatus = 'error';
-            stats.nextPollEta = shouldRetry ? params.retryAfter : null;
-            params.onStatsChange(stats);
-            params.callback(e, stats);
-
-            if (shouldRetry) {
-                // Update number of retries
-                for (let i = 1; i <= params.retryAfter; ++i) {
-                    yield call(delay, 1000);
-                    stats.nextPollEta = params.retryAfter - i;
-                    params.onStatsChange(stats);
-                }
-
-                ++stats.retries;
-                yield put(startPolling(params));
-            } else {
-                stats.isPolling = false;
-                params.onStatsChange(stats);
-                yield put(stopPolling());
-            }
+            // If there's an error, polling will stop.
+            yield put(stopPolling());
         }
     }
 }
@@ -84,7 +24,16 @@ export function* watchPollingTasks() {
     }
 }
 
-export const startPolling = payload => ({
-    type: START_POLLING,
-    payload,
-});
+export const startPolling = payload => {
+    return {
+        type: START_POLLING,
+        payload,
+    };
+};
+
+export const stopPolling = payload => {
+    return {
+        type: STOP_POLLING,
+        payload,
+    };
+};

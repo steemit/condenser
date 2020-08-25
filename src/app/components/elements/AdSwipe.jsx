22a832f6c9ba4cb742ca2d9a4477f1c706aa5e3c
ETY001 <work@akawa.ink>
2020-08-26 00:25:09 +0800

    rewrite the adSwiper component

--- src/app/components/elements/AdSwipe.jsx
+++ src/app/components/elements/AdSwipe.jsx
@@ -2,16 +2,40 @@ import React, { Component } from 'react';
 import { List } from 'immutable';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import ReactSwipe from 'react-swipe';
+import SwiperCore, { Autoplay } from 'swiper';
 import { recordAdsView } from 'app/utils/ServerApiClient';
+import 'swiper/swiper.scss';
+
+SwiperCore.use([Autoplay]);
 
 class AdSwipe extends Component {
     constructor() {
         super();
         this.setRecordAdsView = this.setRecordAdsView.bind(this);
+        this.onMouseOver = this.onMouseOver.bind(this);
+        this.onMouseOut = this.onMouseOut.bind(this);
+        this.state = {
+            hide: true,
+        };
+        this.swiperEl;
+        this.swiperInstance;
     }
 
     componentWillMount() {}
+
+    componentDidMount() {
+        const { width, height, direction, timer } = this.props;
+        this.swiperInstance = new SwiperCore(this.swiperEl, {
+            direction,
+            speed: 1000,
+            width,
+            height,
+            autoplay: {
+                delay: timer,
+            },
+        });
+        this.setState({ hide: false });
+    }
 
     setRecordAdsView(tag) {
         recordAdsView({
@@ -20,63 +44,84 @@ class AdSwipe extends Component {
         });
     }
 
+    onMouseOver() {
+        this.swiperInstance.autoplay.stop();
+    }
+
+    onMouseOut() {
+        this.swiperInstance.autoplay.start();
+    }
+
     render() {
-        const { width, height, adList, timer } = this.props;
-        let reactSwipeEl;
-        const swipeOpt = {
-            speed: 1000,
-            auto: timer,
-        };
+        const { width, height, adList } = this.props;
+        const { hide } = this.state;
+        const swiperWrapperClass = `swiper-wrapper ${
+            hide === true ? 'hide' : ''
+        }`;
         return (
-            <div className="ad-carousel">
-                <ReactSwipe
-                    swipeOptions={swipeOpt}
-                    childCount={adList.size}
-                    ref={el => (reactSwipeEl = el)}
-                >
-                    {adList.size > 0 &&
-                        adList.map(
+            <div
+                className="ad-carousel swiper-container"
+                ref={el => (this.swiperEl = el)}
+                style={{
+                    width: `${width}px`,
+                    height: `${height}px`,
+                }}
+            >
+                {adList.size > 0 && (
+                    <div className={swiperWrapperClass}>
+                        {adList.map(
                             (ad, inx) =>
                                 ad.get('enable') && (
-                                    <a
-                                        key={inx}
-                                        target="_blank"
-                                        href={ad.get('url')}
-                                        onClick={() =>
-                                            this.setRecordAdsView(ad.get('tag'))
-                                        }
-                                        style={{
-                                            height: `${height}px`,
-                                            width: `${width}px`,
-                                        }}
-                                    >
-                                        <img
-                                            key={inx}
-                                            src={ad.get('img')}
-                                            width={`${width}px`}
-                                            height={`${height}px`}
+                                    <div key={inx} className="swiper-slide">
+                                        <a
+                                            target="_blank"
+                                            href={ad.get('url')}
                                             onClick={() =>
-                                                this.goTo(
-                                                    ad.get('url'),
+                                                this.setRecordAdsView(
                                                     ad.get('tag')
                                                 )
                                             }
-                                        />
-                                    </a>
+                                            style={{
+                                                width: `${width}px`,
+                                                height: `${height}px`,
+                                            }}
+                                        >
+                                            <img
+                                                onMouseOut={this.onMouseOut}
+                                                onMouseOver={this.onMouseOver}
+                                                src={ad.get('img')}
+                                                style={{
+                                                    width: `${width}px`,
+                                                    height: `${height}px`,
+                                                }}
+                                            />
+                                        </a>
+                                    </div>
                                 )
                         )}
-                </ReactSwipe>
+                    </div>
+                )}
             </div>
         );
     }
 }
 
+AdSwipe.defaultProps = {
+    trackingId: '',
+    width: 240,
+    height: 240,
+    adList: List(),
+    timer: 5000,
+    direction: 'horizontal', // Could be 'horizontal' or 'vertical'
+};
+
 AdSwipe.propTypes = {
     trackingId: PropTypes.string.isRequired,
-    adList: PropTypes.instanceOf(List),
-    width: PropTypes.number,
-    height: PropTypes.number,
-    timer: PropTypes.number,
+    adList: PropTypes.instanceOf(List).isRequired,
+    width: PropTypes.number.isRequired,
+    height: PropTypes.number.isRequired,
+    timer: PropTypes.number.isRequired,
+    direction: PropTypes.string.isRequired,
 };
 
 export default connect(

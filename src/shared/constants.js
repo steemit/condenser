f057b60b40157b8b7b32f0d57350ba020eab23be
ETY001 <work@akawa.ink>
2020-11-25 23:56:41 +0800

    fix signup url in dev env

--- src/shared/constants.js
+++ src/shared/constants.js
@@ -1,5 +1,8 @@
 export const PARAM_VIEW_MODE = 'view_mode';
 export const VIEW_MODE_WHISTLE = 'whistle';
 export const WHISTLE_SIGNUP_COMPLETE = 'whistle_signup_complete';
-export const SIGNUP_URL = 'https://signup.steemit.com';
+export const SIGNUP_URL =
+    window.location.hostname.indexOf('steemitdev') === -1
+        ? 'https://signup.steemit.com'
+        : 'https://signup.steemitdev.com';
 export const SUBMIT_FORM_ID = 'submitStory';

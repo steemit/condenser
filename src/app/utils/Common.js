74574247d566e431a6e284d44f5a79cc1b6057ec
snow <snow.li@tron.network>
2021-01-14 11:07:38 +0800

    fix bug

--- src/app/utils/Common.js
+++ src/app/utils/Common.js
@@ -1,7 +1,7 @@
-export const isPhone = () => {
-    if (/(iPhone|iPad|iPod|iOS|Android)/i.test(window.navigator.userAgent)) {
+export function isPhone() {
+    if (/(iPhone|iPad|iPod|iOS|Android)/i.test(navigator.userAgent)) {
         //移动端
         return true;
     }
     return false;
-};
+}

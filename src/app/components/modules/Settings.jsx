f382b9246c807406ac4547205b358cde5a400a02
Suncheol Kwon <elytselyts@gmail.com>
2023-04-24 15:20:46 +0900

    (fix) url validation function

--- src/app/components/modules/Settings.jsx
+++ src/app/components/modules/Settings.jsx
@@ -224,7 +224,13 @@ class Settings extends React.Component {
 
     validateUrlFormat(url) {
         if (!url) return false;
-        if (!/^https?:\/\//.test(url)) return false;
+        if (
+            !/^(?:(?:(?:https?|ftp):)?\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:[/?#]\S*)?$/.test(
+                url
+            )
+        )
+            return false;
+
         return true;
     }
 
@@ -322,8 +328,8 @@ class Settings extends React.Component {
                             defaultValue={rpcNode}
                             onChange={this.handleSelectRPCNode}
                         >
-                            {$STM_Config.steemd_rpc_list.map(rpc => (
-                                <option key={rpc} value={rpc}>
+                            {$STM_Config.steemd_rpc_list.map((rpc, idx) => (
+                                <option key={idx} value={rpc}>
                                     {rpc}
                                 </option>
                             ))}

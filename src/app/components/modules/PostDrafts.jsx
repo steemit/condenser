561af7f6e70d1887688f34f36ec75861e504616e
realmankwon <realman0214@gmail.com>
2023-05-12 17:49:20 +0900

    draft thumb modifying

--- src/app/components/modules/PostDrafts.jsx
+++ src/app/components/modules/PostDrafts.jsx
@@ -7,16 +7,46 @@ import DraftSummary from '../cards/DraftSummary';
 class PostDrafts extends Component {
     constructor(props) {
         super();
+
+        this.state = {
+            draftList: [],
+        };
     }
+
+    componentDidMount() {
+        this.getDraftList();
+    }
+
+    getDraftList = () => {
+        const { username } = this.props;
+        let draftList = JSON.parse(localStorage.getItem('draft-list')) || [];
+        draftList = draftList.filter(data => data.author === username);
+        this.setState({ draftList: draftList });
+    };
+
+    onDeleteDraft = post => {
+        let draftList = JSON.parse(localStorage.getItem('draft-list')) || [];
+        const draftIdx = draftList.findIndex(
+            data =>
+                data.author === post.author && data.permlink === post.permlink
+        );
+        draftList.splice(draftIdx, 1);
+        localStorage.setItem('draft-list', JSON.stringify(draftList));
+        this.getDraftList(); // state 업데이트
+    };
 
     render() {
         const { username, onDraftsClose } = this.props;
+        const { draftList } = this.state;
 
-        let draftList = JSON.parse(localStorage.getItem('draft-list')) || [];
-        draftList = draftList.filter(data => data.author === username);
         const drafts = draftList.map((draft, idx) => (
             <div key={idx} className="drafts-option">
-                <DraftSummary post={draft} onDraftsClose={onDraftsClose} />
+                <DraftSummary
+                    idx={idx + 1}
+                    post={draft}
+                    onDraftsClose={onDraftsClose}
+                    onDeleteDraft={this.onDeleteDraft}
+                />
             </div>
         ));
 

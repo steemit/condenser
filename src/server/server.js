a9f0bd5ae3b8bfc116b07801d78797d0b05ade99
ety001 <ety001@users.noreply.github.com>
2023-09-15 17:02:57 +0800

    add nonce into scp policy (#3904)
    * add nonce into scp policy
    
    * change default scriptSrc
    
    * change default settings of csp
    
    * fix csp config
    
    * update csp policy
    
    * remove the useless gptad

--- src/server/server.js
+++ src/server/server.js
@@ -143,10 +143,26 @@ csrf(app);
 koaLocale(app);
 
 function convertEntriesToArrays(obj) {
-    return Object.keys(obj).reduce((result, key) => {
+    const conf = Object.keys(obj).reduce((result, key) => {
         result[key] = obj[key].split(/\s+/);
         return result;
     }, {});
+    console.log('convertEntriesToArrays:', JSON.stringify(conf));
+    // remove connect-src and plugin-types
+    delete conf.pluginTypes;
+    // add nonce support
+    const nonceCb = (req, res) => {
+        return `'nonce-${req.cspNonce}'`;
+    };
+    if (!conf.scriptSrc) {
+        conf.scriptSrc = [];
+    }
+    conf.scriptSrc.push(nonceCb);
+    if (!conf.defaultSrc) {
+        conf.defaultSrc = [];
+    }
+    conf.defaultSrc.push(nonceCb);
+    return conf;
 }
 
 // Fetch cached currency data for homepage
@@ -246,6 +262,15 @@ if (env === 'production') {
 } else {
     app.use(koa_logger());
 }
+
+// Sets the `script-src` directive to
+// "'self' 'nonce-e33ccde670f149c1789b1e1e113b0916'"
+// (or similar)
+app.use(function*(next) {
+    this.session.cspNonce = secureRandom.randomBuffer(16).toString('hex');
+    this.req.cspNonce = this.session.cspNonce;
+    yield next;
+});
 
 // app.use(
 //     helmet({
